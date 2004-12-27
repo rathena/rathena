1b7af8f76681481c5637a0128d8fbabcc2139031
ajarn <ajarn@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2004-12-27 00:38:25 +0000

    Fixes
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/branches/stable@801 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/vending.c
+++ src/map/vending.c
@@ -141,7 +141,7 @@ void vending_openvending(struct map_session_data *sd,int len,char *message,int f
 	}
 
 	if (flag) {
-		for(i = 0; 85 + 8 * i < len; i++) {
+		for(i = 0; (85 + 8 * i < len) && (i < MAX_VENDING); i++) {
 			sd->vending[i].index = *(short*)(p+8*i)-2;
 			sd->vending[i].amount = *(short*)(p+2+8*i);
 			sd->vending[i].value = *(int*)(p+4+8*i);
@@ -154,7 +154,6 @@ void vending_openvending(struct map_session_data *sd,int len,char *message,int f
 		}
 		sd->vender_id = sd->bl.id;
 		sd->vend_num = i;
-                Assert (sd->vend_num < (sizeof (sd->vending) / sizeof(sd->vending[0])));
 		strcpy(sd->message,message);
 		if (clif_openvending(sd,sd->vender_id,sd->vending) > 0)
 			clif_showvendingboard(&sd->bl,message,0);
