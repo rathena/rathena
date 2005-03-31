0bcdd7569977fdf2a0ad58f3b53d02c5ef8ca659
amber <amber@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2005-03-31 21:03:22 +0000

    fixed memory corruption in mapif_guild_info:int_guild.c
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/branches/stable@1367 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/char/int_guild.c
+++ src/char/int_guild.c
@@ -731,7 +731,7 @@ int mapif_guild_noinfo(int fd, int guild_id) {
 
 // ƒMƒ‹ƒhî•ñ‚Ü‚Æ‚ß‘—‚è
 int mapif_guild_info(int fd, struct guild *g) {
-	unsigned char buf[2048];
+	unsigned char buf[16384];
 
 	WBUFW(buf,0) = 0x3831;
 	memcpy(buf + 4, g, sizeof(struct guild));
