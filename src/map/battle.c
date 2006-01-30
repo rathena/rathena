28c7a6168791eeec154abde2308368fd3cac9efe
Komurka <Komurka@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-01-30 10:02:11 +0000

    One more small fix on 'bSPVanishRate' (tsd->status.hp * sd->sp_vanish_per/100 -> tsd->status.sp * sd->sp_vanish_per/100)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@5111 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/battle.c
+++ src/map/battle.c
@@ -3136,7 +3136,7 @@ int battle_weapon_attack( struct block_list *src,struct block_list *target,
 
 			if (target && target->type == BL_PC && rand()%1000 < sd->sp_vanish_rate)
 			{
-				sp = tsd->status.hp * sd->sp_vanish_per/100;
+				sp = tsd->status.sp * sd->sp_vanish_per/100;
 				if (sp > 0)
 					pc_heal(tsd, 0, -sp);
 
