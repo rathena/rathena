dacc1b195d5fd0a79c73c48e65cad41df243019b
Valaris <Valaris@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-02-03 01:51:44 +0000

    * Crash fix in npc.c npc_timerevent.   was: struct timer_event_data *ted = (struct timer_event_data*)ted;   changed: 	struct timer_event_data *ted = (struct timer_event_data*)data;   Skotlex, I am assuming it's calling from the passed data var, If I'm wrong   please fix it.  I just know what was there was definitely not correct. [Valaris]
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@5168 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/npc.c
+++ src/map/npc.c
@@ -560,7 +560,7 @@ int npc_timerevent(int tid,unsigned int tick,int id,int data)
 	unsigned int old_tick;
 	struct npc_data* nd=(struct npc_data *)map_id2bl(id);
 	struct npc_timerevent_list *te;
-	struct timer_event_data *ted = (struct timer_event_data*)ted;
+	struct timer_event_data *ted = (struct timer_event_data*)data;
 	struct map_session_data *sd=NULL;
 	
 	if( nd==NULL ){
