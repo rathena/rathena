78bf3ea131d5369ba2df1803feb37c3525e8a124
Lance <Lance@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-04-16 15:45:25 +0000

    Skot please fix the remaining code by removing the fake npc after sending the input.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@6116 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/clif.c
+++ src/map/clif.c
@@ -1715,7 +1715,7 @@ void send_fake_npc(struct map_session_data *sd, int npcid){
 	WFIFOW(fd,0)=0x78;
 	WFIFOL(fd,2)=npcid;
 	WFIFOW(fd,12)=OPTION_HIDE;
-	WFIFOW(fd,14)=111;
+	WFIFOW(fd,14)=123;
 	WFIFOPOS(fd,46,sd->bl.x,sd->bl.y);
 	WFIFOB(fd,49)=5;
 	WFIFOB(fd,50)=5;
