bbf7f7d351cd4ab2b840f3c232c25dfd7007db2b
Lance <Lance@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-05-31 08:44:43 +0000

    [Improved]: - SQL upgrade file for the new loginlog. Thanks to ultramage.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@6883 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/upgrade_svn6868.sql
+++ sql-files/upgrade_svn6868.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `loginlog` CHANGE `ip` `ip` INT( 10 ) UNSIGNED NOT NULL;
+UPDATE `loginlog` SET `ip` = inet_aton(`ip`);
+ALTER TABLE `loginlog` CHANGE `ip` `ip` INT( 10 ) UNSIGNED ZEROFILL NOT NULL DEFAULT '0';
