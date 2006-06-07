b8ebd2882b464cd3827d7c43aed301714f05e8ff
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-06-07 03:36:01 +0000

    - Solved an ambiguous if in mob_dead.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@7026 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/mob.c
+++ src/map/mob.c
@@ -2131,11 +2131,13 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 	
 	map_freeblock_unlock();
 
-	if(pcdb_checkid(md->vd->class_)) //Player mobs are not removed automatically by the client.
+	if(pcdb_checkid(md->vd->class_))
+	{	//Player mobs are not removed automatically by the client.
 		if(md->nd)
 			return 1; // Let the dead body stay there.. we have something to do with it :D
 		else
 			clif_clearchar_delay(tick+3000,&md->bl,0);
+	}
 
 	if(!md->spawn) //Tell status_damage to remove it from memory.
 		return 5; // Note: Actually, it's 4. Oh well...
