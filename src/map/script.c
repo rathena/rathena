a7644e81ac25354925e9c7440aa2ee2cfe674d96
Lance <Lance@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-06-29 12:34:32 +0000

    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@7393 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/script.c
+++ src/map/script.c
@@ -11564,11 +11564,11 @@ void run_script_main(struct script_state *st)
 				intif_saveregistry(sd,2);
 			if (sd->state.reg_dirty&1)
 				intif_saveregistry(sd,1);
-		}/* else {
+		} else {
 			script_free_stack (st->stack);
 			aFree(st);
 		}
-	}else{ 
+	/*}else{ 
 		if(st->sleep.tick > 0)
 	  	{	//Delay execution
 			TBL_PC *sd = (TBL_PC *)map_id2bl(st->rid);
