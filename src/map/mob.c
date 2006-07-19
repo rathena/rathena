0ec8fc5f1f73b2df1fd38a63e018b8966afcccec
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-07-19 20:49:14 +0000

    - Oops, should have fixed sql compilation of mob.c
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@7753 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/mob.c
+++ src/map/mob.c
@@ -3907,17 +3907,11 @@ static int mob_read_sqldb(void)
 				if(battle_config.monster_damage_delay_rate != 100)
 					status->dmotion = status->dmotion*battle_config.monster_damage_delay_rate/100;
 
-				status_calc_misc(status, mob_db_data[class_]->lv);
+				status_calc_misc(status, BL_MOB, mob_db_data[class_]->lv);
 				
-				if(!battle_config.enemy_str)
-					status->batk = 0;
-
 				if(battle_config.enemy_critical_rate != 100)
 					status->cri = status->cri*battle_config.enemy_critical_rate/100;
 				if(!status->cri && battle_config.enemy_critical_rate) status->cri = 1;
-
-				if(!battle_config.enemy_perfect_flee)
-					status->flee2 = 0;
 
 				// MVP EXP Bonus, Chance: MEXP,ExpPer
 				mob_db_data[class_]->mexp = TO_INT(30) * battle_config.mvp_exp_rate / 100;
