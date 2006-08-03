1296ac937519aa654768d944e8a96cbd69d75c8a
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-08-03 17:15:24 +0000

    - Added a damage cap to CR_ACIDDEMONSTRATION of INT_MAX/2 to prevent overflows. It is not capped to INT_MAX because damage increasing skills would cause it to still over flow afterwards.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@8107 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/battle.c
+++ src/map/battle.c
@@ -2620,6 +2620,10 @@ struct Damage  battle_calc_misc_attack(
 	case CR_ACIDDEMONSTRATION: // updated the formula based on a Japanese formula found to be exact [Reddozen]
 		md.damage = 7*tstatus->vit*sstatus->int_*sstatus->int_ / (10*(tstatus->vit+sstatus->int_));
 		if (tsd) md.damage>>=1;
+		if (md.damage < 0 || md.damage > INT_MAX>>1)
+	  	//Overflow prevention, will anyone whine if I cap it to a few billion?
+		//Not capped to INT_MAX to give some room for further damage increase.
+			md.damage = INT_MAX>>1;
 		break;
 	case NJ_ZENYNAGE:
 		md.damage = skill_get_zeny(skill_num ,skill_lv);
