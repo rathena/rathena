d12f317dbbdd02fcefd13d421a10e32c4f1f627c
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-08-08 18:55:46 +0000

    - Changed the map_freeblock_unlock code so that when there's an "already free'd pointer" error, the memory manager will print out the invoking function rather than map_freeblock_unlock, this should help fix it up next time it happens.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@8189 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/map.c
+++ src/map/map.c
@@ -290,13 +290,15 @@ int map_freeblock_lock (void)
  * バッファにたまっていたblockを全部削除
  *------------------------------------------
  */
-int map_freeblock_unlock (void)
+//int map_freeblock_unlock (void)
+int map_freeblock_unlock_sub(char *file, int lineno)
 {
 	if ((--block_free_lock) == 0) {
 		int i;
 		for (i = 0; i < block_free_count; i++)
 		{	//Directly calling aFree shouldn't be a leak, as Free remembers the size the original pointed to memory was allocated with? [Skotlex]
-			aFree(block_free[i]);
+//			aFree(block_free[i]);
+			_mfree(block_free[i], file, lineno, __func__);
 			block_free[i] = NULL;
 		}
 		block_free_count = 0;
