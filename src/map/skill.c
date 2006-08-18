656b72b31cd168437d2538adf54d42249373ec39
toms <toms@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-08-18 21:42:53 +0000

    Little code cleanup on last fix
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@8356 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/skill.c
+++ src/map/skill.c
@@ -3733,17 +3733,10 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 			sc_start(bl,type,100,skilllv,skill_get_time(skillid,skilllv)));
 		break;
 	case HLIF_AVOID:
-		i = skill_get_time(skillid,skilllv);
-		clif_skill_nodamage(src,bl,skillid,skilllv,sc_start(src,type,100,skilllv,i));
-		clif_skill_nodamage(src,src,skillid,skilllv,sc_start(&hd->master->bl,type,100,skilllv,i));
-		break;
 	case HAMI_DEFENCE:
 		i = skill_get_time(skillid,skilllv);
-		clif_skill_nodamage(src,bl,skillid,skilllv,
-			sc_start(src,type,100,skilllv,i));
-		if (bl != src)
-			clif_skill_nodamage(src,bl,skillid,skilllv,
-				sc_start(bl,type,100,skilllv,i));
+		clif_skill_nodamage(src,bl,skillid,skilllv,sc_start(bl,type,100,skilllv,i)); // Master
+		clif_skill_nodamage(src,src,skillid,skilllv,sc_start(src,type,100,skilllv,i)); // Homunc
 		break;
 	case NJ_BUNSINJYUTSU:
 		clif_skill_nodamage(src,bl,skillid,skilllv,
