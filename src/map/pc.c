12e2210cc2dbb8b1f6d1400d3706c36372386c72
Lance <Lance@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-11-07 03:17:09 +0000

    * Reapplied 'fix equipment scripts which are based on character variables not working.'
    modified   Changelog-Trunk.txt
    modified   src/map/pc.c
    
    
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@9160 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/pc.c
+++ src/map/pc.c
@@ -705,8 +705,6 @@ int pc_authok(struct map_session_data *sd, int login_id2, time_t connect_until_t
 	sd->state.event_kill_pc = 1;
 	sd->state.event_disconnect = 1;
 	sd->state.event_kill_mob = 1;
-
-	status_calc_pc(sd,1);
 			
 	sd->state.auth = 1; //Do not auth him until the initial stats have been placed.
 	{	//Add IP field
@@ -805,8 +803,11 @@ int pc_reg_received(struct map_session_data *sd)
 	
 	sd->change_level = pc_readglobalreg(sd,"jobchange_level");
 	sd->die_counter = pc_readglobalreg(sd,"PC_DIE_COUNTER");
-	if (!sd->die_counter && (sd->class_&MAPID_UPPERMASK) == MAPID_SUPER_NOVICE)
-		status_calc_pc(sd, 0); //Check +10 to all stats bonus.
+	// The earliest I can get is here to prevent more failures of status_calc_pc
+	// while making equipment scripts with global_regs possible. [Lance]
+	status_calc_pc(sd,1);
+	//if (!sd->die_counter && (sd->class_&MAPID_UPPERMASK) == MAPID_SUPER_NOVICE)
+	//	status_calc_pc(sd, 0); //Check +10 to all stats bonus.
 	if (pc_checkskill(sd, TK_MISSION)) {
 		sd->mission_mobid = pc_readglobalreg(sd,"TK_MISSION_ID");
 		sd->mission_count = pc_readglobalreg(sd,"TK_MISSION_COUNT");
