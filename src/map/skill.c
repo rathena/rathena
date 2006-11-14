b8591e80ca87a1aa42267b420ee7d6b844a5abe1
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2006-11-14 13:34:03 +0000

    - Reverted the hp conversion formula "fix" as it was correct before. - Increased the trigger rate of Warmth skills from 500ms to 200ms.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@9214 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/skill.c
+++ src/map/skill.c
@@ -4995,7 +4995,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 		{
 			int hp, sp;
 			hp = sstatus->max_hp/10;
-			sp = hp + (hp * 10 * skilllv) / 100;
+			sp = hp * 10 * skilllv / 100;
 			if (!status_charge(src,hp,0)) {
 				if (sd) clif_skill_fail(sd,skillid,0,0);
 				break;
@@ -5004,6 +5004,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 			status_heal(bl,0,sp,2);
 		}
 		break;
+
 	case HT_REMOVETRAP:
 		clif_skill_nodamage(src,bl,skillid,skilllv,1);
 		{
