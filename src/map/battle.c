9342b7e4d5c8d8ca969f5122fb9ea935e1c0c2a8
skotlex <skotlex@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2007-01-24 14:45:28 +0000

    - Also fixed the battle config setting guild castle defense :x
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@9705 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/battle.c
+++ src/map/battle.c
@@ -540,7 +540,7 @@ int battle_calc_gvg_damage(struct block_list *src,struct block_list *bl,int dama
 		if (md && md->guardian_data) {
 			damage -= damage
 			  	* (md->guardian_data->castle->defense/100)
-				* (battle_config.castle_defense_rate/100);
+				* battle_config.castle_defense_rate/100;
 		}
 		if (flag & BF_SKILL) { //Skills get a different reduction than non-skills. [Skotlex]
 			if (flag&BF_WEAPON)
