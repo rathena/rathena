e7a00c7e064138d798a708745d64b22f34d2359b
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2007-01-28 01:50:39 +0000

    Made @reloadscript wipe ALL previous mobs that the script engine spawned - this way it is manageable via global vars; the other way doesn't allow that
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@9734 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/npc.c
+++ src/map/npc.c
@@ -2852,9 +2852,7 @@ static int npc_cleanup_sub (struct block_list *bl, va_list ap) {
 		npc_unload((struct npc_data *)bl);
 		break;
 	case BL_MOB:
-		//This is used only on reloading npcs, so let's not free spawn-once mobs. [Skotlex]
-		if (((TBL_MOB*)bl)->spawn)
-			unit_free(bl,0);
+		unit_free(bl,0);
 		break;
 	}
 
