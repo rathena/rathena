0fef0f3ad9603bb1b1c8b2dee09ffc341fc6136a
Playtester <Playtester@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2007-10-03 12:50:22 +0000

    Bosses now can use teleport on Land Protector
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@11346 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/status.c
+++ src/map/status.c
@@ -972,7 +972,8 @@ int status_check_skilluse(struct block_list *src, struct block_list *target, int
 	}	
 		
 	//Should fail when used on top of Land Protector [Skotlex]
-	if (src && skill_num == AL_TELEPORT && map_getcell(src->m, src->x, src->y, CELL_CHKLANDPROTECTOR))
+	if (src && skill_num == AL_TELEPORT && map_getcell(src->m, src->x, src->y, CELL_CHKLANDPROTECTOR)
+		&& !(status->mode&MD_BOSS))
 		return 0;
 
 	if (src) sc = status_get_sc(src);
