fd1f06130c70cd82c1792b6bde463a5c379edb32
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2007-10-09 20:09:15 +0000

    Added upgrade_svn11388.sql to correct some log table column widths (for example, atcommandlog only stored first 50 chars)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@11388 54d463be-8e91-2dee-dedb-b68131a5f0ec

---
+++ sql-files/upgrade_svn11388.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `atcommandlog` CHANGE `command` `command` VARCHAR( 255 ) NOT NULL;
+ALTER TABLE `atcommandlog` CHANGE `char_name` `char_name` VARCHAR( 25 ) NOT NULL;
+ALTER TABLE `branchlog` CHANGE `char_name` `char_name` VARCHAR( 25 ) NOT NULL;
+ALTER TABLE `npclog` CHANGE `char_name` `char_name` VARCHAR( 25 ) NOT NULL;
+                                   
