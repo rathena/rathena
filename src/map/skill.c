513052b002d0289c752979b3aec0c279935108ed
Playtester <Playtester@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2007-10-12 19:11:48 +0000

    Fixed Bragi's aftercast delay reduction
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@11425 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/skill.c
+++ src/map/skill.c
@@ -6848,10 +6848,10 @@ struct skill_unit_group *skill_unitsetting (struct block_list *src, int skillid,
 	case BA_POEMBRAGI:
 		val1 = 3*skilllv+status->dex/10; // Casting time reduction
 		//For some reason at level 10 the base delay reduction is 50%.
-		val2 = (skilllv<10?3*skilllv:50)+status->int_/10; // After-cast delay reduction
+		val2 = (skilllv<10?3*skilllv:50)+status->int_/5; // After-cast delay reduction
 		if(sd){
-			val1 += pc_checkskill(sd,BA_MUSICALLESSON);
-			val2 += pc_checkskill(sd,BA_MUSICALLESSON);
+			val1 += 2*pc_checkskill(sd,BA_MUSICALLESSON);
+			val2 += 2*pc_checkskill(sd,BA_MUSICALLESSON);
 		}
 		break;
 	case DC_DONTFORGETME:
