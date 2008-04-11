a6b57811f99b9a7b9c8203172648cffaa6b8e86b
Kevin <Kevin@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-04-11 07:24:03 +0000

    New sql definitions for the quest code.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@12559 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/main.sql
+++ sql-files/main.sql
@@ -570,13 +570,11 @@ CREATE TABLE `pet` (
 --
 
 DROP TABLE IF EXISTS `quest`;
-CREATE TABLE  `quest` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL default '',
+CREATE TABLE `quest` (
   `char_id` int(11) unsigned NOT NULL default '0',
   `quest_id` int(10) unsigned NOT NULL,
   `state` enum('1','0') NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  USING BTREE (`char_id`,`quest_id`)
 ) ENGINE=MyISAM;
 
 --
@@ -584,13 +582,12 @@ CREATE TABLE  `quest` (
 --
 
 DROP TABLE IF EXISTS `quest_objective`;
-CREATE TABLE  `quest_objective` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+CREATE TABLE `quest_objective` (
   `quest_id` int(11) unsigned NOT NULL,
   `count` mediumint(8) unsigned NOT NULL default '0',
   `name` varchar(255) NOT NULL default '',
   `num` tinyint(3) unsigned NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  USING BTREE (`quest_id`,`num`)
 ) ENGINE=MyISAM;
 
 --
--- sql-files/upgrade_svn12559.sql
+++ sql-files/upgrade_svn12559.sql
@@ -3,13 +3,11 @@
 --
 
 DROP TABLE IF EXISTS `quest`;
-CREATE TABLE  `quest` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL default '',
+CREATE TABLE `quest` (
   `char_id` int(11) unsigned NOT NULL default '0',
   `quest_id` int(10) unsigned NOT NULL,
   `state` enum('1','0') NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  USING BTREE (`char_id`,`quest_id`)
 ) ENGINE=MyISAM;
 
 --
@@ -17,11 +15,10 @@ CREATE TABLE  `quest` (
 --
 
 DROP TABLE IF EXISTS `quest_objective`;
-CREATE TABLE  `quest_objective` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+CREATE TABLE `quest_objective` (
   `quest_id` int(11) unsigned NOT NULL,
   `count` mediumint(8) unsigned NOT NULL default '0',
   `name` varchar(255) NOT NULL default '',
   `num` tinyint(3) unsigned NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  USING BTREE (`quest_id`,`num`)
 ) ENGINE=MyISAM;
