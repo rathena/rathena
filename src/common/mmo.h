5dca41aec677c9143619c3f60ae66e61e63bc7fe
Playtester <Playtester@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-05-25 07:33:13 +0000

    Increased storage space to 600
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@12723 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -39,7 +39,7 @@
 #define DEFAULT_WALK_SPEED 150
 #define MIN_WALK_SPEED 0
 #define MAX_WALK_SPEED 1000
-#define MAX_STORAGE 300
+#define MAX_STORAGE 600
 #define MAX_GUILD_STORAGE 1000
 #define MAX_PARTY 12
 #define MAX_GUILD 16+10*6	// increased max guild members +6 per 1 extension levels [Lupus]
