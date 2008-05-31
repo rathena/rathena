5ae3401e2a09152e46b048d2b4c8307eaa6be357
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-05-31 01:53:51 +0000

    Removed the debug message from r12726 (bugreport:1587)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@12747 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/clif.c
+++ src/map/clif.c
@@ -2282,13 +2282,6 @@ int clif_updatestatus(struct map_session_data *sd,int type)
 	}
 	WFIFOSET(fd,len);
 
-	if( type == SP_SPEED )
-	{
-		char buf[256];
-		sprintf(buf, "Your walk speed is now %d", sd->battle_status.speed);
-		clif_displaymessage(fd, buf);
-	}
-
 	return 0;
 }
 
