1aae890f734f2920696318be281e37d59242d3ec
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-06-05 12:51:51 +0000

    Added 'E' type to picklog table's 'type' column (followup to r11721).
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@12758 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/logs.sql
+++ sql-files/logs.sql
@@ -1,6 +1,7 @@
 #PickLog types (M)onsters Drop, (P)layers Drop/Take, Mobs Drop (L)oot Drop/Take,
 # Players (T)rade Give/Take, Players (V)ending Sell/Take, (S)hop Sell/Take, (N)PC Give/Take,
-# (C)onsumable Items, (A)dministrators Create/Delete, Sto(R)age, (G)uild Storage
+# (C)onsumable Items, (A)dministrators Create/Delete, Sto(R)age, (G)uild Storage,
+# (E)mail attachment
 
 #Database: log
 #Table: picklog
@@ -8,7 +9,7 @@ CREATE TABLE `picklog` (
   `id` int(11) NOT NULL auto_increment,
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
   `char_id` int(11) NOT NULL default '0',
-  `type` enum('M','P','L','T','V','S','N','C','A','R','G') NOT NULL default 'P',
+  `type` enum('M','P','L','T','V','S','N','C','A','R','G',E') NOT NULL default 'P',
   `nameid` int(11) NOT NULL default '0',
   `amount` int(11) NOT NULL default '1',
   `refine` tinyint(3) unsigned NOT NULL default '0',
