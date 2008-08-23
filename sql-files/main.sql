b6c7de7c0092573a00980458f7f1e8c3afb613d9
zephyrus <zephyrus@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-08-23 12:14:13 +0000

    - Database structure for Mercenary.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13115 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/main.sql
+++ sql-files/main.sql
@@ -482,6 +482,21 @@ CREATE TABLE IF NOT EXISTS `memo` (
 ) ENGINE=MyISAM;
 
 --
+-- Table structure for table `mercenary`
+--
+
+CREATE TABLE IF NOT EXISTS `mercenary` (
+  `mer_id` int(11) unsigned NOT NULL auto_increment,
+  `char_id` int(11) NOT NULL,
+  `class` mediumint(9) unsigned NOT NULL default '0',
+  `hp` int(12) NOT NULL default '1',
+  `sp` int(12) NOT NULL default '1',
+  `kill_counter` int(11) NOT NULL,
+  `life_time` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`mer_id`)
+) ENGINE=MyISAM;
+
+--
 -- Table structure for table `party`
 --
 
