6f28fcce25583a972cdd22310418d600a9617e71
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-09-13 06:32:40 +0000

    Modified the 'player/GM has logged in' mapserver message to respect the lowest_gm_level config setting.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13206 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/pc.c
+++ src/map/pc.c
@@ -683,6 +683,7 @@ bool pc_authok(struct map_session_data *sd, int login_id2, time_t expiration_tim
 {
 	int i;
 	unsigned long tick = gettick();
+	uint32 ip = session[sd->fd]->client_addr;
 
 	sd->login_id2 = login_id2;
 	sd->gmlevel = gmlevel;
@@ -770,22 +771,20 @@ bool pc_authok(struct map_session_data *sd, int login_id2, time_t expiration_tim
 	//Prevent S. Novices from getting the no-death bonus just yet. [Skotlex]
 	sd->die_counter=-1;
 
-	{	//Add IP field
-		uint32 ip = session[sd->fd]->client_addr;
-		if (pc_isGM(sd))
-			ShowInfo("GM '"CL_WHITE"%s"CL_RESET"' logged in."
-				" (AID/CID: '"CL_WHITE"%d/%d"CL_RESET"',"
-				" Packet Ver: '"CL_WHITE"%d"CL_RESET"', IP: '"CL_WHITE"%d.%d.%d.%d"CL_RESET"',"
-				" GM Level '"CL_WHITE"%d"CL_RESET"').\n",
-				sd->status.name, sd->status.account_id, sd->status.char_id,
-				sd->packet_ver, CONVIP(ip), pc_isGM(sd));
-		else
-			ShowInfo("'"CL_WHITE"%s"CL_RESET"' logged in."
-				" (AID/CID: '"CL_WHITE"%d/%d"CL_RESET"',"
-				" Packet Ver: '"CL_WHITE"%d"CL_RESET"', IP: '"CL_WHITE"%d.%d.%d.%d"CL_RESET"').\n",
-				sd->status.name, sd->status.account_id, sd->status.char_id,
-				sd->packet_ver, CONVIP(ip));
-	}
+	//display login notice
+	if( sd->gmlevel >= battle_config.lowest_gm_level )
+		ShowInfo("GM '"CL_WHITE"%s"CL_RESET"' logged in."
+			" (AID/CID: '"CL_WHITE"%d/%d"CL_RESET"',"
+			" Packet Ver: '"CL_WHITE"%d"CL_RESET"', IP: '"CL_WHITE"%d.%d.%d.%d"CL_RESET"',"
+			" GM Level '"CL_WHITE"%d"CL_RESET"').\n",
+			sd->status.name, sd->status.account_id, sd->status.char_id,
+			sd->packet_ver, CONVIP(ip), sd->gmlevel);
+	else
+		ShowInfo("'"CL_WHITE"%s"CL_RESET"' logged in."
+			" (AID/CID: '"CL_WHITE"%d/%d"CL_RESET"',"
+			" Packet Ver: '"CL_WHITE"%d"CL_RESET"', IP: '"CL_WHITE"%d.%d.%d.%d"CL_RESET"').\n",
+			sd->status.name, sd->status.account_id, sd->status.char_id,
+			sd->packet_ver, CONVIP(ip));
 	
 	// Send friends list
 	clif_friendslist_send(sd);
