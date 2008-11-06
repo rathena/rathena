caa8b3ed2a65511fcb06ceb79006a63de87fc517
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2008-11-06 06:40:14 +0000

    some minor tweaks to previous commits
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13355 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/script.c
+++ src/map/script.c
@@ -1,4 +1,4 @@
-// Copyright (c) Athena Dev Teams - Licensed under GNU GPL 
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
 // For more information, see LICENCE in the main folder
 
 //#define DEBUG_DISP
