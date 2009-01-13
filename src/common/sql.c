09be248ca5ab5e1c5b1f5de7973bfce701796753
ultramage <ultramage@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2009-01-13 15:44:47 +0000

    Fixed Sql_SetEncoding() returning SQL_ERROR when it succeeds and vice versa (thanks to Rail for noticing it).
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13447 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/common/sql.c
+++ src/common/sql.c
@@ -162,7 +162,7 @@ int Sql_GetColumnNames(Sql* self, const char* table, char* out_buf, size_t buf_l
 /// Changes the encoding of the connection.
 int Sql_SetEncoding(Sql* self, const char* encoding)
 {
-	if( self && mysql_set_character_set(&self->handle, encoding) )
+	if( self && mysql_set_character_set(&self->handle, encoding) == 0 )
 		return SQL_SUCCESS;
 	return SQL_ERROR;
 }
