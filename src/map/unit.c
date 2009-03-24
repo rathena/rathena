2f424ee87c9da67fb3e1cde349dd6875d9dda28a
akrus <akrus@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2009-03-24 08:32:12 +0000

    Lol :x
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13616 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/unit.c
+++ src/map/unit.c
@@ -1071,7 +1071,7 @@ int unit_skilluse_id2(struct block_list *src, int target_id, short skill_num, sh
 
 	ud->canact_tick  = tick + casttime + 100;
 	if ( sd )
-		clif_status_change(bl, SI_ACTIONDELAY, 1, ud->canact_tick);
+		clif_status_change(src, SI_ACTIONDELAY, 1, ud->canact_tick);
 	ud->skilltarget  = target_id;
 	ud->skillx       = 0;
 	ud->skilly       = 0;
@@ -1178,7 +1178,7 @@ int unit_skilluse_pos2( struct block_list *src, short skill_x, short skill_y, sh
 
 	ud->canact_tick  = tick + casttime + 100;
 	if ( sd )
-		clif_status_change(bl, SI_ACTIONDELAY, 1, ud->canact_tick);
+		clif_status_change(src, SI_ACTIONDELAY, 1, ud->canact_tick);
 	ud->skillid      = skill_num;
 	ud->skilllv      = skill_lv;
 	ud->skillx       = skill_x;
@@ -1596,7 +1596,7 @@ int unit_skillcastcancel(struct block_list *bl,int type)
 	
 	ud->canact_tick = tick;
 	if ( sd )
-		clif_status_change(bl, SI_ACTIONDELAY, 1, ud->canact_tick);
+		clif_status_change(src, SI_ACTIONDELAY, 1, ud->canact_tick);
 
 	if(type&1 && sd)
 		skill = sd->skillid_old;
