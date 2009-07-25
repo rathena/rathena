ea229fd7e0d8df2a70aecaf73bf9140c58145112
Inkfish <Inkfish@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2009-07-25 21:02:17 +0000

    adding the sql update I forgot...
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@13960 54d463be-8e91-2dee-dedb-b68131a5f0ec

---
+++ sql-files/upgrade_svn_questlog.sql
@@ -0,0 +1,40 @@
+--
+-- Table structure for table `questlog`
+--
+
+CREATE TABLE IF NOT EXISTS `questlog` (
+  `char_id` int(11) unsigned NOT NULL default '0',
+  `quest_id` int(10) unsigned NOT NULL,
+  `state` enum('2','1','0') NOT NULL default '0',
+  `time` int(11) unsigned NOT NULL default '0',
+  `mob1` mediumint(9) unsigned NOT NULL default '0',
+  `count1` mediumint(8) unsigned NOT NULL default '0',
+  `mob2` mediumint(9) unsigned NOT NULL default '0',
+  `count2` mediumint(8) unsigned NOT NULL default '0',
+  `mob3` mediumint(9) unsigned NOT NULL default '0',
+  `count3` mediumint(8) unsigned NOT NULL default '0',
+  PRIMARY KEY  USING BTREE (`char_id`,`quest_id`)
+) ENGINE=MyISAM;
+
+DROP TABLE `quest_objective`;
+DROP TABLE `quest`;
+--
+-- Table structure for table `questlog`
+--
+
+CREATE TABLE IF NOT EXISTS `questlog` (
+  `char_id` int(11) unsigned NOT NULL default '0',
+  `quest_id` int(10) unsigned NOT NULL,
+  `state` enum('2','1','0') NOT NULL default '0',
+  `time` int(11) unsigned NOT NULL default '0',
+  `mob1` mediumint(9) unsigned NOT NULL default '0',
+  `count1` mediumint(8) unsigned NOT NULL default '0',
+  `mob2` mediumint(9) unsigned NOT NULL default '0',
+  `count2` mediumint(8) unsigned NOT NULL default '0',
+  `mob3` mediumint(9) unsigned NOT NULL default '0',
+  `count3` mediumint(8) unsigned NOT NULL default '0',
+  PRIMARY KEY  USING BTREE (`char_id`,`quest_id`)
+) ENGINE=MyISAM;
+
+DROP TABLE `quest_objective`;
+DROP TABLE `quest`;
