0089516243aa1ccbad69cf170bc6029cc0603d05
Inkfish <Inkfish@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2009-09-04 11:16:30 +0000

    forget to update main.sql
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@14037 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/main.sql
+++ sql-files/main.sql
@@ -558,11 +558,8 @@ CREATE TABLE IF NOT EXISTS `quest` (
   `quest_id` int(10) unsigned NOT NULL,
   `state` enum('0','1','2') NOT NULL default '0',
   `time` int(11) unsigned NOT NULL default '0',
-  `mob1` mediumint(9) unsigned NOT NULL default '0',
   `count1` mediumint(8) unsigned NOT NULL default '0',
-  `mob2` mediumint(9) unsigned NOT NULL default '0',
   `count2` mediumint(8) unsigned NOT NULL default '0',
-  `mob3` mediumint(9) unsigned NOT NULL default '0',
   `count3` mediumint(8) unsigned NOT NULL default '0',
   PRIMARY KEY  (`char_id`,`quest_id`)
 ) ENGINE=MyISAM;
