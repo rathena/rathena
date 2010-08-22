a5170e769486f837baa081035754f1c5cdffb92e
L0ne_W0lf <L0ne_W0lf@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2010-08-22 03:02:51 +0000

    Follow up to r14392, forgot to remove the old snprintf.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@14393 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/mob.c
+++ src/map/mob.c
@@ -2995,7 +2995,6 @@ int mobskill_use(struct mob_data *md, unsigned int tick, int event)
  			snprintf(name, sizeof name,"%s", md->name);
  			strtok(name, "#"); // discard extra name identifier if present [Daegaladh]
  			snprintf(temp, sizeof temp,"%s : %s", name, mc->msg);
-			snprintf(temp, sizeof temp,"%s : %s", md->name, mc->msg);
 			clif_messagecolor(&md->bl, mc->color, temp);
 		}
 		
