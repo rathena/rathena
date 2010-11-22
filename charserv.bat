611892db22b84607d71c960c0ac261604ef34fc8
ai4rei <ai4rei@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2010-11-22 12:36:39 +0000

    * Batch files (Windows) now detect whether the server crashed, aborted or shutdown cleanly (Ctrl+C or @mapexit) and print appropriate message, instead of always assuming, that the server crashed (bugreport:3476, since r1818).
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@14484 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- charserv.bat
+++ charserv.bat
@@ -1,9 +1,2 @@
-@echo off
-rem Writen by Jbain
-:end
-char-server.exe
-echo .
-echo .
-echo Char server crashed! restarting in 15 seconds! press ctl+C to cancel restart!
-PING -n 15 127.0.0.1 >nul
-goto end
+@ECHO OFF
+CALL serv.bat char-server.exe Char-Server
--- mapserv.bat
+++ mapserv.bat
@@ -1,9 +1,2 @@
-@echo off
-rem Writen by Jbain
-:end
-map-server.exe
-echo .
-echo .
-echo Map server crashed! restarting in 15 seconds! press ctl+C to cancel restart!
-PING -n 15 127.0.0.1 >nul
-goto end
+@ECHO OFF
+CALL serv.bat map-server.exe Map-Server
