6875f52a54b8312dca5db542a9870a4d74febc90
ai4rei <ai4rei@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-02-26 12:16:06 +0000

    * Reduced MAX_GUILD_STORAGE from 1000 to 600 (like MAX_STORAGE), since storage with 1000 items causes too large packets that are dropped then, which in turn gives the impression that the items are lost (follow up to r14503). - Note: When updating an existing server, keep in mind that excess items in guild storage get lost, when MAX_GUILD_STORAGE is reduced.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@14725 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -94,7 +94,7 @@
 #define MIN_WALK_SPEED 0
 #define MAX_WALK_SPEED 1000
 #define MAX_STORAGE 600
-#define MAX_GUILD_STORAGE 1000
+#define MAX_GUILD_STORAGE 600
 #define MAX_PARTY 12
 #define MAX_GUILD 16+10*6	// increased max guild members +6 per 1 extension levels [Lupus]
 #define MAX_GUILDPOSITION 20	// increased max guild positions to accomodate for all members [Valaris] (removed) [PoW]
