5179a206ba0b0ae3242f38b2578d047ce9fc9c20
ai4rei <ai4rei@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-05-15 05:14:29 +0000

    * Merged pcdb_checkid update from renewal [14810/branches/renewal].
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@14820 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/pc.h
+++ src/map/pc.h
@@ -549,7 +549,11 @@ enum equip_index {
 #define pc_check_weapontype(sd, type) ((type)&((sd)->status.weapon < MAX_WEAPON_TYPE? \
 	1<<(sd)->status.weapon:(1<<(sd)->weapontype1)|(1<<(sd)->weapontype2)))
 //Checks if the given class value corresponds to a player class. [Skotlex]
-#define pcdb_checkid(class_) (class_ < JOB_MAX_BASIC || (class_ >= JOB_NOVICE_HIGH && class_ < JOB_MAX))
+#define pcdb_checkid(class_) \
+( \
+	( (class_) >= JOB_NOVICE      && (class_) <  JOB_MAX_BASIC   ) \
+||	( (class_) >= JOB_NOVICE_HIGH && (class_) <  JOB_MAX         ) \
+)
 
 int pc_class2idx(int class_);
 int pc_isGM(struct map_session_data *sd);
