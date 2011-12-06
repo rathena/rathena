389f88c14f3a3ef06de6427df00ae4507bcc979f
shennetsind <shennetsind@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-12-06 05:09:21 +0000

    Fixed @mute death immunity, bugreport:4960
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@15014 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/status.c
+++ src/map/status.c
@@ -5304,7 +5304,7 @@ int status_change_start(struct block_list* bl,enum sc_type type,int rate,int val
 	if( !sc )
 		return 0; //Unable to receive status changes
 	
-	if( status_isdead(bl) )
+	if( status_isdead(bl) && type != SC_NOCHAT ) // SC_NOCHAT should work even on dead characters
 		return 0;
 
 	if( bl->type == BL_MOB && type != SC_SAFETYWALL && type != SC_PNEUMA )
