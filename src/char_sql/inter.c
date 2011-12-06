0439f415f861caa0f5ecb3f1396b6db3b190e7d4
shennetsind <shennetsind@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-12-06 05:23:33 +0000

    removed unnecessary debug message bugreport:4952 -- I actually planned this commit to have something else but I'm falling asleep, apologizes.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@15015 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/char_sql/inter.c
+++ src/char_sql/inter.c
@@ -580,7 +580,6 @@ int mapif_parse_WisToGM(int fd)
 {
 	unsigned char buf[2048]; // 0x3003/0x3803 <packet_len>.w <wispname>.24B <min_gm_level>.w <message>.?B
 	
-	ShowDebug("Sent packet back!\n");
 	memcpy(WBUFP(buf,0), RFIFOP(fd,0), RFIFOW(fd,2));
 	WBUFW(buf, 0) = 0x3803;
 	mapif_sendall(buf, RFIFOW(fd,2));
