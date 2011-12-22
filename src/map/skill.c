3ff2130031e4ce208cf41613b8c219a5449d9690
shennetsind <shennetsind@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2011-12-22 12:22:47 +0000

    Fixed create elemental converter, bugreport:5142
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@15210 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/skill.c
+++ src/map/skill.c
@@ -4724,7 +4724,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 
 	case SA_CREATECON:
 		if(sd) {
-			clif_skill_produce_mix_list(sd,skillid,23);
+			clif_skill_produce_mix_list(sd,AM_PHARMACY,23);
 			clif_skill_nodamage(src,bl,skillid,skilllv,1);
 		}
 		break;
