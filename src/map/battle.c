3c6cf65bdfb5f99a57cd5265a1cfe35e892bd3cc
epoque11 <epoque11@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2012-03-08 13:23:50 +0000

    - Fixed skillratio bonus only ever being multiplied by a factor of 1 (follow-up to r15666)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@15667 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/battle.c
+++ src/map/battle.c
@@ -1996,12 +1996,12 @@ static struct Damage battle_calc_weapon_attack(struct block_list *src,struct blo
 				case RA_ARROWSTORM:
 					skillratio += 900 + 80 * skill_lv;
 					if( status_get_lv(src) > 100 )
-						skillratio *= (status_get_lv(src) / 100); // Base level bonus.
+						skillratio = skillratio * status_get_lv(src) / 100; // Base level bonus.
 					break;
 				case RA_AIMEDBOLT:
 					skillratio += 400 + 50 * skill_lv;
 					if( status_get_lv(src) > 100 )
-						skillratio *= (status_get_lv(src) / 100); // Base level bonus.
+						skillratio = skillratio * status_get_lv(src) / 100; // Base level bonus.
 					if( tsc && (tsc->data[SC_BITE] || tsc->data[SC_ANKLE] || tsc->data[SC_ELECTRICSHOCKER]) )
 						wd.div_ = tstatus->size + 2 + rnd()%2;
 					break;
