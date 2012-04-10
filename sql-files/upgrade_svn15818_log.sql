7629e2a2921116a8eb34bdd293d340099ea633e5
brianluau <brianluau@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2012-04-10 20:32:39 +0000

    - Follow up to r15084.
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@15841 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- sql-files/upgrade_svn15818_log.sql
+++ sql-files/upgrade_svn15818_log.sql
@@ -1,3 +1,3 @@
--- Adds 'I' to `type` in `picklog` and `zenylog`
+-- Adds 'I' to `type` in `zenylog`
 
 ALTER TABLE `zenylog` MODIFY `type` ENUM('M','T','V','S','N','A','E','B','I') NOT NULL DEFAULT 'S';
