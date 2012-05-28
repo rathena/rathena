919738969e3ed7a50fc132ea2356418af2eff5ef
jmanfffreak <jmanfffreak@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2012-05-28 21:19:30 +0000

    Massive updates to /npc/custom folder: * Removed eAAC scripts, which were outdated. (tid: 64380) (will be released in downloads section) * Removed scripts in /Lance/ folder, which haven't been updated in years. (will be released in downloads section) * Combined heal.txt and heal_payment.txt into one script, set options within file to enable or disable zeny requirement. * Added choice in job changer if you allow third job changes. * Updated warper with new towns and some dungeons. * Card remover now has option to prevent or allow item destruction upon removing failure * Removed 2-2shop and replaced with a full itemmall (thanks Masao!) * General file structure changes (possibly more later)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@16164 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- npc/custom/eAAC_Scripts/DonationGirl/sql/donate.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `donate` (
-  `account_id` INT(11) UNSIGNED NOT NULL,
-  `amount` FLOAT(9,2) UNSIGNED NOT NULL,
-  `claimed` FLOAT(9,2) UNSIGNED NOT NULL,
-  PRIMARY KEY  (`account_id`)
-) TYPE=MYISAM;
