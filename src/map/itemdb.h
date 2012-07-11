da7609c973477740c4c92a41017e954286e02242
daegaladh <daegaladh@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2012-07-11 09:57:49 +0000

    Updated Item Group items to episode 13.2 and reorganized the files for future renewal updates. (bugtracker:3412) Removed renewal cards from pre-renewal item_cardalbum.txt. (bugtracker:5414)
    git-svn-id: https://svn.code.sf.net/p/rathena/svn/trunk@16392 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/itemdb.h
+++ src/map/itemdb.h
@@ -68,7 +68,7 @@ enum {
 #define IG_FINDINGORE 6
 #define IG_POTION 37
 //The max. item group count (increase this when needed).
-#define MAX_ITEMGROUP 62
+#define MAX_ITEMGROUP 63
 
 #define CARD0_FORGE 0x00FF
 #define CARD0_CREATE 0x00FE
