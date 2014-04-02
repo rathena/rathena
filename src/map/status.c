c4a41c4049a028550a7617ff218efe571f79d624
aleos89 <jarajabi@msn.com>
2014-04-02 08:27:42 -0400

    Fixed a compile error for pre-renewal. (bugreport:8855)

--- src/map/status.c
+++ src/map/status.c
@@ -3897,7 +3897,7 @@ void status_calc_regen_rate(struct block_list *bl, struct regen_data *regen, str
 #endif
 #ifndef RENEWAL
 		(bl->type == BL_PC && (((TBL_PC*)bl)->class_&MAPID_UPPERMASK) == MAPID_MONK &&
-		(sc->data[SC_EXTREMITYFIST] || (sc->data[SC_EXPLOSIONSPIRITS]) && (!sc->data[SC_SPIRIT] || sc->data[SC_SPIRIT]->val2 != SL_MONK)) ||
+		(sc->data[SC_EXTREMITYFIST] || sc->data[SC_EXPLOSIONSPIRITS]) && (!sc->data[SC_SPIRIT] || sc->data[SC_SPIRIT]->val2 != SL_MONK)) ||
 #endif
 		sc->data[SC_OBLIVIONCURSE] || sc->data[SC_VITALITYACTIVATION])
 		regen->flag &= ~RGN_SP; // No natural SP regen
