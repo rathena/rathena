3fcc7e7f3a30df5f31ae7d9ad1ffb7b533fdf3ed
Cydh Ramdh <house.bad@gmail.com>
2014-08-03 06:11:03 +0700

    Follow up 7fd6776, update main.sql for `buyingstores` & `vendings` table following https://github.com/rathena/rathena/blob/7fd6776/sql-files/upgrades/upgrade_20140723.sql
    Signed-off-by: Cydh Ramdh <house.bad@gmail.com>

--- sql-files/main.sql
+++ sql-files/main.sql
@@ -731,6 +731,9 @@ CREATE TABLE IF NOT EXISTS `vendings` (
   `x` smallint(5) unsigned NOT NULL,
   `y` smallint(5) unsigned NOT NULL,
   `title` varchar(80) NOT NULL,
+  `body_direction` CHAR( 1 ) NOT NULL DEFAULT '4',
+  `head_direction` CHAR( 1 ) NOT NULL DEFAULT '0',
+  `sit` CHAR( 1 ) NOT NULL DEFAULT '1',
   `autotrade` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
@@ -753,6 +756,9 @@ CREATE TABLE IF NOT EXISTS `buyingstores` (
   `y` smallint(5) unsigned NOT NULL,
   `title` varchar(80) NOT NULL,
   `limit` int(10) unsigned NOT NULL,
+  `body_direction` CHAR( 1 ) NOT NULL DEFAULT '4',
+  `head_direction` CHAR( 1 ) NOT NULL DEFAULT '0',
+  `sit` CHAR( 1 ) NOT NULL DEFAULT '1',
   `autotrade` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
