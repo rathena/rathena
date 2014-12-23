9d97fa14bf8f2d019f516948ccc833585dd29531
Saifur Rahman Mohsin <timetraveller1992@gmail.com>
2014-12-23 22:24:30 +0530

    Doubled the hostname length
    Most database servers use hostname of greater than 64 length. Using 32 gives an error [SQL]: Unknown MySQL server host 'MY_DATABASE_HOSTNAME_TRIMMED' (0)

--- src/login/ipban.c
+++ src/login/ipban.c
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 
 // login sql settings
-static char   ipban_db_hostname[32] = "127.0.0.1";
+static char   ipban_db_hostname[64] = "127.0.0.1";
 static uint16 ipban_db_port = 3306;
 static char   ipban_db_username[32] = "ragnarok";
 static char   ipban_db_password[32] = "";
