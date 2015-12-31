79295b4c054164329e9149fe8dd24be1ae04bc29
Lemongrass3110 <lemongrass@kstp.at>
2015-12-31 02:49:44 +0100

    Added a new file to export source side constants into the script engine.

---
+++ src/map/script_constants.h
@@ -0,0 +1,11 @@
+// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef _SCRIPT_CONSTANTS_H_
+	#define _SCRIPT_CONSTANTS_H_
+
+	#define export_constant(a) script_set_constant(###a,a,false)
+
+	#undef export_constant
+
+#endif /* _SCRIPT_CONSTANTS_H_ */
