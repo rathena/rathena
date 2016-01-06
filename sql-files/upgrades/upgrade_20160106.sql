09a87306b184f7ffbe445736133c8a51399c6945
Lemongrass3110 <lemongrass@kstp.at>
2016-01-06 02:13:19 +0100

    Added a unique index for aegis name to the item tables

---
+++ sql-files/upgrades/upgrade_20160106.sql
@@ -0,0 +1,13 @@
+-- Please only run those, that you need
+
+ALTER TABLE `item_db`
+	ADD UNIQUE INDEX `UniqueAegisName` (`name_english`) ;
+
+ALTER TABLE `item_db2`
+	ADD UNIQUE INDEX `UniqueAegisName` (`name_english`) ;
+
+ALTER TABLE `item_db_re`
+	ADD UNIQUE INDEX `UniqueAegisName` (`name_english`) ;
+
+ALTER TABLE `item_db2_re`
+	ADD UNIQUE INDEX `UniqueAegisName` (`name_english`) ;
