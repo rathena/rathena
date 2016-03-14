074bdded0979bb503288c02ec47cc5498da4023e
aleos89 <aleos89@users.noreply.github.com>
2016-03-14 18:50:29 -0400

    Cleaned up rental item expiration * Boarding Halter will now properly be removed on expiration when a player is logged out. (fixes #1035) * Removed hard coded checks for other official rental items. * Rental items now call the OnUnequip Script of the item which can be used to remove player states. * Updated various items in the database to match the new format.

--- src/map/pc.h
+++ src/map/pc.h
@@ -1180,7 +1180,6 @@ int map_night_timer(int tid, unsigned int tick, int id, intptr_t data); // by [y
 void pc_inventory_rentals(struct map_session_data *sd);
 void pc_inventory_rental_clear(struct map_session_data *sd);
 void pc_inventory_rental_add(struct map_session_data *sd, unsigned int seconds);
-void pc_rental_expire(struct map_session_data *sd, int i);
 
 int pc_read_motd(void); // [Valaris]
 int pc_disguise(struct map_session_data *sd, int class_);
