5d24d73abf8ce20ef8c3f5f0fae00805a7473638
Playtester <Kenji.Ito@gmx.de>
2016-10-29 19:43:53 +0200

    Lex Divina on allies (fixes #1668) - Lex Divina can now be cast on a silenced ally - When casting Lex Divina on an ally that is not silenced, it will no longer display the skill animation

--- src/map/skill.c
+++ src/map/skill.c
@@ -10920,10 +10920,10 @@ static int8 skill_castend_id_check(struct block_list *src, struct block_list *ta
 		case MER_LEXDIVINA:
 			{
 				//If it's not an enemy, and not silenced, you can't use the skill on them. [Skotlex]
-				if (battle_check_target(src,target, BCT_ENEMY) <= 0 && (!tsc || !tsc->data[SC_SILENCE])) {
-					clif_skill_nodamage (src, target, skill_id, skill_lv, 0);
-					return USESKILL_FAIL_MAX;
-				}
+				if (battle_check_target(src,target, BCT_ENEMY) <= 0 && (!tsc || !tsc->data[SC_SILENCE]))
+					return USESKILL_FAIL_LEVEL;
+				else
+					return -1; //Works on silenced allies
 			}
 			break;
 		case RA_WUGSTRIKE:
