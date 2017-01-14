b5a20b62cfbb55b70182312accc5985c9c583359
vstumpf <vincents.995@gmail.com>
2017-01-13 21:18:07 -0800

    Clan Chat logging is now available

--- src/map/log.c
+++ src/map/log.c
@@ -99,6 +99,7 @@ static char log_chattype2char(e_log_chat_type type)
 		case LOG_CHAT_PARTY:    return 'P';  // (P)arty
 		case LOG_CHAT_GUILD:    return 'G';  // (G)uild
 		case LOG_CHAT_MAINCHAT: return 'M';  // (M)ain chat
+		case LOG_CHAT_CLAN:     return 'C';  // (C)lan
 	}
 
 	// should not get here, fallback
