2c4cd07af02ab6ad1822508faf407d09f3d29233
aleos89 <aleos89@users.noreply.github.com>
2017-01-18 10:06:37 -0500

    Corrected script command atcommand (fixes #1894) * Resolves the map ID being stored instead of the map index when a dummy player is used. Thanks to @Tokeiburu!

--- src/map/script.c
+++ src/map/script.c
@@ -14062,7 +14062,7 @@ int atcommand_sub(struct script_state* st,int type) {
 			memcpy(&dummy_sd.bl, bl, sizeof(struct block_list));
 			if (bl->type == BL_NPC)
 				safestrncpy(dummy_sd.status.name, ((TBL_NPC*)bl)->name, NAME_LENGTH);
-			sd->mapindex = (bl->m > 0) ? bl->m : mapindex_name2id(map_default.mapname);
+			sd->mapindex = (bl->m > 0) ? map_id2index(bl->m) : mapindex_name2id(map_default.mapname);
 		}
 
 		// Init Group ID, Level, & permissions
