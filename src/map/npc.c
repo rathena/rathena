09cbf2dfa61e3eed34936e7cd2e312c02d20a211
Atemo <capucrath@gmail.com>
2017-01-22 19:14:19 +0100

    Follow up a63604d92a1c8090257277e353fd28464e859c08 The display name of the NPC won't get appended in front of the message for globalmes script command.
    Corrected area monsters spawn in Nydhoggr's Nest Instance

--- src/map/npc.c
+++ src/map/npc.c
@@ -1205,7 +1205,7 @@ int npc_globalmessage(const char* name, const char* mes)
 	if (!nd)
 		return 0;
 
-	snprintf(temp, sizeof(temp), "%s : %s", name, mes);
+	snprintf(temp, sizeof(temp), "%s", mes);
 	clif_GlobalMessage(&nd->bl,temp,ALL_CLIENT);
 
 	return 0;
