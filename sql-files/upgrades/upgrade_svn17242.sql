ec33dd6ae0c76ba6d760d1cd36dcc0176adfdb54
Daegaladh <daegaladh@shadowgames.es>
2017-03-28 10:57:58 +0200

    Fixed default dates for MySQL 5.7

--- sql-files/upgrades/upgrade_svn17242.sql
+++ sql-files/upgrades/upgrade_svn17242.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `cashlog`;
 
 CREATE TABLE `cashlog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
   `char_id` int(11) NOT NULL DEFAULT '0',
   `type` enum('T','V','P','M','S','N','D','C','A','E','I','B','$') NOT NULL DEFAULT 'S',
   `cash_type` enum('O','K','C') NOT NULL DEFAULT 'O',
