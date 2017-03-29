d92fd01368901f9bcb4fb66fc725df73ee6ed44d
Daegaladh <daegaladh@shadowgames.es>
2017-03-30 00:47:23 +0200

    Follow up to bd70335fd0fca79ebb3d24b38f5ea201e2d5edd9

--- sql-files/upgrades/upgrade_20170329.sql
+++ sql-files/upgrades/upgrade_20170329.sql
@@ -1,3 +1,10 @@
+ALTER TABLE `charlog` ALTER `time` SET DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `interlog` ALTER `time` SET DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `ipbanlist` ALTER `btime` SET DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `ipbanlist` ALTER `rtime` SET DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `login` ALTER `lastlogin` SET DEFAULT '1000-01-01 00:00:00';
+ALTER TABLE `login` ALTER `birthdate` SET DEFAULT '1000-01-01';
+
 UPDATE `charlog` SET `time` = '1000-01-01 00:00:00' WHERE `time` = '0000-00-00 00:00:00';
 UPDATE `interlog` SET `time` = '1000-01-01 00:00:00' WHERE `time` = '0000-00-00 00:00:00';
 UPDATE `ipbanlist` SET `btime` = '1000-01-01 00:00:00' WHERE `btime` = '0000-00-00 00:00:00';
