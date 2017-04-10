fd1d915e08d9ffd1b70899cf401946bd030101b6
aleos <aleos89@users.noreply.github.com>
2017-04-10 19:43:19 -0400

    Fixed AG_GET_ZENY achievement group trigger * The trigger was being called when spending Zeny but it should be called when gaining Zeny. Thanks to @Akkarinage!

--- src/map/pc.c
+++ src/map/pc.c
@@ -4279,8 +4279,6 @@ char pc_payzeny(struct map_session_data *sd, int zeny, enum e_log_pick_type type
 		clif_messagecolor(&sd->bl, color_table[COLOR_LIGHT_GREEN], output, false, SELF);
 	}
 
-	achievement_update_objective(sd, AG_GET_ZENY, 1, sd->status.zeny);
-
 	return 0;
 }
 
@@ -4315,6 +4313,8 @@ char pc_getzeny(struct map_session_data *sd, int zeny, enum e_log_pick_type type
 		sprintf(output, "Gained %dz.", zeny);
 		clif_messagecolor(&sd->bl, color_table[COLOR_LIGHT_GREEN], output, false, SELF);
 	}
+
+	achievement_update_objective(sd, AG_GET_ZENY, 1, sd->status.zeny);
 
 	return 0;
 }
