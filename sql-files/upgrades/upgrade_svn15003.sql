90e0036bbac2aa1c6283682f9758df6491fb547b
Akkarinage <akkarin@rathena.org>
2017-04-11 17:29:47 +0100

    Removed ancient SQL Upgrade files. * Old SVN upgrades are no longer needed. * Retained the last 3 years of upgrades.
    Signed-off-by: Akkarinage <akkarin@rathena.org>

--- sql-files/upgrades/upgrade_svn15003.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `global_reg_value` MODIFY `type` TINYINT(1) UNSIGNED NOT NULL DEFAULT '3';
--- sql-files/upgrades/upgrade_svn16914_log.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `zenylog` MODIFY `type` ENUM('T','V','P','M','S','N','D','C','A','E','I','B') NOT NULL DEFAULT 'S';
