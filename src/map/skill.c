857f1d102db1b7f592f2a896060d09c1fe3a50b1
aleos <aleos89@users.noreply.github.com>
2017-05-02 20:47:09 -0400

    Updated Scratch skill * Updated success rate to skill level * 10 + 70%. * Corrected duration to be 120 seconds. Thanks to @Atemo!

--- src/map/skill.c
+++ src/map/skill.c
@@ -1871,7 +1871,7 @@ int skill_additional_effect(struct block_list* src, struct block_list *bl, uint1
 		status_change_end(bl, SC_C_MARKER, INVALID_TIMER);
 		break;
 	case SU_SCRATCH:
-		sc_start2(src, bl, SC_BLEEDING, (skill_lv * 3), skill_lv, src->id, skill_get_time2(skill_id, skill_lv)); //! TODO: What's the chance/time?
+		sc_start2(src, bl, SC_BLEEDING, skill_lv * 10 + 70, skill_lv, src->id, skill_get_time(skill_id, skill_lv));
 		break;
 	case SU_SV_STEMSPEAR:
 		sc_start2(src, bl, SC_BLEEDING, 10, skill_lv, src->id, skill_get_time2(skill_id, skill_lv));
