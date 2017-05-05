2e524f18fc98271e89f2adb9553d82d4ff347d91
Jittapan Pluemsumran <foreverfk@stephack.com>
2017-05-05 16:50:58 +0700

    Removed an extra C linkage declaration in core.h

--- src/common/core.h
+++ src/common/core.h
@@ -38,18 +38,12 @@ enum {
 extern char SERVER_TYPE;
 
 extern int parse_console(const char* buf);
-#ifdef __cplusplus
-extern "C" {
-#endif
 const char *get_svn_revision(void);
 const char *get_git_hash(void);
 extern int do_init(int,char**);
 extern void set_server_type(void);
 extern void do_abort(void);
 extern void do_final(void);
-#ifdef __cplusplus
-}
-#endif
 
 /// The main loop continues until runflag is CORE_ST_STOP
 enum E_CORE_ST
