2a6277a3dc6697b8fef9f03fdb5e99ea1b348193
aleos89 <aleos89@users.noreply.github.com>
2017-06-22 14:39:05 -0400

    Adjusted NPC casted skill cast times * Fixes #2045. * NPC's that cast skills through script commands unitskilluseid or unitskillusepos with a given cast time will no longer be instant cast.

--- src/map/skill.c
+++ src/map/skill.c
@@ -16269,7 +16269,7 @@ int skill_castfix_sc(struct block_list *bl, double time, uint8 flag)
 	if (time < 0)
 		return 0;
 
-	if (bl->type == BL_MOB)
+	if (bl->type == BL_MOB || bl->type == BL_NPC)
 		return (int)time;
 
 	if (sc && sc->count) {
@@ -16322,7 +16322,7 @@ int skill_vfcastfix(struct block_list *bl, double time, uint16 skill_id, uint16 
 	if (time < 0)
 		return 0;
 
-	if (bl->type == BL_MOB)
+	if (bl->type == BL_MOB || bl->type == BL_NPC)
 		return (int)time;
 
 	if (fixed < 0 || !battle_config.default_fixed_castrate) // no fixed cast time
