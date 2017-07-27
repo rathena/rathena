02135c4e2e1c72b65b8dd06b0c64ce0aa5b2eb9a
Lemongrass3110 <lemongrass@kstp.at>
2017-07-27 21:52:43 +0200

    Added support for RODEX 2017 (#2290)

--- src/map/clif_packetdb.h
+++ src/map/clif_packetdb.h
@@ -2323,6 +2323,12 @@
 	packet(0x0AA5,-1);
 #endif
 
+// 2017-04-19bRagexeRE
+#if PACKETVER >= 20170419
+	parseable_packet(0x0AC0,26,clif_parse_Mail_refreshinbox,2,10);
+	parseable_packet(0x0AC1,26,clif_parse_Mail_refreshinbox,2,10);
+#endif
+
 // 2017-05-02dRagexeRE
 #if PACKETVER >= 20170502
 	packet(0x0A43,85);
