29a45d4dc6ef0961a5b5d0325e1e55a28f986f60
aleos <aleos89@users.noreply.github.com>
2017-10-14 17:07:19 -0400

    Cleaned up some family skills * Cleaned up some checks during skill usage. * Call All Family, One Forever, and Cheer Up are no longer influenced by INT/DEX. Thanks to @Lemongrass3110 and Rytech2!

--- src/map/skill.c
+++ src/map/skill.c
@@ -8383,7 +8383,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			struct map_session_data *p_sd = pc_get_partner(sd);
 			struct map_session_data *c_sd = pc_get_child(sd);
 
-			if (!p_sd && !c_sd) { // Fail if no family members are found
+			if (!p_sd && !c_sd && !dstsd) { // Fail if no family members are found
 				clif_skill_fail(sd, skill_id, USESKILL_FAIL_LEVEL, 0);
 				map_freeblock_unlock();
 				return 1;
@@ -8399,11 +8399,11 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 					break;
 				if (tsc && tsc->data[SC_HELLPOWER])
 					break;
-				if (map[bl->m].flag.pvp && dstsd && dstsd->pvp_point < 0)
+				if (map[bl->m].flag.pvp && dstsd->pvp_point < 0)
 					break;
-				if (dstsd && dstsd->special_state.restart_full_recover)
+				if (dstsd->special_state.restart_full_recover)
 					per = sper = 100;
-				if (dstsd && (dstsd->status.char_id == sd->status.partner_id || dstsd->status.char_id == sd->status.child) && status_revive(bl, per, sper)) // Only family members can be revived
+				if ((dstsd == p_sd || dstsd == c_sd) && status_revive(bl, per, sper)) // Only family members can be revived
 					clif_skill_nodamage(src, bl, skill_id, skill_lv, 1);
 			}
 		}
@@ -8414,13 +8414,13 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			struct map_session_data *f_sd = pc_get_father(sd);
 			struct map_session_data *m_sd = pc_get_mother(sd);
 
-			if (!f_sd && !m_sd) { // Fail if neither parent is found
+			if (!f_sd && !m_sd && !dstsd) { // Fail if no family members are found
 				clif_skill_fail(sd, skill_id, USESKILL_FAIL_LEVEL, 0);
 				map_freeblock_unlock();
 				return 1;
 			}
 			if (flag&1) { // Buff can only be given to parents in 7x7 AoE around baby
-				if (dstsd && (dstsd->status.char_id == sd->status.father || dstsd->status.char_id == sd->status.mother))
+				if (dstsd == f_sd || dstsd == m_sd)
 					clif_skill_nodamage(src, bl, skill_id, skill_lv, sc_start(src, bl, type, 100, skill_lv, skill_get_time(skill_id, skill_lv)));
 			} else
 				map_foreachinrange(skill_area_sub, bl, skill_get_splash(skill_id, skill_lv), BL_PC, src, skill_id, skill_lv, tick, flag|BCT_ALL|1, skill_castend_nodamage_id);
