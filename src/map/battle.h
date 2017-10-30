6ea6a58c55b4ea5344601e9b3568e0231c052d1d
aleos <aleos89@users.noreply.github.com>
2017-10-30 18:51:30 -0400

    Added battle config autoloot_adjust * Fixes #2540. * Added battle config autoloot_adjust that allows autoloot to take player drop penalties and bonuses into account. Thanks to @ecdarreola!

--- src/map/battle.h
+++ src/map/battle.h
@@ -634,6 +634,7 @@ extern struct Battle_Config
 	int feature_achievement;
 	int allow_bound_sell;
 	int event_refine_chance;
+	int autoloot_adjust;
 
 #include "../custom/battle_config_struct.inc"
 } battle_config;
