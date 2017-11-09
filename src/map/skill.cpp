1f7a314c783e358dfdd8adb219769103161229a7
Roman Kalashnikov <lunix0x@gmail.com>
2017-11-09 22:50:11 +0300

    Fixed twice assigned values (#2560)
    * Resolves the skill timer getting assigned twice.
    Thanks to @lunixoid!

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -11128,7 +11128,6 @@ int skill_castend_id(int tid, unsigned int tick, int id, intptr_t data)
 
 		if( sd && ud->skilltimer != INVALID_TIMER && (pc_checkskill(sd,SA_FREECAST) > 0 || ud->skill_id == LG_EXEEDBREAK) )
 		{// restore original walk speed
-			ud->skilltimer = INVALID_TIMER;
 			status_calc_bl(&sd->bl, SCB_SPEED);
 		}
 
@@ -11409,7 +11408,6 @@ int skill_castend_pos(int tid, unsigned int tick, int id, intptr_t data)
 
 	if( sd && ud->skilltimer != INVALID_TIMER && ( pc_checkskill(sd,SA_FREECAST) > 0 || ud->skill_id == LG_EXEEDBREAK ) )
 	{// restore original walk speed
-		ud->skilltimer = INVALID_TIMER;
 		status_calc_bl(&sd->bl, SCB_SPEED);
 	}
 	ud->skilltimer = INVALID_TIMER;
