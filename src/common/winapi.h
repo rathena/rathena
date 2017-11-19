796dfe8d5aef6a8b3cdef1ef68566c2bfbdb6baa
Lemongrass3110 <lemongrass@kstp.at>
2017-11-19 03:43:51 +0100

    Moved Windows Vista declaration up
    This way all defines that should actually come from sdkddkver.h will be checked right behind it and defined if they were not.

--- src/common/winapi.h
+++ src/common/winapi.h
@@ -14,6 +14,9 @@
 #ifndef _WIN32_IE
 	#define _WIN32_IE 	0x0600 // Internet Explorer 6.0
 #endif
+#ifndef _WIN32_WINNT_VISTA
+	#define _WIN32_WINNT_VISTA 0x0600 // Windows Vista
+#endif
 
 #define STRICT
 #define WIN32_LEAN_AND_MEAN
@@ -30,10 +33,6 @@
 #define NOSERVICE
 #define NOSOUND
 #define NOTEXTMETRIC
-
-#ifndef _WIN32_WINNT_VISTA
-	#define _WIN32_WINNT_VISTA 0x0600
-#endif
 
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_NONSTDC_NO_WARNINGS
