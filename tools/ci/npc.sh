61220e5ff94776d8d36c106972fd5714518d54c9
Jey <jey@bjx-studios.de>
2017-12-01 12:50:14 +0100

    Corrects the relative path
    * npc.sh is executed from rathenas root directory.

--- tools/ci/npc.sh
+++ tools/ci/npc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-out=../../npc/scripts_custom.conf
+out=npc/scripts_custom.conf
 
 printf "\n" >> $out
 echo "// Custom Scripts" >> $out
 
-find ../../npc/ \( -wholename "*npc/custom/*.txt" -and -not -wholename "*npc/custom/battleground/*" \) | cut -c 7- | xargs -I % echo "npc: %" >> $out
+find npc/custom \( -name "*.txt" -and -not -wholename "*/battleground/*" \) | xargs -I % echo "npc: %" >> $out
 
 echo "// Test Scripts" >> $out
 
-find ../../npc/ \( -wholename "*npc/test/*.txt" \) | cut -c 7- | xargs -I % echo "npc: %" >> $out
+find npc/test \( -name "*.txt" \) | xargs -I % echo "npc: %" >> $out
