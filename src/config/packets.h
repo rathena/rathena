f68aba07956f69244d07c14384c458d19d2bc848
sader fawall <sader1992@hotmail.com>
2017-12-01 20:46:42 +0200

    update 3

--- src/config/packets.h
+++ src/config/packets.h
@@ -9,9 +9,9 @@
  **/
 
 #ifndef PACKETVER
-	///Do not edit this line. To set your client version, please do one of the folowing instead.
-	///for Windows and Linux: add this line in your src\custom\defines_pre.h file: #define PACKETVER YYYYMMDD
-	///for Linux Only: run the following command: ./configure --enable-packetver=YYYYMMDD
+	///Do not edit this line. To set your client version, please do this instead.
+	///In Windows: add this line in your src\custom\defines_pre.h file: #define PACKETVER YYYYMMDD
+	///In Linux: The same as above, or run the following command: ./configure --enable-packetver=YYYYMMDD
 	#define PACKETVER 20151104
 #endif
 
