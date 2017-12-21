9315781a485d20660f34f168a3a36c6018225df1
Aleos <aleos89@users.noreply.github.com>
2017-12-21 12:54:19 -0500

    Updated Pet catching values (#2719)
    * Added pet catching constants.
    * Converted catch_target_class from short to uint16 for future expansion.
    Thanks to @Lemongrass3110!

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -1347,6 +1347,8 @@ bool pc_authok(struct map_session_data *sd, uint32 login_id2, time_t expiration_
 	sd->hatEffectCount = 0;
 #endif
 
+	sd->catch_target_class = PET_CATCH_FAIL;
+
 	// Check EXP overflow, since in previous revision EXP on Max Level can be more than 'official' Max EXP
 	if (pc_is_maxbaselv(sd) && sd->status.base_exp > MAX_LEVEL_BASE_EXP) {
 		sd->status.base_exp = MAX_LEVEL_BASE_EXP;
@@ -5059,8 +5061,8 @@ int pc_useitem(struct map_session_data *sd,int n)
 
 	sd->itemid = item.nameid;
 	sd->itemindex = n;
-	if(sd->catch_target_class != -1) //Abort pet catching.
-		sd->catch_target_class = -1;
+	if(sd->catch_target_class != PET_CATCH_FAIL) //Abort pet catching.
+		sd->catch_target_class = PET_CATCH_FAIL;
 
 	amount = item.amount;
 	script = id->script;
--- src/map/pet.hpp
+++ src/map/pet.hpp
@@ -47,6 +47,11 @@ extern struct s_pet_db pet_db[MAX_PET_DB];
 
 enum e_pet_itemtype : uint8 { PET_CLASS,PET_CATCH,PET_EGG,PET_EQUIP,PET_FOOD };
 
+enum e_pet_catch : uint16 {
+	PET_CATCH_FAIL = 0, ///< A catch attempt failed
+	PET_CATCH_UNIVERSAL = 1, ///< The catch attempt is universal (ignoring MD_STATUS_IMMUNE/Boss)
+};
+
 struct pet_recovery { //Stat recovery
 	enum sc_type type;	//Status Change id
 	unsigned short delay; //How long before curing (secs).
