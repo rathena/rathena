c5c092e40bcf12fda35ad6ac3fbb7c3f715cafa8
Daegaladh <daegaladh@shadowgames.es>
2018-02-01 00:11:28 +0100

    Fixed AL_HOLYWATER casting and consuming sp when having no bottles

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -14967,6 +14967,12 @@ bool skill_check_condition_castbegin(struct map_session_data* sd, uint16 skill_i
 				return false;
 			}
 			break;
+		case AL_HOLYWATER:
+			if(pc_search_inventory(sd,ITEMID_EMPTY_BOTTLE) < 0) {
+				clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
+				return false;
+			}
+			break;
 		case MO_CALLSPIRITS:
 			if(sc && sc->data[SC_RAISINGDRAGON])
 				skill_lv += sc->data[SC_RAISINGDRAGON]->val1;
