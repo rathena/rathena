58b15a25c60ee015e1d9dba40af51effec0b0839
vstumpf <vincents.995@gmail.com>
2018-03-18 21:45:13 +0000

    Corrected item_db script curly parsing Fixes #2964 Correctly finds the last closeright curly.

--- src/map/itemdb.cpp
+++ src/map/itemdb.cpp
@@ -1484,7 +1484,7 @@ static int itemdb_readdb(void){
 				ShowError("itemdb_readdb: Invalid format (OnUnequip_Script column) in line %d of \"%s\" (item with id %d), skipping.\n", lines, path, atoi(str[0]));
 				continue;
 			}
-			str[21] = p + 1;
+			str[21] = p;
 			p = &str[21][strlen(str[21]) - 2];
 
 			if ( *p != '}' ) {
@@ -1494,8 +1494,10 @@ static int itemdb_readdb(void){
 				for( v = 0; v < strlen(str[21]); v++ ) {
 					if( str[21][v] == '{' )
 						lcurly++;
-					else if ( str[21][v] == '}' )
+					else if (str[21][v] == '}') {
 						rcurly++;
+						p = &str[21][v];
+					}
 				}
 
 				if( lcurly != rcurly ) {
@@ -1503,7 +1505,8 @@ static int itemdb_readdb(void){
 					continue;
 				}
 			}
-			*p = '\0';
+			str[21] = str[21] + 1;  //skip the first left curly
+			*p = '\0';              //null the last right curly
 
 			if (!itemdb_parse_dbrow(str, path, lines, SCRIPT_IGNORE_EXTERNAL_BRACKETS))
 				continue;
