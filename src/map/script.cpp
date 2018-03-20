41ea1531bcf05b79f287107a9af973ae1d087ec5
Jittapan Pluemsumran <nightsheep@outlook.com>
2018-03-20 21:56:11 +0700

    Prevent unloadnpc script command to unload the invoking NPC. (#3000)
    * Prevent unloadnpc script command to unload the invoking NPC.
    Addressed #2999

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -23391,6 +23391,10 @@ BUILDIN_FUNC(unloadnpc) {
 	if( nd == NULL ){
 		ShowError( "buildin_unloadnpc: npc '%s' was not found.\n", name );
 		return SCRIPT_CMD_FAILURE;
+	} else if ( nd->bl.id == st->oid ) {
+		// Supporting self-unload isn't worth the problem it may cause. [Secret]
+		ShowError("buildin_unloadnpc: You cannot self-unload NPC '%s'.\n.", name);
+		return SCRIPT_CMD_FAILURE;
 	}
 
 	npc_unload_duplicates(nd);
