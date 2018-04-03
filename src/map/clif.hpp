170da17844af3e4f34db3c8e317356d0987f2acb
Lemongrass3110 <lemongrass@kstp.at>
2018-04-03 10:15:46 +0200

    Dropped an unused declaration

--- src/map/clif.hpp
+++ src/map/clif.hpp
@@ -1065,7 +1065,6 @@ void clif_SelectCart(struct map_session_data *sd);
 /// Achievement System
 void clif_achievement_list_all(struct map_session_data *sd);
 void clif_achievement_update(struct map_session_data *sd, struct achievement *ach, int count);
-void clif_pAchievementCheckReward(int fd, struct map_session_data *sd);
 void clif_achievement_reward_ack(int fd, unsigned char result, int ach_id);
 
 #endif /* _CLIF_HPP_ */
