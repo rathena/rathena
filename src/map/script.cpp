b7f7a04ce3ebd2bb985b561140adef6a999ce95d
sader fawall <sader1992@hotmail.com>
2018-04-24 13:15:20 +0200

    fix compiling error and add doc

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -6659,7 +6659,7 @@ BUILDIN_FUNC(getelementofarray)
 	return SCRIPT_CMD_SUCCESS;
 }
 
-/// return the matches of an array, from the starting index.
+/// return the matches count of an array, from the starting index.
 /// ex: searcharray arr,1,2,3;
 ///
 /// searcharray <array variable>,<value1>{,<value2>...};
@@ -6667,77 +6667,73 @@ BUILDIN_FUNC(searcharray)
 {
 	struct script_data *data;
 	const char* name;
-	int id , i, j, array_size , case_count = 0;
+	int id, i, j, array_size, case_count = 0;
 	struct map_session_data* sd = NULL;
+	struct reg_db *ref = NULL;
 	data = script_getdata(st, 2);
-	
+
 	if (!data_isreference(data))
 	{
-		ShowError("script:searcharray: not a variable\n");
+		ShowError("buildin_searcharray: not a variable\n");
 		script_reportdata(data);
-		script_pushnil(st);
+		st->state = END;
+		return SCRIPT_CMD_FAILURE;
+	}
+
+	name = reference_getname(data);
+	ref = reference_getref(data);
+
+	if (not_server_variable(*name) && !script_rid2sd(sd))
+			return SCRIPT_CMD_FAILURE;
+
+	array_size = script_array_highest_key(st, sd, name, ref) - 1;
+
+	if (array_size > SCRIPT_MAX_ARRAYSIZE)
+	{
+		ShowError("buildin_searcharray: The array is too large.\n");
+		script_reportdata(data);
 		st->state = END;
 		return SCRIPT_CMD_FAILURE;
 	}
 
 	id = reference_getid(data);
-	name = reference_getname(data);
-
-	if (not_server_variable(*name)) {
-		if (!script_rid2sd(sd))
-			return SCRIPT_CMD_SUCCESS;
-	}
-
-	array_size = script_array_highest_key(st, sd, name, reference_getref(data)) - 1;
-
-	if (array_size > SCRIPT_MAX_ARRAYSIZE)
-	{
-		ShowError("script:searcharray: The array is to large.\n");
-		script_reportdata(data);
-		script_pushnil(st);
-		st->state = END;
-		return SCRIPT_CMD_FAILURE;
-	}
-
-	if (is_string_variable(name)) 
+	if (is_string_variable(name))
 	{
 		const char* temp;
 		const char* value_list;
-		for (i = 0; i <= array_size; ++i) {
-			temp = (char*)get_val2(st, reference_uid(id, i), reference_getref(data));
-			j = 3;
-			while (script_hasdata(st, j))
+		for (i = 0; i <= array_size; ++i)
+		{
+			temp = (char*)get_val2(st, reference_uid(id, i), ref);
+			for (j = 3; script_hasdata(st, j); j++)
 			{
 				value_list = script_getstr(st, j);
 				if (!strcmp(temp, value_list))
 				{
 					case_count++;
 				}
-				j++;
 			}
 			script_removetop(st, -1, 0);
 		}
 	}
-	else 
+	else
 	{
-		int temp , value_list;
-		for (i = 0; i <= array_size; ++i) {
-			temp = (int)get_val2(st, reference_uid(id, i), reference_getref(data));
-			j = 3;
-			while (script_hasdata(st, j)) 
+		int temp, value_list;
+		for (i = 0; i <= array_size; ++i)
+		{
+			temp = (int32)__64BPRTSIZE(get_val2(st, reference_uid(id, i), ref));
+			for (j = 3; script_hasdata(st, j); j++)
 			{
 				value_list = script_getnum(st, j);
 				if (temp == value_list)
 				{
 					case_count++;
 				}
-				j++;
 			}
 			script_removetop(st, -1, 0);
 		}
 	}
-	
-	push_val(st->stack, C_INT, case_count);
+
+	script_pushint(st, case_count);
 	return SCRIPT_CMD_SUCCESS;
 }
 
