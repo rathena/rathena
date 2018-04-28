06318a071c2e569684816f7abbfa25a56f187703
Atemo <capucrath@gmail.com>
2018-04-28 15:25:19 +0200

    Corrected NPC_COMET ratio and Hero_Judgement_Shawl / Hero_Taget_Suits item script
    Thanks to @kukuasir1 and @Akaineko-astasi !

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -6231,7 +6231,7 @@ struct Damage battle_calc_magic_attack(struct block_list *src,struct block_list 
 					case NPC_COMET:
 						i = (sc ? distance_xy(target->x, target->y, sc->comet_x, sc->comet_y) : 8) / 2;
 						i = cap_value(i, 1, 4);
-						skillratio = 2500 + ((skill_lv - i) * 500);
+						skillratio = 2500 + ((skill_lv - i + 1) * 500);
 						break;
 				}
 
