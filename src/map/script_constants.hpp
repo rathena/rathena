dd60f4c47ee5eb5b1b9aa383b1e9bc81d5a2bc61
Lemongrass3110 <lemongrass@kstp.at>
2018-05-27 22:52:08 +0200

    Added 2 new parameters for getpetinfo (#3164)
    Fixed inter-server handling for creation of pets that are not incubated.
    
    Thanks to @Everade for his idea.

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -3922,6 +3922,8 @@
 	export_constant(PETINFO_RENAMED);
 	export_constant(PETINFO_LEVEL);
 	export_constant(PETINFO_BLOCKID);
+	export_constant(PETINFO_EGGID);
+	export_constant(PETINFO_FOODID);
 
 	// For backwards compatability - might be removed in the near future
 	export_constant2("PET_ID",PETINFO_ID);
@@ -4334,6 +4336,7 @@
 	export_constant(ROA_VALUE);
 	export_constant(ROA_PARAM);
 
+	/* special card flags */
 	export_constant(CARD0_FORGE);
 	export_constant(CARD0_CREATE);
 	export_constant(CARD0_PET);
