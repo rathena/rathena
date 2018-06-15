f0c53d2536ed437c8a4f5e53a70aa34dd494f527
aleos89 <aleos89@users.noreply.github.com>
2018-06-15 13:40:51 -0400

    Synchronized login-server headers * Alphabetically sorted includes. * Updated copyright and license text to match across all files. * Removed pragma once define in header files in lieu of ifdef guards.

--- src/login/loginchrif.hpp
+++ src/login/loginchrif.hpp
@@ -1,13 +1,6 @@
-/**
- * @file loginchrif.h
- * Module purpose is to handle incoming and outgoing requests with char-server.
- * Licensed under GNU GPL.
- *  For more information, see LICENCE in the main folder.
- * @author Athena Dev Teams originally in login.c
- * @author rAthena Dev Team
- */
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
 
-#pragma once
 #ifndef _LOGINCHRIF_HPP_
 #define _LOGINCHRIF_HPP_
 
