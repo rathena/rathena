b05ca3bc32bc3d82f220a19be25512aa6ca70a4d
aleos89 <aleos89@users.noreply.github.com>
2018-08-31 09:52:38 -0400

    Corrected SQL upgrade format * Fixed incorrect ticks. Thanks to @Haikenz!

--- sql-files/upgrades/upgrade_20180830.sql
+++ sql-files/upgrades/upgrade_20180830.sql
@@ -1,3 +1,3 @@
-UPDATE `char` ch, `skill` sk SET ch.`skill_point` = ch.`skill_point + sk.`lv` WHERE sk.`id` = 2049 AND ch.`char_id` = sk.`char_id`;
+UPDATE `char` SET `char`.`skill_point` = `char`.`skill_point` + `skill`.`lv` WHERE `skill`.`id` = 2049 AND `char`.`char_id` = `skill`.`char_id`;
 
 DELETE FROM `skill` WHERE `id` = 2049;
