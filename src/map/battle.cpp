266adc7492f05b016de73feacc829022886c417e
aleos89 <aleos89@users.noreply.github.com>
2018-09-19 10:00:16 -0400

    Corrected Union of the Sun Moon and Stars HP cost * Fixes #3500. * Corrected the HP cost formula to reflect Aegis. Thanks to @mrjnumber1!

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5150,13 +5150,14 @@ struct Damage battle_calc_weapon_final_atk_modifiers(struct Damage wd, struct bl
 	if( sc ) {
 		//SC_FUSION hp penalty [Komurka]
 		if (sc->data[SC_FUSION]) {
-			int hp= sstatus->max_hp;
+			unsigned int hp = sstatus->max_hp;
+
 			if (sd && tsd) {
-				hp = 8*hp/100;
+				hp = hp / 13;
 				if (((int64)sstatus->hp * 100) <= ((int64)sstatus->max_hp * 20))
 					hp = sstatus->hp;
 			} else
-				hp = 2*hp/100; //2% hp loss per hit
+				hp = hp / 50; //2% hp loss per hit
 			status_zap(src, hp, 0);
 		}
 		// Only affecting non-skills
