205e51119ae8c44d48df5f8e1c70c0b3ab8a4d06
Lemongrass3110 <lemongrass@kstp.at>
2018-10-04 23:10:34 +0200

    Fixed some small issues found by @Atemo

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -8512,7 +8512,7 @@ static const struct _battle_data {
 	{ "allow_bound_sell",                   &battle_config.allow_bound_sell,                0,      0,      0xF,            },
 	{ "event_refine_chance",                &battle_config.event_refine_chance,             0,      0,      1,              },
 	{ "autoloot_adjust",                    &battle_config.autoloot_adjust,                 0,      0,      1,              },
-    { "feature.petevolution",               &battle_config.feature_petevolution,            1,      0,      1,              },
+	{ "feature.petevolution",               &battle_config.feature_petevolution,            1,      0,      1,              },
 	{ "feature.petautofeed",                &battle_config.feature_pet_auto_feed,           1,      0,      1,              },
 	{ "broadcast_hide_name",                &battle_config.broadcast_hide_name,             2,      0,      NAME_LENGTH,    },
 	{ "skill_drop_items_full",              &battle_config.skill_drop_items_full,           0,      0,      1,              },
@@ -8662,9 +8662,9 @@ void battle_adjust_conf()
 		ShowWarning("conf/battle/feature.conf petevolution is enabled but it requires PACKETVER 2014-10-08 or newer, disabling...\n");
 		battle_config.feature_petevolution = 0;
 	}
-	if (battle_config.feature_petevolution) {
+	if (battle_config.feature_pet_auto_feed) {
 		ShowWarning("conf/battle/feature.conf pet auto feed is enabled but it requires PACKETVER 2014-10-08 or newer, disabling...\n");
-		battle_config.feature_petevolution = 0;
+		battle_config.feature_pet_auto_feed = 0;
 	}
 #endif
 
