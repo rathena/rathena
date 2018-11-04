5f69ece994369f310192e0221f27d0df05ffa1c4
Hafiz Bistar <hafiz.bistar@gmail.com>
2018-11-04 23:08:49 +0800

    Diwali

---
+++ sql-files/custom/20181104_diwali.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `item_db2_re` (`id`, `name_english`, `name_japanese`, `type`, `price_buy`, `price_sell`, `weight`, `atk:matk`, `defence`, `range`, `slots`, `equip_jobs`, `equip_upper`, `equip_genders`, `equip_locations`, `weapon_level`, `equip_level`, `refineable`, `view`, `script`, `equip_script`, `unequip_script`) VALUES
+(16361, 'FDesert_Prince_Box', 'FDesert Prince Box', 18, 0, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'rentitem 5591,1728000;', NULL, NULL);
