2adccce5a00436865b1893b7b28a09d918e0c3ef
aleos <aleos89@users.noreply.github.com>
2018-11-12 11:31:59 -0500

    Corrected White Imprison and reflected damage * Fixes #3634. * Targets in White Imprison using reflect equipment or skills should not send damage back to those outside. Thanks to @admkakaroto!

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -6842,6 +6842,9 @@ int64 battle_calc_return_damage(struct block_list* bl, struct block_list *src, i
 	sc = status_get_sc(bl);
 	ssc = status_get_sc(src);
 
+	if (sc->data[SC_WHITEIMPRISON])
+		return 0; // White Imprison does not reflect any damage
+
 	if (flag & BF_SHORT) {//Bounces back part of the damage.
 		if ( (skill_get_inf2(skill_id)&INF2_TRAP || !status_reflect) && sd && sd->bonus.short_weapon_damage_return ) {
 			rdamage += damage * sd->bonus.short_weapon_damage_return / 100;
