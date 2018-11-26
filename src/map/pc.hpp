79e983dfc0efc7f6b41ff7f1aa7658252b30cec9
aleos <aleos89@users.noreply.github.com>
2018-11-26 13:51:45 -0500

    Corrected a potential overflow in item bonuses * Fixes #3703. * Fixed bHealPower, bHealPower2, bSPGainValue, bHPGainValue, bMagicSPGainValue, and bMagicHPGainValue having a chance to potentially overflow from signed short limit. Thanks to @Everade!

--- src/map/pc.hpp
+++ src/map/pc.hpp
@@ -485,8 +485,8 @@ struct map_session_data {
 
 		short splash_range, splash_add_range;
 		short add_steal_rate;
-		short add_heal_rate, add_heal2_rate;
-		short sp_gain_value, hp_gain_value, magic_sp_gain_value, magic_hp_gain_value;
+		int add_heal_rate, add_heal2_rate;
+		int sp_gain_value, hp_gain_value, magic_sp_gain_value, magic_hp_gain_value;
 		short sp_vanish_rate, hp_vanish_rate;
 		short sp_vanish_per, hp_vanish_per;
 		unsigned short unbreakable;	// chance to prevent ANY equipment breaking [celest]
