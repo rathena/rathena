55971054518a82724132b2f1ff46d652099ac78c
Lemongrass3110 <lemongrass@kstp.at>
2018-12-06 22:26:35 +0100

    Fixed an issue with bonus recalculation
    Fixes #3725
    
    Thanks to @jester-frost and @laziem

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -3556,8 +3556,9 @@ int status_calc_pc_sub(struct map_session_data* sd, enum e_status_calc_opt opt)
 		+ sizeof(sd->norecover_state_race)
 		+ sizeof(sd->hp_vanish_race)
 		+ sizeof(sd->sp_vanish_race)
-		+ sizeof(sd->bonus)
 	);
+
+	memset(&sd->bonus, 0, sizeof(sd->bonus));
 
 	// Autobonus
 	pc_delautobonus(sd, sd->autobonus, true);
