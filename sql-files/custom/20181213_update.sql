7b9ee6686b1a63a2bf874f7c6fbdfb36baa33684
Hafiz Bistar <hafiz.bistar@gmail.com>
2018-12-11 17:47:04 +0800

    Fixed Mob DB's elemental property.
    Change the skill ID for Stefan from fake to real.

--- sql-files/custom/20181213_update.sql
+++ sql-files/custom/20181213_update.sql
@@ -27,19 +27,19 @@ REPLACE INTO `item_db2_re` (`id`, `name_english`, `name_japanese`, `type`, `pric
 
 # SkyFortress Mobs
 REPLACE INTO `mob_db2_re` (`ID`, `Sprite`, `kName`, `iName`, `LV`, `HP`, `SP`, `EXP`, `JEXP`, `Range1`, `ATK1`, `ATK2`, `DEF`, `MDEF`, `STR`, `AGI`, `VIT`, `INT`, `DEX`, `LUK`, `Range2`, `Range3`, `Scale`, `Race`, `Element`, `Mode`, `Speed`, `aDelay`, `aMotion`, `dMotion`, `MEXP`, `MVP1id`, `MVP1per`, `MVP2id`, `MVP2per`, `MVP3id`, `MVP3per`, `Drop1id`, `Drop1per`, `Drop2id`, `Drop2per`, `Drop3id`, `Drop3per`, `Drop4id`, `Drop4per`, `Drop5id`, `Drop5per`, `Drop6id`, `Drop6per`, `Drop7id`, `Drop7per`, `Drop8id`, `Drop8per`, `Drop9id`, `Drop9per`, `DropCardid`, `DropCardper`) VALUES
-(3485, 'AS_D_CURSED_SOLDIER', 'Cursed Soldier of Bijou', 'Cursed Soldier of Bijou', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3484, 'AS_D_RAGGED_GOLEM', 'Stefan.J.E.Wolf', 'Stefan.J.E.Wolf', 160, 20000000, 1, 1000000, 1000000, 1, 1307, 103, 146, 30, 189, 25, 125, 45, 104, 33, 10, 12, 2, 1, 89, 103298709, 300, 1008, 1200, 540, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3483, 'AS_EVIL_SHADOW3', 'Immortal Death Shadow', 'Immortal Death Shadow', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3482, 'AS_EVIL_SHADOW2', 'Immortal Angry Shadow', 'Immortal Angry Shadow', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3481, 'AS_EVIL_SHADOW1', 'Immortal Nightmare Shadow', 'Immortal Nightmare Shadow', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3480, 'AS_CURSED_SOLDIER', 'Immortal Cursed Zombie', 'Immortal Cursed Zombie', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3479, 'AS_ZOMBIE_MASTER', 'Immortal Zombie Assault', 'Immortal Zombie Assault', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3478, 'AS_ZOMBIE_SLAUGHTER', 'Sky Fortress Key Keeper', 'Sky Fortress Key Keeper', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 33566869, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3477, 'AS_IMMORTAL_CORPS', 'Immortal Fortress Legio', 'Immortal Fortress Legio', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3476, 'AS_ZOMBIE', 'Immortal Zombie Soldier', 'Immortal Zombie Soldier', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3475, 'AS_WIND_GHOST', 'Immortal Wind Ghost', 'Immortal Wind Ghost', 160, 100000, 1, 100000, 100000, 2, 182, 95, 64, 51, 62, 27, 25, 55, 102, 20, 10, 12, 1, 6, 64, 102774421, 150, 1056, 1056, 336, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31025, 1),
-(3474, 'AS_BLOODY_KNIGHT', 'Immortal Cursed Knight', 'Immortal Cursed Knight', 160, 1000000, 1, 100000, 100000, 3, 1319, 123, 122, 50, 132, 59, 70, 57, 137, 45, 10, 12, 2, 0, 87, 102774421, 250, 828, 528, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31024, 1),
-(3473, 'AS_RAGGED_GOLEM', 'Stefan.J.E.Wolf', 'Stefan.J.E.Wolf', 160, 20000000, 1, 1000000, 1000000, 1, 1307, 103, 146, 30, 189, 25, 125, 45, 104, 33, 10, 12, 2, 1, 89, 103298709, 300, 1008, 1200, 540, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31026, 1);
+(3473, 'AS_D_RAGGED_GOLEM', 'Stefan J.E Wolf', 'Stefan J.E Wolf', 160, 20000000, 1, 2800090, 2675920, 3, 4500, 6500, 100, 68, 200, 100, 200, 200, 220, 100, 10, 12, 2, 1, 89, 103298709, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 984, 250, 985, 250, 607, 250, 608, 500, 2027, 75, 21018, 75, 28010, 75, 0, 0, 0, 0, 31026, 1),
+(3474, 'AS_BLOODY_KNIGHT', 'Immortal Cursed Knight', 'Immortal Cursed Knight', 160, 7000000, 1, 7513, 8457, 2, 4785, 4977, 138, 53, 102, 104, 72, 57, 98, 57, 10, 12, 1, 0, 87, 102774421, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31024, 1),
+(3475, 'AS_WIND_GHOST', 'Immortal Wind Ghost', 'Immortal Wind Ghost', 160, 7000000, 1, 9086, 17532, 5, 2513, 3315, 105, 41, 100, 63, 35, 99, 106, 61, 10, 12, 1, 6, 64, 102774421, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31025, 1),
+(3476, 'AS_ZOMBIE', 'Immortal Zombie Soldier', 'Immortal Zombie Soldier', 160, 405694, 1, 10570, 10100, 1, 1504, 1625, 128, 84, 5, 99, 39, 48, 30, 24, 10, 12, 1, 1, 29, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3477, 'AS_IMMORTAL_CORPS', 'Immortal Fortress Legion', 'Immortal Fortress Legion', 160, 405694, 1, 11040, 10730, 1, 1851, 2022, 103, 25, 109, 76, 61, 53, 98, 30, 10, 12, 1, 6, 89, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3478, 'AS_ZOMBIE_SLAUGHTER', 'Immortal Fortress Key Keeper', 'Immortal Fortress Key Keeper', 160, 423332, 1, 11040, 10730, 1, 1851, 2022, 103, 25, 109, 76, 61, 53, 98, 30, 10, 12, 1, 1, 69, 33566869, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3479, 'AS_ZOMBIE_MASTER', 'Immortal Zombie Assaulter', 'Immortal Zombie Assaulter', 160, 405694, 1, 11500, 10100, 1, 1653, 3274, 140, 44, 155, 126, 89, 108, 213, 76, 10, 12, 1, 1, 29, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3480, 'AS_CURSED_SOLDIER', 'Immortal Cursed Zombie', 'Immortal Cursed Zombie', 160, 405694, 1, 10101, 10003, 1, 1850, 3451, 81, 87, 119, 87, 86, 60, 94, 55, 10, 12, 1, 1, 69, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3481, 'AS_EVIL_SHADOW1', 'Immortal Nightmare Shadow', 'Immortal Nightmare Shadow', 160, 423330, 1, 6666, 7332, 2, 1444, 1944, 106, 44, 44, 166, 44, 44, 166, 44, 10, 12, 1, 6, 47, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3482, 'AS_EVIL_SHADOW2', 'Immortal Angry Shadow', 'Immortal Angry Shadow', 160, 388054, 1, 279836, 260199, 1, 2730, 3990, 80, 180, 155, 88, 110, 135, 154, 59, 10, 12, 1, 6, 47, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3483, 'AS_EVIL_SHADOW3', 'Immortal Death Shadow', 'Immortal Death Shadow', 160, 423330, 1, 280009, 267592, 1, 3020, 4265, 80, 150, 165, 82, 110, 122, 154, 52, 10, 12, 1, 6, 47, 12437, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3484, 'AS_D_RAGGED_GOLEM', 'Stefan J.E Wolf', 'Stefan J.E Wolf', 160, 5000000, 1, 0, 0, 3, 2800, 3200, 100, 68, 100, 50, 100, 100, 110, 50, 10, 12, 2, 1, 89, 103298709, 500, 1500, 500, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3485, 'AS_D_CURSED_SOLDIER', 'Cursed Soldier of Bijou', 'Cursed Soldier of Bijou', 1, 100000, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 1, 3, 21, 12437, 400, 1872, 672, 480, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 # SkyFortress Mobs Skills
 REPLACE INTO mob_skill_db2_re (`mob_id`, `info`, `state`, `skill_id`, `skill_lv`, `rate`, `casttime`, `delay`, `cancelable`, `target`, `condition`, `val1`, `val2`, `val3`, `val4`, `val5`, `emotion`, `chat`) VALUES
@@ -74,13 +74,13 @@ REPLACE INTO mob_skill_db2_re (`mob_id`, `info`, `state`, `skill_id`, `skill_lv`
 (3483,"ImmortalShadow3@NPC_CRITICALSLASH","attack",170,1,500,500,5000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,6,NULL),
 (3483,"ImmortalShadow3@NPC_BLOODDRAIN","angry",199,1,500,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (3483,"ImmortalShadow3@NPC_BLOODDRAIN","attack",199,1,500,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3484,"Stefan@NPC_POWERUP","attack",349,5,10000,0,120000,"yes","self","myhpltmaxrate",30,NULL,NULL,NULL,NULL,NULL,6,NULL),
-(3484,"Stefan@NPC_DRAGONFEAR","chase",659,5,10000,1000,10000,"no","self","myhpltmaxrate",80,NULL,NULL,NULL,NULL,NULL,32,NULL),
-(3484,"Stefan@NPC_DRAGONFEAR","attack",659,5,10000,1000,10000,"no","self","myhpltmaxrate",80,NULL,NULL,NULL,NULL,NULL,32,NULL),
-(3484,"Stefan@NPC_GUIDEDATTACK","chase",172,5,500,0,20000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3484,"Stefan@AL_HEAL","idle",28,11,10000,0,5000,"yes","self","mystatuson","hiding",NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3484,"Stefan@NPC_CRITICALSLASH","attack",170,1,2000,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3484,"Stefan@NPC_CRITICALSLASH","chase",170,1,2000,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(3484,"Stefan@WZ_METEOR","attack",83,10,2000,500,5000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,21,NULL),
-(3484,"Stefan@WZ_METEOR","chase",83,10,2000,500,5000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,21,NULL),
-(3484,"Stefan@WZ_METEOR","chase",83,10,10000,500,5000,"no","target","myhpltmaxrate",50,NULL,NULL,NULL,NULL,NULL,32,NULL);
+(3473,"Stefan@NPC_POWERUP","attack",349,5,10000,0,120000,"yes","self","myhpltmaxrate",30,NULL,NULL,NULL,NULL,NULL,6,NULL),
+(3473,"Stefan@NPC_DRAGONFEAR","chase",659,5,10000,1000,10000,"no","self","myhpltmaxrate",80,NULL,NULL,NULL,NULL,NULL,32,NULL),
+(3473,"Stefan@NPC_DRAGONFEAR","attack",659,5,10000,1000,10000,"no","self","myhpltmaxrate",80,NULL,NULL,NULL,NULL,NULL,32,NULL),
+(3473,"Stefan@NPC_GUIDEDATTACK","chase",172,5,500,0,20000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3473,"Stefan@AL_HEAL","idle",28,11,10000,0,5000,"yes","self","mystatuson","hiding",NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3473,"Stefan@NPC_CRITICALSLASH","attack",170,1,2000,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3473,"Stefan@NPC_CRITICALSLASH","chase",170,1,2000,0,5000,"yes","target","always",0,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3473,"Stefan@WZ_METEOR","attack",83,10,2000,500,5000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,21,NULL),
+(3473,"Stefan@WZ_METEOR","chase",83,10,2000,500,5000,"no","target","always",0,NULL,NULL,NULL,NULL,NULL,21,NULL),
+(3473,"Stefan@WZ_METEOR","chase",83,10,10000,500,5000,"no","target","myhpltmaxrate",50,NULL,NULL,NULL,NULL,NULL,32,NULL);
