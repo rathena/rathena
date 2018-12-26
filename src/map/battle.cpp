49caf9bea4624cde3c1ebb6ae223cce256605456
violetharmony <45129902+violetharmony@users.noreply.github.com>
2018-12-26 12:19:24 +0700

    Update battle.cpp

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -6351,8 +6351,7 @@ struct Damage battle_calc_magic_attack(struct block_list *src,struct block_list 
 			case CR_GRANDCROSS:
 			case NPC_GRANDDARKNESS: {
 					struct Damage wd = battle_calc_weapon_attack(src,target,skill_id,skill_lv,mflag);
-
-					ad.damage = battle_attr_fix(src, target, wd.damage + ad.damage, s_ele, tstatus->def_ele, tstatus->ele_lv) * (100 + 40 * skill_lv) / 100;
+					ad.damage = battle_attr_fix(src, target, wd.damage + ad.damage, s_ele, tstatus->def_ele, tstatus->ele_lv) * (100 + 40 * skill_lv) / 200;
 					if(src == target) {
 						if(src->type == BL_PC)
 							ad.damage = ad.damage / 2;
