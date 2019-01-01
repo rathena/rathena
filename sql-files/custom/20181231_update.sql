ed47f5f1ebadb52a1edf0436b73216506f0fd157
NubsPixel <hafiz.bistar@gmail.com>
2019-01-01 20:13:14 +0800

    SQL changes -incomplete-

--- sql-files/custom/20181231_update.sql
+++ sql-files/custom/20181231_update.sql
@@ -1,3 +1,34 @@
 REPLACE INTO `item_db2_re` (`id`, `name_english`, `name_japanese`, `type`, `price_buy`, `price_sell`, `weight`, `atk:matk`, `defence`, `range`, `slots`, `equip_jobs`, `equip_upper`, `equip_genders`, `equip_locations`, `weapon_level`, `equip_level`, `refineable`, `view`, `script`, `equip_script`, `unequip_script`) VALUES
-(8507, 'Acidbomb_10_Box_F', 'Acid Bomb 10 Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ACIDBOMB_10_BOX);', NULL, NULL),
-(8508, 'Acidbomb_50_Box_F', 'Acid Bomb 50 Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ACIDBOMB_50_BOX);', NULL, NULL);
+(8503, 'F_10_Food_Box', '+10 Food Box', 18, 20, NULL, 0, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_CASH_FOOD);', NULL, NULL),
+(8507, 'F_Acidbomb_10_Box', 'Acid Bomb Box (10)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ACIDBOMB_10_BOX);', NULL, NULL),
+(8508, 'F_Acidbomb_50_Box', 'Acid Bomb Box (50)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ACIDBOMB_50_BOX);', NULL, NULL),
+(8509, 'F_Poison_Bottle_10_Box', 'Deadly Poison Box (10)', 18, 0, NULL, 0, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_POISON_10_BOX);', NULL, NULL),
+(8510, 'F_Poison_Bottle_50_Box', 'Deadly Poison Box (50)', 18, 0, NULL, 0, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_POISON_50_BOX);', NULL, NULL),
+(8511, 'F_Blacksmith_Blessing_20Box', 'Blacksmith Blessing Box (20)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_BLESSING_20_BOX);', NULL, NULL),
+(14219, 'F_Enriched_Elunium_Box5', 'Enriched Elunium Box (5)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ENELU_5_BOX);', NULL, NULL),
+(14176, 'F_Enriched_Elunium_Box10', 'Enriched Elunium Box (10)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ENELU_10_BOX);', NULL, NULL),
+(14220, 'F_Enriched_Oridecon_Box5', 'Enriched Oridecon Box (5)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ENORI_5_BOX);', NULL, NULL),
+(14177, 'F_Enriched_Oridecon_Box10', 'Enriched Oridecon Box (10)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_ENORI_10_BOX);', NULL, NULL),
+(16262, 'F_HD_Bradium_Box5', 'HD Bradium Box (5)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_HDBRA_5_BOX);', NULL, NULL),
+(16263, 'F_HD_Carnium_Box5', 'HD Carnium Box (5)', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_HDCAR_5_BOX);', NULL, NULL),
+(17246, 'F_HD_Elunium_Box_5', 'HD Elunium Box (5)', 18, 10, NULL, 10, NULL, NULL, NULL, 0, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_HDELU_5_BOX);', NULL, NULL),
+(17247, 'F_HD_Oridecon_Box_5', 'HD Oridecon Box (5)', 18, 10, NULL, 10, NULL, NULL, NULL, 0, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_HDORI_5_BOX);', NULL, NULL),
+(14158, 'F_Bubble_Gum_Box', 'Bubble Gum Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_BG_5_BOX);', NULL, NULL),
+(14168, 'F_Convex_Mirror_Box', 'Convex Mirror Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_CM_5_BOX);', NULL, NULL),
+(14156, 'F_Battle_Manual_Box', 'Battle Manual Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_BM_5_BOX);', NULL, NULL),
+(14178, 'F_Token_Of_Siegfried_Box', 'Token of Siegfried Box', 18, 20, NULL, 10, NULL, NULL, NULL, NULL, 4294967295, 63, 2, NULL, NULL, NULL, NULL, NULL, 'getgroupitem(IG_MILKY_TS_5_BOX);', NULL, NULL),
+(20439, 'C_New_Year_Shine', 'Costume New Year Shine', 4, 0, NULL, 0, NULL, NULL, NULL, 0, 4294967295, 63, 2, 2048, NULL, '1', 0, 0, NULL, 'hateffect HAT_EF_GLOW_OF_NEW_YEAR,true;', 'hateffect HAT_EF_GLOW_OF_NEW_YEAR,false;'),
+(19808, 'C_Blazing_Sun', 'Costume Blazing Sun', 4, 0, NULL, 100, NULL, 0, NULL, 0, 4294967295, 63, 2, 1024, NULL, '1', 0, 654, NULL, NULL, NULL),
+(14499, 'C_Peacock_Feather', 'Costume Peacock Feather', 4, 20, NULL, 100, NULL, 0, NULL, 0, 4294967295, 63, 2, 1024, NULL, '1', 0, 514, NULL, NULL, NULL),
+(31160, 'C_Rune_Helm', 'Costume Rune Helm', 4, 20, NULL, 100, NULL, 0, NULL, 0, 4294967295, 63, 2, 1024, NULL, '1', 0, 1361, NULL, NULL, NULL),
+(31091, 'C_Show_Me_The_Zeny', 'Costume Show Me The Zeny', 4, 0, NULL, 0, NULL, NULL, NULL, 0, 4294967295, 63, 2, 1024, NULL, '1', 0, 0, NULL, 'hateffect HAT_EF_GOLD_SHOWER,true;', 'hateffect HAT_EF_GOLD_SHOWER,false;'),
+(2976, 'Red_Lantern', 'Red Lantern', 4, 10, NULL, 200, NULL, 0, NULL, 1, 4294967295, 63, 2, 136, NULL, '100', 0, NULL, 'bonus bMaxSP,-300; skill \"MG_SIGHT\",1; bonus2 bAddMonsterDropItem,970,25; bonus2 bAddMonsterDropItem,971,25; bonus2 bAddMonsterDropItem,972,25;', NULL, 'sc_end SC_SIGHT;'),
+(19095, 'Happy_Balloon', 'Happy Balloon', 4, 0, NULL, 10, NULL, 0, NULL, 0, 4294967295, 63, 2, 1, NULL, '1', 0, 1289, 'bonus2 bDropAddRace,RC_All,5; bonus2 bExpAddRace,RC_All,10;', NULL, NULL),
+(19530, 'C_Wild_Rose', 'Costume Wild Rose', 4, 0, NULL, 0, NULL, 0, NULL, 0, 4294967295, 63, 2, 1024, NULL, '1', 0, 541, NULL, NULL, NULL);
+
+
+# imp dropping cash blaze huuma shuriken
+REPLACE INTO `mob_db2_re` (`ID`, `Sprite`, `kName`, `iName`, `LV`, `HP`, `SP`, `EXP`, `JEXP`, `Range1`, `ATK1`, `ATK2`, `DEF`, `MDEF`, `STR`, `AGI`, `VIT`, `INT`, `DEX`, `LUK`, `Range2`, `Range3`, `Scale`, `Race`, `Element`, `Mode`, `Speed`, `aDelay`, `aMotion`, `dMotion`, `MEXP`, `MVP1id`, `MVP1per`, `MVP2id`, `MVP2per`, `MVP3id`, `MVP3per`, `Drop1id`, `Drop1per`, `Drop2id`, `Drop2per`, `Drop3id`, `Drop3per`, `Drop4id`, `Drop4per`, `Drop5id`, `Drop5per`, `Drop6id`, `Drop6per`, `Drop7id`, `Drop7per`, `Drop8id`, `Drop8per`, `Drop9id`, `Drop9per`, `DropCardid`, `DropCardper`) VALUES
+(1837, 'IMP', 'Imp', 'Imp', 129, 36830, 1, 4594, 3445, 1, 1239, 406, 56, 75, 66, 83, 65, 88, 72, 27, 10, 12, 0, 6, 63, 33567381, 150, 824, 432, 360, 0, 0, 0, 0, 0, 0, 0, 7122, 3000, 13303, 3, 7098, 2500, 1376, 10, 1972, 25, 12374, 1, 6223, 1, 0, 0, 0, 0, 4433, 1);
+
+
