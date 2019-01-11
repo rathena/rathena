6af1978a3a1e78ef4f22d56cc6620f8282b4f1eb
aleos <aleos89@users.noreply.github.com>
2019-01-11 16:58:07 -0500

    Some small cleanups * Updated some of the cast times. These are assumptions until the actual values are found. * Corrected a potential crash with homunculus experience gain.

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -2631,7 +2631,7 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 				}
 			}
 #ifdef RENEWAL
-			if (base_exp && tmpsd[i] && tmpsd[i]->hd)
+			if (base_exp && tmpsd[i])
 				hom_gainexp(tmpsd[i]->hd, base_exp * battle_config.homunculus_exp_gain / 100); // Homunculus only receive 10% of EXP
 #else
 			if (base_exp && md->dmglog[i].flag == MDLF_HOMUN) //tmpsd[i] is null if it has no homunc.
