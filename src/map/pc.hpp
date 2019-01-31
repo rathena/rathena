97ea08ee851fb814430cc11b2d42a677b481255b
aleos89 <aleos89@users.noreply.github.com>
2019-01-31 13:44:06 -0500

    Added item bonus bNoWalkDelay * Fixes #131 and fixes #2699. * Added item bonus bNoWalkDelay to replace infinite SC_ENDURE bonus in items. * Included some minor cleanups and additions. Thanks to @exneval, @c0nflicts, and @esu1214!

--- src/map/pc.hpp
+++ src/map/pc.hpp
@@ -301,6 +301,7 @@ struct map_session_data {
 		unsigned int no_knockback : 1;
 		unsigned int bonus_coma : 1;
 		unsigned int no_mado_fuel : 1; // Disable Magic_Gear_Fuel consumption [Secret]
+		unsigned int no_walk_delay : 1;
 	} special_state;
 	uint32 login_id1, login_id2;
 	unsigned short class_;	//This is the internal job ID used by the map server to simplify comparisons/queries/etc. [Skotlex]
