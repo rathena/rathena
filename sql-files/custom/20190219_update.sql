588291682c0cc0cabbd75c516e3679b3e644b074
NubsPixel <nubspixel@gmail.com>
2019-02-05 14:27:23 +0800

    Updated guild invite to allow admin to "approve".
    Remove the specials (events) drops and fireworks.
    
    Minor update to multi_pc. not sure if it works or not.

---
+++ sql-files/custom/20190219_update.sql
@@ -0,0 +1,9 @@
+# remove special mission
+DELETE FROM `player_mission` WHERE `mission_id` IN (1548750155, 1548750287, 1548750314, 1548750365, 1548750433);
+DELETE FROM `mission_board` WHERE `npc_id` LIKE '|99|';
+
+# set Golden Thread to be repeatable
+UPDATE `pakkun`.`mission_board` SET `redo_delay`='0' WHERE  `id`=1548750837;
+
+# Alter Guild Invite table.
+ALTER TABLE `guild_invite_members`  ADD `request` TINYINT(1) NOT NULL DEFAULT '0'  AFTER `date_registered`,  ADD `date_requested` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'  AFTER `request`;
