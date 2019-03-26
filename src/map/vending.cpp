6099a9f33573bde615d99027fa2f1046d9fa224e
Atemo <Atemo@users.noreply.github.com>
2019-03-26 16:29:05 +0100

    Revert "Added pcblock and checkpcblock script commands. (#3951)"
    This reverts commit 3e0d3d2fe6b244099d3987b7a21260d6b070512e.

--- src/map/vending.cpp
+++ src/map/vending.cpp
@@ -599,10 +599,7 @@ void do_init_vending_autotrade(void)
 				CREATE(at->sd, struct map_session_data, 1);
 				pc_setnewpc(at->sd, at->account_id, at->char_id, 0, gettick(), at->sex, 0);
 				at->sd->state.autotrade = 1|2;
-				if (battle_config.autotrade_monsterignore)
-					at->sd->state.block_action |= PCBLOCK_IMMUNE;
-				else
-					at->sd->state.block_action &= ~PCBLOCK_IMMUNE;
+				at->sd->state.monster_ignore = (battle_config.autotrade_monsterignore);
 				chrif_authreq(at->sd, true);
 				uidb_put(vending_autotrader_db, at->char_id, at);
 			}
