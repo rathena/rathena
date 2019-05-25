d946552768a8373c5792002c0e4216affc6e1d09
Sader Fawall <sader1992@hotmail.com>
2019-05-25 22:53:11 +0200

    mail_setitem

--- src/map/mail.cpp
+++ src/map/mail.cpp
@@ -146,6 +146,9 @@ enum mail_attach_result mail_setitem(struct map_session_data *sd, short idx, uin
 		if( idx < 0 || idx >= MAX_INVENTORY || sd->inventory_data[idx] == nullptr )
 			return MAIL_ATTACH_ERROR;
 
+		if (sd->inventory.u.items_inventory[idx].card[0] == CARD0_PET && sd->inventory.u.items_inventory[idx].attribute == 1)
+			return MAIL_ATTACH_ERROR;
+
 		if( sd->inventory.u.items_inventory[idx].equipSwitch ){
 			return MAIL_ATTACH_EQUIPSWITCH;
 		}
