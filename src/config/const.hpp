eb4b46255fec237a2fdaad46e8fca7df22cc7c56
norm <Norman.Ziebal@web.de>
2019-05-30 22:22:13 +0200

    Changed renewal start coordinates (#4172)
    * Fixes #4171.
    Thanks to @Badarosk0 and @Normynator!

--- src/config/const.hpp
+++ src/config/const.hpp
@@ -101,8 +101,8 @@
  **/
 #ifdef RENEWAL
     #define MAP_DEFAULT_NAME "iz_int"
-    #define MAP_DEFAULT_X 97
-    #define MAP_DEFAULT_Y 90
+    #define MAP_DEFAULT_X 18
+    #define MAP_DEFAULT_Y 26
 #else
     #define MAP_DEFAULT_NAME "new_1-1"
     #define MAP_DEFAULT_X 53
