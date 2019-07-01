c7ab9e5ca5a416808e4fa13d916525e7e9710aed
aleos89 <aleos89@users.noreply.github.com>
2019-07-01 09:14:52 -0400

    Added config back * Added config back but created a new config for renewal.

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -2435,8 +2435,11 @@ unsigned short status_base_atk(const struct block_list *bl, const struct status_
 {
 	int flag = 0, str, dex, dstr;
 
-#ifndef RENEWAL // Deprecated battle_config.enable_baseatk
-	if (!(bl->type&(BL_PC|BL_HOM)))
+#ifdef RENEWAL
+	if (!(bl->type&battle_config.enable_baseatk_renewal))
+		return 0;
+#else
+	if (!(bl->type&battle_config.enable_baseatk))
 		return 0;
 #endif
 
