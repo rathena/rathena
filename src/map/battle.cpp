22e2927ea89c01e5af7c485ea6e9ad3def65f29e
SonaSonaSona <bestsonabr@gmail.com>
2019-07-02 13:06:13 -0300

    Adding Battle Configuration

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -8486,6 +8486,7 @@ static const struct _battle_data {
 	{ "save_body_style",                    &battle_config.save_body_style,                 0,      0,      1,              },
 	{ "monster_eye_range_bonus",            &battle_config.mob_eye_range_bonus,             0,      0,      10,             },
 	{ "monster_stuck_warning",              &battle_config.mob_stuck_warning,               0,      0,      1,              },
+	{ "greenaura_reduction_rate",           &battle_config.greenaura_reduction_rate,        0,      0,      100,            },
 	{ "skill_eightpath_algorithm",          &battle_config.skill_eightpath_algorithm,       1,      0,      1,              },
 	{ "death_penalty_maxlv",                &battle_config.death_penalty_maxlv,             0,      0,      3,              },
 	{ "exp_cost_redemptio",                 &battle_config.exp_cost_redemptio,              1,      0,      100,            },
