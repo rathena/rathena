774f7bf8ffdf57b4373cb7f03e4a2452328bfd5a
aleos89 <aleos89@users.noreply.github.com>
2019-07-22 08:09:40 -0400

    Corrected another potential crash with Joint Beat * Follow up to cb2c553. Thanks to @teededung!

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -4773,7 +4773,7 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 
 	case LK_JOINTBEAT:
 		flag = 1 << rnd() % 6;
-		if (flag != BREAK_NECK && tsc && tsc->data[SC_JOINTBEAT]->val2 & BREAK_NECK)
+		if (flag != BREAK_NECK && tsc && tsc->data[SC_JOINTBEAT] && tsc->data[SC_JOINTBEAT]->val2 & BREAK_NECK)
 			flag = BREAK_NECK; // Target should always receive double damage if neck is already broken
 		if (skill_attack(BF_WEAPON, src, src, bl, skill_id, skill_lv, tick, flag))
 			sc_start4(src, bl, SC_JOINTBEAT, 50 + (skill_lv + 1), skill_lv, flag&BREAK_FLAGS, src->id, 0, skill_get_time2(skill_id, skill_lv));
