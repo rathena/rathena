fb42177ac63b7f2cd84526a7f798608a2c3bc128
Jittapan Pluemsumran <secret@jittapan.app>
2019-09-17 14:48:29 +0700

    Added multi-hit critical damage display support (#2982)
    * Fixed #1788.
    * Double Attack and Fear Breeze can now critically strike.

--- src/map/clif.hpp
+++ src/map/clif.hpp
@@ -547,6 +547,7 @@ enum e_damage_type : uint8_t {
 	DMG_CRITICAL,			/// critical hit
 	DMG_LUCY_DODGE,			/// lucky dodge
 	DMG_TOUCH,				/// (touch skill?)
+	DMG_MULTI_HIT_CRITICAL  /// multi-hit with critical
 };
 
 enum class e_pet_evolution_result : uint32 {
