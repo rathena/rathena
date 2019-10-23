efe6830ba941976d46b53237a36a9b2afe8c356e
attackjom <35770095+attackjom@users.noreply.github.com>
2019-10-23 15:14:22 +0700

    fix skillusesprate

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -16249,7 +16249,7 @@ struct skill_condition skill_get_requirement(struct map_session_data* sd, uint16
 
 	for (auto &it : sd->skillusesprate) {
 		if (it.id == skill_id) {
-			sp_skill_rate_bonus += it.val;
+			sp_skill_rate_bonus -= it.val;
 			break;
 		}
 	}
