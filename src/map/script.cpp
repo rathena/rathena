011f010ecae3edd3e0ac9be540e9a03e996aa072
Daegaladh <daegaladh@shadowgames.es>
2019-12-17 20:57:40 +0100

    Added support for bounded items

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -24057,7 +24057,7 @@ static inline bool mail_sub( struct script_state *st, struct script_data *data, 
 	return true;
 }
 
-// mail <destination id>,"<sender name>","<title>","<body>"{,<zeny>{,<item id array>,<item amount array>{,refine{,<item card0 array>{,<item card1 array>{,<item card2 array>{,<item card3 array>{, <random option id0 array>, <random option value0 array>, <random option paramter0 array>{, <random option id1 array>, <random option value1 array>, <random option paramter1 array>{, <random option id2 array>, <random option value2 array>, <random option paramter2 array>{, <random option id3 array>, <random option value3 array>, <random option paramter3 array>{, <random option id4 array>, <random option value4 array>, <random option paramter4 array>}}}}}}}};
+// mail <destination id>,"<sender name>","<title>","<body>"{,<zeny>{,<item id array>,<item amount array>{,refine{,bound{,<item card0 array>{,<item card1 array>{,<item card2 array>{,<item card3 array>{,<random option id0 array>, <random option value0 array>, <random option paramter0 array>{,<random option id1 array>, <random option value1 array>, <random option paramter1 array>{,<random option id2 array>, <random option value2 array>, <random option paramter2 array>{,<random option id3 array>, <random option value3 array>, <random option paramter3 array>{,<random option id4 array>, <random option value4 array>, <random option paramter4 array>}}}}}}}}};
 BUILDIN_FUNC(mail){
 	const char *sender, *title, *body, *name;
 	struct mail_message msg;
@@ -24210,12 +24210,37 @@ BUILDIN_FUNC(mail){
 				msg.item[i].refine = 0;
 		}
 
-		// Cards
-		if( !script_hasdata(st,10) ){
+		// Bound
+		if (!script_hasdata(st, 10)) {
+			ShowError("buildin_mail: missing item bound variable at position %d.\n", 10);
 			break;
 		}
 
-		for( i = 0, j = 10; i < MAX_SLOTS && script_hasdata(st,j); i++, j++ ){
+		data = script_getdata(st,10);
+
+		if( !mail_sub( st, data, sd, 10, &name, &start, &end, &id ) ){
+			return SCRIPT_CMD_FAILURE;
+		}
+
+		for( i = 0; i < num_items && start < end; i++, start++ ){
+			struct item_data *item = itemdb_exists(msg.item[i].nameid);
+
+			msg.item[i].bound = (int32)__64BPRTSIZE( get_val2( st, reference_uid( id, start ), reference_getref( data ) ) );
+
+			script_removetop(st, -1, 0);
+
+			if( msg.item[i].bound <= BOUND_NONE || msg.item[i].bound >= BOUND_MAX ){
+				ShowError( "buildin_mail: bound %d for item %hu is invalid.\n", msg.item[i].bound, msg.item[i].nameid );
+				return SCRIPT_CMD_FAILURE;
+			}
+		}
+
+		// Cards
+		if( !script_hasdata(st,11) ){
+			break;
+		}
+
+		for( i = 0, j = 11; i < MAX_SLOTS && script_hasdata(st,j); i++, j++ ){
 			data = script_getdata(st,j);
 
 			if( !mail_sub( st, data, sd, j + 1, &name, &start, &end, &id ) ){
@@ -24235,11 +24260,11 @@ BUILDIN_FUNC(mail){
 		}
 	
 		// Random Options
-		if( !script_hasdata(st,10 + MAX_SLOTS) ){
+		if( !script_hasdata(st,12 + MAX_SLOTS) ){
 			break;
 		}
 
-		for( i = 0, j = 10 + MAX_SLOTS; i < MAX_ITEM_RDM_OPT && script_hasdata(st,j) && script_hasdata(st,j + 1) && script_hasdata(st,j + 2); i++, j++ ){
+		for( i = 0, j = 12 + MAX_SLOTS; i < MAX_ITEM_RDM_OPT && script_hasdata(st,j) && script_hasdata(st,j + 1) && script_hasdata(st,j + 2); i++, j++ ){
 			// Option IDs
 			data = script_getdata(st, j);
 
