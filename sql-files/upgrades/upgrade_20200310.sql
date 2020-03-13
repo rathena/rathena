c6c914c60e3d67e705c463ffbfbf4f1dce9b40f3
atemo <capucrath@gmail.com>
2020-03-13 02:39:34 +0100

    Added a sql update to replace the main quest variable

---
+++ sql-files/upgrades/upgrade_20200310.sql
@@ -0,0 +1,4 @@
+UPDATE `char_reg_num` SET `key` = 'ep16_royal' WHERE `key` = 'banquet_main_quest' AND `value` < 16;
+UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 1 WHERE `key` = 'banquet_main_quest' AND `value` < 20;
+UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 2 WHERE `key` = 'banquet_main_quest' AND `value` < 26;
+UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = 25 WHERE `key` = 'banquet_main_quest' AND `value` > 25;
