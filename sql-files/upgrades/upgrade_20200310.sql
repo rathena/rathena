f3f132ac8268c5492c6575afc80f8020051d33bf
atemo <capucrath@gmail.com>
2020-03-13 04:22:42 +0100

    Corrected Nerius part

--- sql-files/upgrades/upgrade_20200310.sql
+++ sql-files/upgrades/upgrade_20200310.sql
@@ -2,3 +2,5 @@ UPDATE `char_reg_num` SET `key` = 'ep16_royal' WHERE `key` = 'banquet_main_quest
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 1 WHERE `key` = 'banquet_main_quest' AND `value` < 20;
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 2 WHERE `key` = 'banquet_main_quest' AND `value` < 26;
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = 25 WHERE `key` = 'banquet_main_quest' AND `value` > 25;
+
+DELETE FROM `char_reg_num` WHERE `key` = 'banquet_nerius_quest';
