7200ce5007dc78d740e27defed8c0e697bb3a9ae
atemo <capucrath@gmail.com>
2020-03-13 04:32:03 +0100

    Corrected Heine part

--- sql-files/upgrades/upgrade_20200310.sql
+++ sql-files/upgrades/upgrade_20200310.sql
@@ -4,3 +4,4 @@ UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 2 WHERE `key
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = 25 WHERE `key` = 'banquet_main_quest' AND `value` > 25;
 
 DELETE FROM `char_reg_num` WHERE `key` = 'banquet_nerius_quest';
+DELETE FROM `char_reg_num` WHERE `key` = 'banquet_heine_quest';
