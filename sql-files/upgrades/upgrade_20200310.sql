e30f419d1307e7d3ee7447a700a01f02b19cb438
atemo <capucrath@gmail.com>
2020-03-21 16:32:16 +0100

    Small corrections

--- sql-files/upgrades/upgrade_20200310.sql
+++ sql-files/upgrades/upgrade_20200310.sql
@@ -25,3 +25,14 @@ WHERE `key` = 'banquet_wigner_quest' AND `value` = 5 AND `quest_id` = 14481 AND 
 UPDATE `char_reg_num` SET `key` = 'ep16_wig', `value` = 6 FROM `char_reg_num` INNER JOIN `quest` ON `char_reg_num`.`char_id` = `quest`.`char_id`
 WHERE `key` = 'banquet_wigner_quest' AND `value` = 5 AND `quest_id` = 14481 AND `state` = 1;
 UPDATE `char_reg_num` SET `key` = 'ep16_wig' WHERE `key` = 'banquet_wigner_quest' AND `value` = 5;
+
+UPDATE `char_reg_num` SET `key` = 'ep16_cookbs' WHERE `key` = 'banquet_quest_cooking' AND `value` < 3;
+UPDATE `char_reg_num` SET `key` = 'ep16_cookbs', `value` = 2 WHERE `key` = 'banquet_quest_cooking' AND `value` = 3;
+UPDATE `char_reg_num` SET `key` = 'ep16_cookbs', `value` = `value` + 8 WHERE `key` = 'banquet_quest_cooking';
+
+DELETE FROM `quest` WHERE `quest_id` = 11428;
+DELETE FROM `quest` WHERE `quest_id` = 11429;
+DELETE FROM `quest` WHERE `quest_id` = 11430;
+DELETE FROM `quest` WHERE `quest_id` = 11431;
+
+DELETE FROM `char_reg_num` WHERE `key` = 'banquet_quest_sauce';
