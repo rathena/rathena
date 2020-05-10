8d2c26d115f0c44fb2d53efb5409c43ad0103946
Yashim Wong <yashimwong@gmail.com>
2020-05-10 14:49:46 +0800

    Removed sonic blow attack animation (#4926)

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -2270,9 +2270,7 @@ int skill_additional_effect(struct block_list* src, struct block_list *bl, uint1
 				!battle_check_range(bl, tbl, skill_get_range2(src, skill, autospl_skill_lv, true)))
 				continue;
 
-			if (skill == AS_SONICBLOW)
-				pc_stop_attack(sd); //Special case, Sonic Blow autospell should stop the player attacking.
-			else if (skill == PF_SPIDERWEB) //Special case, due to its nature of coding.
+			if (skill == PF_SPIDERWEB) //Special case, due to its nature of coding.
 				type = CAST_GROUND;
 
 			sd->state.autocast = 1;
