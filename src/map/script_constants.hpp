fed2a537cf87231ed030ea5892996953918b3872
Lemongrass3110 <lemongrass@kstp.at>
2020-06-04 08:46:06 +0200

    Added support for skill scale display
    Fixes #4530
    
    Thanks to @Balferian

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -7527,6 +7527,7 @@
 	export_constant(INF2_IGNOREWUGBITE);
 	export_constant(INF2_IGNOREAUTOGUARD);
 	export_constant(INF2_IGNORECICADA);
+	export_constant(INF2_SHOWSCALE);
 
 	/* skill no near npc flags */
 	export_constant(SKILL_NONEAR_WARPPORTAL);
