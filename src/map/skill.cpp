a0f8942cf46f81c337fae75f2a70ad98e8918e66
flamefury <some_guy_unknown2u@hotmail.com>
2020-06-08 09:58:07 -0700

    Use only one sc_start

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -7665,11 +7665,12 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 	case PR_MAGNIFICAT:
 	case PR_GLORIA:
 		if (sd == NULL || sd->status.party_id == 0 || (flag & 1)) {
+
+			// Animations don't play when outside visible range
 			if (check_distance_bl(src, bl, AREA_SIZE))
-				clif_skill_nodamage(bl, bl, skill_id, skill_lv, sc_start(src, bl, type, 100, skill_lv, skill_get_time(skill_id, skill_lv)));
-			else
-				// Animations don't play when outside visible range
-				sc_start(src, bl, type, 100, skill_lv, skill_get_time(skill_id, skill_lv));
+				clif_skill_nodamage(bl, bl, skill_id, skill_lv, 1);
+
+			sc_start(src, bl, type, 100, skill_lv, skill_get_time(skill_id, skill_lv));
 		}
 		else if (sd)
 			party_foreachsamemap(skill_area_sub, sd, skill_get_splash(skill_id, skill_lv), src, skill_id, skill_lv, tick, flag | BCT_PARTY | 1, skill_castend_nodamage_id);
