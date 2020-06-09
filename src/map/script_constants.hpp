152b4bd6a349c5c499a9603624ff50f0137f090b
rAthenaAPI <administrator@rathena.org>
2020-06-09 06:00:30 +0200

    Mob and NPC Identity Update

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -6106,6 +6106,17 @@
 	export_constant_npc(JT_4_EP17_MERMAID);
 	export_constant_npc(JT_4_JP_AB_NPC_009);
 	export_constant_npc(JT_4_JP_AB_NPC_010);
+	export_constant_npc(JT_4_4JOB_SILLA);
+	export_constant_npc(JT_4_4JOB_MAGGI);
+	export_constant_npc(JT_4_4JOB_ROBIN);
+	export_constant_npc(JT_4_4JOB_ROBIN_DRUNK);
+	export_constant_npc(JT_4_4JOB_LETICIA);
+	export_constant_npc(JT_4_4JOB_SERANG);
+	export_constant_npc(JT_4_4JOB_EINHAR);
+	export_constant_npc(JT_4_4JOB_SEALSTONE);
+	export_constant_npc(JT_4_4JOB_PHANTOMBOOK1);
+	export_constant_npc(JT_4_4JOB_PHANTOMBOOK2);
+	export_constant_npc(JT_4_4JOB_PHANTOMBOOK3);
 	export_constant_npc(JT_NEW_NPC_3RD_END);
 	#undef export_constant_npc
 
