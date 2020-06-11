824575185264df81975111f3c9a5d180522a9889
aleos <aleos89@users.noreply.github.com>
2020-06-11 13:50:17 -0400

    Fixes Rolling Cutter counter not resetting * Fixes #5052. * Fixes Rolling Cutter counter not resetting with blown-type attacks/movements.

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -2881,6 +2881,8 @@ short skill_blown(struct block_list* src, struct block_list* target, char count,
 	if (tsc) {
 		if (tsc->data[SC_SU_STOOP]) // Any knockback will cancel it.
 			status_change_end(target, SC_SU_STOOP, INVALID_TIMER);
+		if (tsc->data[SC_ROLLINGCUTTER])
+			status_change_end(target, SC_ROLLINGCUTTER, INVALID_TIMER);
 		if (tsc->data[SC_SV_ROOTTWIST]) // Shouldn't move.
 			return 0;
 	}
