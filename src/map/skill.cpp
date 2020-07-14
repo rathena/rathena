d687c66127fd787f00ed3ae114947c7faf2252c8
aleos <aleos89@users.noreply.github.com>
2020-07-14 14:23:52 -0400

    Fixes GD_EMERGENCY_MOVE to be usable * Skill should be usable and not passive. * Skill should only be usable in GvG maps.

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -15926,6 +15926,7 @@ bool skill_check_condition_castbegin(struct map_session_data* sd, uint16 skill_i
 		case GD_RESTORE:
 		case GD_CHARGESHOUT_FLAG:
 		case GD_CHARGESHOUT_BEATING:
+		case GD_EMERGENCY_MOVE:
 			if (!map_flag_gvg2(sd->bl.m)) {
 				clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
 				return false;
