fafa888bde0b873b7d03e71f16e551025f615434
aleos <aleos89@users.noreply.github.com>
2020-07-21 12:26:28 -0400

    Use defaults

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -1748,7 +1748,7 @@ static bool mob_ai_sub_hard(struct mob_data *md, t_tick tick)
 				)
 			&&  md->state.attacked_count++ >= RUDE_ATTACKED_COUNT
 			&&  !mobskill_use(md, tick, MSC_RUDEATTACKED) // If can't rude Attack
-			&&  can_move && unit_escape(&md->bl, tbl, rnd()%10 +1, 0)) // Attempt escape
+			&&  can_move && unit_escape(&md->bl, tbl, rnd()%10 +1)) // Attempt escape
 			{	//Escaped
 				md->attacked_id = md->norm_attacked_id = 0;
 				return true;
@@ -1776,7 +1776,7 @@ static bool mob_ai_sub_hard(struct mob_data *md, t_tick tick)
 				if (abl->id != md->bl.id //Self damage does not cause rude attack
 				&& md->state.attacked_count++ >= RUDE_ATTACKED_COUNT				
 				&& !mobskill_use(md, tick, MSC_RUDEATTACKED) && can_move
-				&& !tbl && unit_escape(&md->bl, abl, rnd()%10 +1, 0))
+				&& !tbl && unit_escape(&md->bl, abl, rnd()%10 +1))
 				{	//Escaped.
 					//TODO: Maybe it shouldn't attempt to run if it has another, valid target?
 					md->attacked_id = md->norm_attacked_id = 0;
