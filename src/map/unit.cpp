476036c1cb2d8c9b13a04110c9490b4d66d6c34b
aleos <aleos89@users.noreply.github.com>
2020-07-21 12:28:43 -0400

    Adds a safety check

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -1550,7 +1550,7 @@ int unit_set_walkdelay(struct block_list *bl, t_tick tick, t_tick delay, int typ
 			if (bl->type == BL_MOB) {
 				mob_data *md = BL_CAST(BL_MOB, bl);
 
-				if (md->state.alchemist == 1) // Sphere Mine needs to escape, don't stop it
+				if (md && md->state.alchemist == 1) // Sphere Mine needs to escape, don't stop it
 					return 0;
 			}
 			unit_stop_walking(bl,4); //Unit might still be moving even though it can't move
