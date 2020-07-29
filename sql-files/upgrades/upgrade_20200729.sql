08a6596872d3a02fb3f44b8b3b5c7922133a4b5f
atemo <capucrath@gmail.com>
2020-07-29 17:46:58 +0200

    ep 15 updates - updated Krotzel's Request quest

--- sql-files/upgrades/upgrade_20200729.sql
+++ sql-files/upgrades/upgrade_20200729.sql
@@ -8,3 +8,5 @@ UPDATE `char_reg_num` SET `key` = 'ep15_1_elb' WHERE `key` = 'VER_ELEVATOR' `val
 UPDATE `char_reg_num` SET `key` = 'ep15_1_elb', `value` = `value` - 94 WHERE `key` = 'VER_ELEVATOR' AND `value` >= 100 AND `value` < 1000;
 -- Vestige quest
 UPDATE `char_reg_num` SET `key` = 'ep15_2_bslast', `value` = `value` - 999 WHERE `key` = 'VER_ELEVATOR' AND `value` >= 1000;
+-- Krotzel's Request quests
+UPDATE `char_reg_num` SET `key` = 'ep15_2_brigan' WHERE `key` = 'VER_REPORTER';
