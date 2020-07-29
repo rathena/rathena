388e87ce60c3122a62bbed71b42db6f5a9b0b866
atemo <capucrath@gmail.com>
2020-07-29 16:40:52 +0200

    15.1 updates using aegis files - Police quests - Wandering Bard Quest

---
+++ sql-files/upgrades/upgrade_20200729.sql
@@ -0,0 +1,5 @@
+-- Verus police quests
+UPDATE `char_reg_num` SET `key` = 'ep15_1_mang' WHERE `key` = 'trap_doom_prayers';
+UPDATE `char_reg_num` SET `key` = 'ep15_1_mang' WHERE `key` = 'count_stone_seiden';
+-- Verus Wandering Bard Quest
+DELETE FROM `char_reg_num` WHERE `key` = 'wandering_bard_quest';
