2b97aec44f25607017c757e86e911e1e967e223f
atemo <capucrath@gmail.com>
2020-07-30 16:52:33 +0200

    Updated memory quest

--- sql-files/upgrades/upgrade_20200729.sql
+++ sql-files/upgrades/upgrade_20200729.sql
@@ -16,3 +16,6 @@ UPDATE `char_reg_num` SET `key` = 'ep15_1_atnad' WHERE `key` = 'VER_MAIN' AND `v
 UPDATE `char_reg_num` SET `key` = 'ep15_1_atnad', `value` = 30 WHERE `key` = 'VER_MAIN' AND `value` > 30 AND `value` < 37 AND `index` = 0;
 UPDATE `char_reg_num` SET `key` = 'ep15_1_atnad', `value` = `value` - 6 WHERE `key` = 'VER_MAIN' AND `value` > 36 AND `index` = 0;
 DELETE FROM `char_reg_num` WHERE `key` = 'VERUS_DAILY_QUEST' AND `index` = 0;
+-- Memory quest
+DELETE FROM `char_reg_num` WHERE `key` = 'recorder_quest_type' AND `index` = 0;
+DELETE FROM `char_reg_num` WHERE `key` = 'recorder_quest_status' AND `index` = 0;
