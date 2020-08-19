2995ccb90cc1de2720e0eac75ae66ad60f7011af
aleos <aleos89@users.noreply.github.com>
2020-08-19 08:51:14 -0400

    Corrects SQL syntax

--- sql-files/upgrades/upgrade_20200817.sql
+++ sql-files/upgrades/upgrade_20200817.sql
@@ -1,2 +1,2 @@
-DELETE FROM `skill` s WHERE EXISTS (SELECT * FROM `char` c WHERE `c`.char_id == `s`.char_id AND (`c`.class == 4218 OR `c`.class == 4220) AND `c`.job_level > 60);
+DELETE s FROM `skill` s, `char` c WHERE `s`.char_id = `c`.char_id AND (`c`.class = 4218 OR `c`.class = 4220) AND `c`.job_level > 60;
 UPDATE `char` c SET `c`.job_level = 60, `c`.skill_point = 59 WHERE (`c`.class == 4218 OR `c`.class == 4220) AND `c`.job_level > 60;
