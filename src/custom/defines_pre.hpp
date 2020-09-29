5f5bc5f85a0cf7a11a3496a8ee8eacc62190870a
inhyositsu <inhyositsu@gmail.com>
2020-09-29 16:19:29 +0800

    F

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -9,8 +9,6 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 
-#define PACKET_OBFUSCATION_KEY1 0x12f20e15
-#define PACKET_OBFUSCATION_KEY2 0x22a3c827
-#define PACKET_OBFUSCATION_KEY3 0x1234d0e3
+#define PACKETVER 20180620
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
