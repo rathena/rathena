c47fadd1b5918282a787aed49d12fa624ce71d9c
Aleos <aleos89@users.noreply.github.com>
2020-09-30 09:09:46 -0400

    Resolves some LGTM warnings

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -1559,9 +1559,9 @@ ACMD_FUNC(baselevelup)
 		status_calc_pc(sd, SCO_FORCE);
 		status_percent_heal(&sd->bl, 100, 100);
 		clif_misceffect(&sd->bl, 0);
-		for (uint16 i = sd->status.base_level - level; i <= sd->status.base_level; i++) {
-			achievement_update_objective(sd, AG_GOAL_LEVEL, 1, i);
-			achievement_update_objective(sd, AG_GOAL_STATUS, 2, i, sd->status.class_);
+		for (uint32 j = sd->status.base_level - level; j <= sd->status.base_level; j++) {
+			achievement_update_objective(sd, AG_GOAL_LEVEL, 1, j);
+			achievement_update_objective(sd, AG_GOAL_STATUS, 2, j, sd->status.class_);
 		}
 		clif_displaymessage(fd, msg_txt(sd,21)); // Base level raised.
 	} else {
@@ -1624,7 +1624,7 @@ ACMD_FUNC(joblevelup)
 		sd->status.job_level += (unsigned int)level;
 		sd->status.skill_point += level;
 		clif_misceffect(&sd->bl, 1);
-		for (uint16 i = sd->status.job_level - level; i <= sd->status.job_level; i++)
+		for (uint32 i = sd->status.job_level - level; i <= sd->status.job_level; i++)
 			achievement_update_objective(sd, AG_GOAL_LEVEL, 1, i);
 		clif_displaymessage(fd, msg_txt(sd,24)); // Job level raised.
 	} else {
