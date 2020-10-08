cb5b9aba60d55697466e643f8da1bc02326f48b1
aleos <aleos89@users.noreply.github.com>
2020-10-08 10:47:38 -0400

    Fixes Stellar Mark causing a map crash * Fixes #5429. * Follow up to 6ba869e. * Fixes a potential crash from Stellar Mark. Thanks to @ecdarreola!

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -6285,7 +6285,7 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 				// Tag the target only if damage was done. If it deals no damage, it counts as a miss and won't tag.
 				// Note: Not sure if it works like this in official but you can't mark on something you can't
 				// hit, right? For now well just use this logic until we can get a confirm on if it does this or not. [Rytech]
-				if (tmd->status.hp > 0) { // Add the ID of the tagged target to the player's tag list and start the status on the target.
+				if (tmd && tmd->status.hp > 0) { // Add the ID of the tagged target to the player's tag list and start the status on the target.
 					sd->stellar_mark[i] = bl->id;
 
 					// Val4 flags if the status was applied by a player or a monster.
