7c6070c85ac1bdf7237afc96ce05da1e3b5c9e04
aleos <aleos89@users.noreply.github.com>
2020-10-24 12:25:33 -0400

    Fixes a LGTM alert

--- src/map/itemdb.cpp
+++ src/map/itemdb.cpp
@@ -2239,7 +2239,7 @@ static int itemdb_read_sqldb(void) {
 		// free the query result
 		Sql_FreeResult(mmysql_handle);
 
-		ShowStatus("Done reading '" CL_WHITE "%u" CL_RESET "' entries in '" CL_WHITE "%s" CL_RESET "'.\n", count, item_db_name[fi]);
+		ShowStatus("Done reading '" CL_WHITE "%" PRIu64 CL_RESET "' entries in '" CL_WHITE "%s" CL_RESET "'.\n", count, item_db_name[fi]);
 	}
 
 	return 0;
