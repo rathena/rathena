1f3786275610c2d2842686e2b0fa45d8edf3b590
atemo <capucrath@gmail.com>
2020-10-28 14:55:17 +0100

    Updated SQL table

--- sql-files/item_db_re_etc.sql
+++ sql-files/item_db_re_etc.sql
@@ -1,6 +1,119 @@
 #
-# Table data for table `item_db_re`
+# Table structure for table `item_db_re`
 #
+
+DROP TABLE IF EXISTS `item_db_re`;
+CREATE TABLE `item_db_re` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `name_aegis` varchar(50) NOT NULL DEFAULT '',
+  `name_english` varchar(50) NOT NULL DEFAULT '',
+  `type` varchar(20) DEFAULT NULL,
+  `subtype` varchar(20) DEFAULT NULL,
+  `price_buy` mediumint(8) unsigned DEFAULT NULL,
+  `price_sell` mediumint(8) unsigned DEFAULT NULL,
+  `weight` smallint(5) unsigned DEFAULT NULL,
+  `attack` smallint(5) unsigned DEFAULT NULL,
+  `magic_attack` smallint(5) unsigned DEFAULT NULL,
+  `defense` smallint(5) unsigned DEFAULT NULL,
+  `range` tinyint(2) unsigned DEFAULT NULL,
+  `slots` tinyint(2) unsigned DEFAULT NULL,
+  `job_all` tinyint(1) unsigned DEFAULT NULL,
+  `job_acolyte` tinyint(1) unsigned DEFAULT NULL,
+  `job_alchemist` tinyint(1) unsigned DEFAULT NULL,
+  `job_archer` tinyint(1) unsigned DEFAULT NULL,
+  `job_assassin` tinyint(1) unsigned DEFAULT NULL,
+  `job_barddancer` tinyint(1) unsigned DEFAULT NULL,
+  `job_blacksmith` tinyint(1) unsigned DEFAULT NULL,
+  `job_crusader` tinyint(1) unsigned DEFAULT NULL,
+  `job_gunslinger` tinyint(1) unsigned DEFAULT NULL,
+  `job_hunter` tinyint(1) unsigned DEFAULT NULL,
+  `job_kagerouoboro` tinyint(1) unsigned DEFAULT NULL,
+  `job_knight` tinyint(1) unsigned DEFAULT NULL,
+  `job_mage` tinyint(1) unsigned DEFAULT NULL,
+  `job_merchant` tinyint(1) unsigned DEFAULT NULL,
+  `job_monk` tinyint(1) unsigned DEFAULT NULL,
+  `job_ninja` tinyint(1) unsigned DEFAULT NULL,
+  `job_novice` tinyint(1) unsigned DEFAULT NULL,
+  `job_priest` tinyint(1) unsigned DEFAULT NULL,
+  `job_rebellion` tinyint(1) unsigned DEFAULT NULL,
+  `job_rogue` tinyint(1) unsigned DEFAULT NULL,
+  `job_sage` tinyint(1) unsigned DEFAULT NULL,
+  `job_soullinker` tinyint(1) unsigned DEFAULT NULL,
+  `job_stargladiator` tinyint(1) unsigned DEFAULT NULL,
+  `job_summoner` tinyint(1) unsigned DEFAULT NULL,
+  `job_supernovice` tinyint(1) unsigned DEFAULT NULL,
+  `job_swordman` tinyint(1) unsigned DEFAULT NULL,
+  `job_taekwon` tinyint(1) unsigned DEFAULT NULL,
+  `job_thief` tinyint(1) unsigned DEFAULT NULL,
+  `job_wizard` tinyint(1) unsigned DEFAULT NULL,
+  `class_all` tinyint(1) unsigned DEFAULT NULL,
+  `class_normal` tinyint(1) unsigned DEFAULT NULL,
+  `class_upper` tinyint(1) unsigned DEFAULT NULL,
+  `class_baby` tinyint(1) unsigned DEFAULT NULL,
+  `class_third` tinyint(1) unsigned DEFAULT NULL,
+  `class_third_upper` tinyint(1) unsigned DEFAULT NULL,
+  `class_third_baby` tinyint(1) unsigned DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `location_head_top` tinyint(1) unsigned DEFAULT NULL,
+  `location_head_mid` tinyint(1) unsigned DEFAULT NULL,
+  `location_head_low` tinyint(1) unsigned DEFAULT NULL,
+  `location_armor` tinyint(1) unsigned DEFAULT NULL,
+  `location_right_hand` tinyint(1) unsigned DEFAULT NULL,
+  `location_left_hand` tinyint(1) unsigned DEFAULT NULL,
+  `location_garment` tinyint(1) unsigned DEFAULT NULL,
+  `location_shoes` tinyint(1) unsigned DEFAULT NULL,
+  `location_right_accessory` tinyint(1) unsigned DEFAULT NULL,
+  `location_left_accessory` tinyint(1) unsigned DEFAULT NULL,
+  `location_costume_head_top` tinyint(1) unsigned DEFAULT NULL,
+  `location_costume_head_mid` tinyint(1) unsigned DEFAULT NULL,
+  `location_costume_head_low` tinyint(1) unsigned DEFAULT NULL,
+  `location_costume_garment` tinyint(1) unsigned DEFAULT NULL,
+  `location_ammo` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_armor` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_weapon` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_shield` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_shoes` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_right_accessory` tinyint(1) unsigned DEFAULT NULL,
+  `location_shadow_left_accessory` tinyint(1) unsigned DEFAULT NULL,
+  `weapon_level` tinyint(1) unsigned DEFAULT NULL,
+  `equip_level_min` tinyint(3) unsigned DEFAULT NULL,
+  `equip_level_max` tinyint(3) unsigned DEFAULT NULL,
+  `refineable` tinyint(1) unsigned DEFAULT NULL,
+  `view` smallint(5) unsigned DEFAULT NULL,
+  `alias_name` varchar(50) DEFAULT NULL,
+  `flag_buyingstore` tinyint(1) unsigned DEFAULT NULL,
+  `flag_deadbranch` tinyint(1) unsigned DEFAULT NULL,
+  `flag_container` tinyint(1) unsigned DEFAULT NULL,
+  `flag_uniqueid` tinyint(1) unsigned DEFAULT NULL,
+  `flag_bindonequip` tinyint(1) unsigned DEFAULT NULL,
+  `flag_dropannounce` tinyint(1) unsigned DEFAULT NULL,
+  `flag_noconsume` tinyint(1) unsigned DEFAULT NULL,
+  `flag_dropeffect` varchar(20) DEFAULT NULL,
+  `delay_duration` bigint(20) unsigned DEFAULT NULL,
+  `delay_status` varchar(30) DEFAULT NULL,
+  `stack_amount` smallint(5) unsigned DEFAULT NULL,
+  `stack_inventory` tinyint(1) unsigned DEFAULT NULL,
+  `stack_cart` tinyint(1) unsigned DEFAULT NULL,
+  `stack_storage` tinyint(1) unsigned DEFAULT NULL,
+  `stack_guildstorage` tinyint(1) unsigned DEFAULT NULL,
+  `nouse_override` smallint(5) unsigned DEFAULT NULL,
+  `nouse_sitting` tinyint(1) unsigned DEFAULT NULL,
+  `trade_override` smallint(5) unsigned DEFAULT NULL,
+  `trade_nodrop` tinyint(1) unsigned DEFAULT NULL,
+  `trade_notrade` tinyint(1) unsigned DEFAULT NULL,
+  `trade_tradepartner` tinyint(1) unsigned DEFAULT NULL,
+  `trade_nosell` tinyint(1) unsigned DEFAULT NULL,
+  `trade_nocart` tinyint(1) unsigned DEFAULT NULL,
+  `trade_nostorage` tinyint(1) unsigned DEFAULT NULL,
+  `trade_noguildstorage` tinyint(1) unsigned DEFAULT NULL,
+  `trade_nomail` tinyint(1) unsigned DEFAULT NULL,
+  `trade_noauction` tinyint(1) unsigned DEFAULT NULL,
+  `script` text,
+  `equip_script` text,
+  `unequip_script` text,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `UniqueAegisName` (`name_aegis`)
+) ENGINE=MyISAM;
 
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`) VALUES (670,'Gold_Coin_Moneybag','Bag of Gold Coins','Etc',100000,400);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`) VALUES (672,'Copper_Coin_Moneybag','Bag of Bronze Coins','Etc',1000,400);
@@ -2480,6 +2593,7 @@ REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7321,'Fragment_Of_Crystal','Crystal Fragment','Etc',552,10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7322,'Poisonous_Gas','Toxic Gas','Etc',666,10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7323,'Battered_Kettle','Battered Kettle','Etc',256,10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (7324,'aegis_7324','Deep Sea Kraken\'s Leg','Etc',10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7325,'Tube','Flexible Tube','Etc',102,10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7326,'Fluorescent_Liquid','Fluorescent Liquid','Etc',712,10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`flag_buyingstore`) VALUES (7327,'Headlamp','Flashlight','Etc',1024,10,true);
@@ -3500,6 +3614,14 @@ REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`script`) VALUES (25703,'M_M_Overpower','Modification Module (Overpower)','Card',20,10,'autobonus "{ bonus bStr,50; bonus2 bAddClass,Class_All,15; bonus bMatkRate,-15; }",20,10000,BF_WEAPON; /* unknown rate */');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`script`) VALUES (25704,'M_M_Fatal_Flash','Modification Module (Fatal Flash)','Card',20,10,'autobonus "{ bonus bStr,50; bonus bCritAtkRate,10; bonus2 bHPLossRate,300,1000; }",20,10000,BF_WEAPON; /* unknown rate */');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`script`) VALUES (25705,'M_M_Lucky_Strike','Modification Module (Lucky Strike)','Card',20,10,'autobonus "{ bonus bLuk,50; bonus2 bMagicAtkEle,Ele_All,10; bonus2 bHPLossRate,300,1000; }",20,10000,BF_MAGIC; /* unknown rate */');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25892,'aegis_25892','Deep Sea Shell','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25893,'aegis_25893','Deep Sea Fish\'s Tail','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25894,'aegis_25894','Rotten Deep Sea Fish','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25895,'aegis_25895','Deep Sea Starfish','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25896,'aegis_25896','Deep Sea Crab','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25897,'aegis_25897','Deep Sea Witch\'s Crown','Etc',10,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`trade_override`,`trade_nodrop`,`trade_notrade`,`trade_nosell`,`trade_nocart`,`trade_nostorage`,`trade_noguildstorage`,`trade_nomail`,`trade_noauction`) VALUES (25898,'Siroods_Fish_Rod','Searud\'s Fishing Rod','Etc',100,true,true,true,true,true,true,true,true);
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`flag_buyingstore`) VALUES (25899,'aegis_25899','Abyssal Essence','Etc',10,true);
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_armor`,`script`) VALUES (27012,'Kick_Step_Card','Kick Step Card','Card',20,10,true,'bonus bMaxHPrate,8; if((eaclass()&EAJ_THIRDMASK) == EAJ_MECHANIC){ bonus bLongAtkDef,30; }');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_right_hand`,`script`) VALUES (27013,'Kick_&_Kick_Card','Kick & Kick Card','Card',20,10,true,'bonus2 bSkillAtk,"NC_VULCANARM",10+((getrefine()>=10)*20);');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_shoes`,`script`) VALUES (27014,'Green_Cenere_Card','Green Cenere Card','Card',20,10,true,'bonus bMaxHPrate,2; autobonus2 "{ bonus2 bHPRegenRate,1000,1000; }",30,5000;');
@@ -3926,3 +4048,15 @@ REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_right_hand`,`flag_buyingstore`,`script`) VALUES (31024,'As_Bdy_Knight_Card','Immortal Cursed Knight Card','Card',20,10,true,true,'bonus2 bAddClass,Class_All,10;\nbonus5 bAutoSpell,"RK_IGNITIONBREAK",5,20,BF_WEAPON,1;');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_right_hand`,`flag_buyingstore`,`script`) VALUES (31025,'As_Wind_Ghost_Card','Immortal Wind Ghost Card','Card',20,10,true,true,'bonus bMatkRate,10;\nbonus5 bAutoSpell,"SO_CLOUD_KILL",5,10,BF_MAGIC,1;');
 REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_shoes`,`flag_buyingstore`,`script`) VALUES (31026,'As_Ragged_Golem_Card','Stephane Jack Earnest Wolf Card','Card',20,10,true,true,'bonus5 bAutoSpellWhenHit,"SO_FIREWALK",5,35,BF_WEAPON,0;\nbonus5 bAutoSpellWhenHit,"SO_ELECTRICWALK",5,80,BF_MAGIC,0;\nautobonus "{ bonus bFlee,200; }",30,10000,BF_WEAPON,"{ specialeffect2 EF_TEIHIT1; }";\nautobonus "{ bonus bSpeedRate,25; }",25,10000,BF_MAGIC,"{ specialeffect2 EF_POTION_BERSERK; }";');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_right_hand`,`script`) VALUES (300140,'aegis_300140','Deep Sea Sropho Card','Card',10,true,'.@r = getrefine();\nbonus2 bMagicAtkEle,Ele_Wind,3;\nif (.@r>=9) {\n   bonus2 bMagicAtkEle,Ele_Wind,2;\n}\nif (.@r>=11) {\n   bonus2 bMagicAtkEle,Ele_Wind,5;\n}');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_right_accessory`,`location_left_accessory`,`script`) VALUES (300141,'aegis_300141','Deep Sea Obeaune Card','Card',10,true,true,'bonus bMatk,30;');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_right_hand`,`script`) VALUES (300142,'aegis_300142','Deep Sea Deviace Card','Card',10,true,'.@r = getrefine();\nbonus2 bAddRace,RC_Fish,5;\nbonus2 bAddEle,Ele_Water,5;\nif (.@r>=7) {\n   bonus2 bAddEle,Ele_Water,5;\n}\nif (.@r>=9) {\n   bonus2 bAddRace,RC_Fish,5;\n}');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_left_hand`,`script`) VALUES (300143,'aegis_300143','Deep Sea Marse Card','Card',10,true,'bonus2 bSubEle,Ele_Earth,15;\nbonus2 bSubRace,RC_Insect,15;');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_shoes`,`script`) VALUES (300144,'aegis_300144','Deep Sea Merman Card','Card',10,true,'bonus bBaseAtk,4*(readparam(bStr)/15);');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_garment`,`script`) VALUES (300145,'aegis_300145','Deep Sea Witch Card','Card',10,true,'bonus2 bMagicAtkEle,Ele_Fire,100;\nbonus2 bMagicAtkEle,Ele_Dark,100;\nbonus2 bSubEle,Ele_All,-30;');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_right_hand`,`script`) VALUES (300146,'aegis_300146','Deep Sea Sedora Card','Card',10,true,'bonus2 bCriticalAddRace,RC_Fish,30;');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_head_top`,`location_head_mid`,`location_head_low`,`script`) VALUES (300147,'aegis_300147','Deep Sea Swordfish Card','Card',10,true,true,true,'bonus2 bSubEle,Ele_Water,10;\nif (getrefine()>=9) {\n   bonus2 bMagicAddEle,Ele_Water,10;\n}');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_armor`,`script`) VALUES (300148,'aegis_300148','Deep Sea Strouf Card','Card',10,true,'bonus bMaxHPrate,10;\nbonus bMaxSPrate,5;\nif (getrefine()>=9) {\n   bonus2 bMagicAddEle,Ele_Water,5;\n   bonus2 bAddEle,Ele_Water,5;\n}');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_garment`,`script`) VALUES (300149,'aegis_300149','Deep Sea Phen Card','Card',10,true,'bonus2 bSubEle,Ele_Neutral,15;\nbonus2 bMagicAtkEle,Ele_Wind,3*getrefine();');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_head_top`,`location_head_mid`,`location_head_low`,`script`) VALUES (300150,'aegis_300150','Deep Sea King Dramoh Card','Card',10,true,true,true,'bonus bMaxHPrate,-5;\nbonus2 bAddClass,Class_All,(getrefine()/3);');
+REPLACE INTO `item_db_re` (`id`,`name_aegis`,`name_english`,`type`,`weight`,`location_right_hand`,`script`) VALUES (300151,'aegis_300151','Deep Sea Kraken Card','Card',10,true,'bonus bLongAtkRate,25;\nbonus bPerfectHitRate,5;');
