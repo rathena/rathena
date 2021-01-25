20baa2fdbae02a50a55f90c588e0626672c391d1
Lemongrass3110 <lemongrass@kstp.at>
2021-01-25 23:10:58 +0100

    Added a config for respawn time variance (#5715)
    By default the respawn time variance only applies to boss monsters on official servers.
    
    Fixes #5714
    
    Thanks to @Playtester

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -4150,6 +4150,12 @@ static const char* npc_parse_mob(char* w1, char* w2, char* w3, char* w4, const c
 		mob.xs = mob.ys = -1;
 	}
 
+	// Check if monsters should have variance applied to their respawn time
+	if( ( ( battle_config.mob_spawn_variance & 1 ) == 0 && mob.state.boss ) || ( ( battle_config.mob_spawn_variance & 2 ) == 0 && !mob.state.boss ) ){
+		// Remove the variance
+		mob.delay2 = 0;
+	}
+
 	if(mob.delay1>0xfffffff || mob.delay2>0xfffffff) {
 		ShowError("npc_parse_mob: Invalid spawn delays %u %u (file '%s', line '%d').\n", mob.delay1, mob.delay2, filepath, strline(buffer,start-buffer));
 		return strchr(start,'\n');// skip and continue
