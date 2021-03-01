f251e0d2c3c2d7bcf6ab015ad8220937b899cdb7
Atemo <capucrath@gmail.com>
2021-03-01 15:43:25 +0100

    Small fix in RoomOfConsciousness.txt

--- sql-files/upgrades/upgrade_20210301.sql
+++ sql-files/upgrades/upgrade_20210301.sql
@@ -1,3 +1,4 @@
+-- 16.1 official variables
 UPDATE `char_reg_num` SET `key` = 'ep16_royal' WHERE `key` = 'banquet_main_quest' AND `value` < 16;
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 1 WHERE `key` = 'banquet_main_quest' AND `value` < 20;
 UPDATE `char_reg_num` SET `key` = 'ep16_royal', `value` = `value` - 2 WHERE `key` = 'banquet_main_quest' AND `value` < 26;
@@ -8,7 +9,7 @@ DELETE FROM `char_reg_num` WHERE `key` = 'banquet_heine_quest';
 DELETE FROM `char_reg_num` WHERE `key` = 'banquet_richard_quest';
 
 UPDATE `char_reg_num` SET `key` = 'ep16_wal' WHERE `key` = 'banquet_walther_quest' AND `value` < 2;
-UPDATE `char_reg_num` SET `key` = 'ep16_wal', `value` = `value` - 1 WHERE `key` = 'banquet_walther_quest';
+UPDATE `char_reg_num` SET `key` = 'ep16_wal', `value` = `value` - 1 WHERE `key` = 'banquet_walther_quest' AND `value` > 1;
 UPDATE `char_reg_num` SET `key` = 'ep16_lug' WHERE `key` = 'banquet_roegenburg_quest';
 UPDATE `char_reg_num` SET `key` = 'ep16_gaobs' WHERE `key` = 'banquet_geoborg_quest';
 
@@ -28,7 +29,7 @@ UPDATE `char_reg_num` SET `key` = 'ep16_wig' WHERE `key` = 'banquet_wigner_quest
 
 UPDATE `char_reg_num` SET `key` = 'ep16_cookbs' WHERE `key` = 'banquet_quest_cooking' AND `value` < 3;
 UPDATE `char_reg_num` SET `key` = 'ep16_cookbs', `value` = 2 WHERE `key` = 'banquet_quest_cooking' AND `value` = 3;
-UPDATE `char_reg_num` SET `key` = 'ep16_cookbs', `value` = `value` + 8 WHERE `key` = 'banquet_quest_cooking';
+UPDATE `char_reg_num` SET `key` = 'ep16_cookbs', `value` = `value` + 8 WHERE `key` = 'banquet_quest_cooking' AND `value` > 3;
 
 DELETE FROM `quest` WHERE `quest_id` = 11428;
 DELETE FROM `quest` WHERE `quest_id` = 11429;
