455b02406f8815575fa85387eb13d9eb076bdd6f
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2021-04-01 23:58:47 +0900

    hotfix/issue5684
    Fix NPC_MAXPAIN skill mechanism error

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -1382,9 +1382,6 @@ int64 battle_calc_damage(struct block_list *src,struct block_list *bl,struct Dam
 	if( sc && sc->data[SC_INVINCIBLE] && !sc->data[SC_INVINCIBLEOFF] )
 		return 1;
 
-	if (sc && sc->data[SC_MAXPAIN])
-		return 0;
-
 	switch (skill_id) {
 #ifndef RENEWAL
     case PA_PRESSURE:
@@ -5705,6 +5702,14 @@ void battle_do_reflect(int attack_type, struct Damage *wd, struct block_list* sr
 
 		if (!tsc)
 			return;
+		
+		if (tsc->data[SC_MAXPAIN]) {
+			rdamage = wd->damage + wd->damage2;
+			tsc->data[SC_MAXPAIN]->val2 = (int)rdamage;
+			if(tsc && (!tsc->data[SC_KYOMU] && !(tsc->data[SC_DARKCROW] && wd->flag&BF_SHORT))) {//SC_KYOMU invalidates reflecting ability. SC_DARKCROW also does, but only for short weapon attack.
+				skill_castend_damage_id(target, src, NPC_MAXPAIN_ATK, tsc->data[SC_MAXPAIN]->val1, tick, ((wd->flag&1)?wd->flag-1:wd->flag));
+			}
+		}
 
 		// Calculate skill reflect damage separately
 		if ((ud && !ud->immune_attack) || !status_bl_has_mode(target, MD_SKILL_IMMUNE))
@@ -5715,11 +5720,6 @@ void battle_do_reflect(int attack_type, struct Damage *wd, struct block_list* sr
 
 			if (sc && sc->data[SC_VITALITYACTIVATION])
 				rdamage /= 2;
-			if (tsc->data[SC_MAXPAIN]) {
-				tsc->data[SC_MAXPAIN]->val2 = (int)rdamage;
-				skill_castend_damage_id(target, src, NPC_MAXPAIN_ATK, tsc->data[SC_MAXPAIN]->val1, tick, wd->flag);
-				tsc->data[SC_MAXPAIN]->val2 = 0;
-			}
 			else if( attack_type == BF_WEAPON && tsc->data[SC_REFLECTDAMAGE] ) // Don't reflect your own damage (Grand Cross)
 				map_foreachinshootrange(battle_damage_area,target,skill_get_splash(LG_REFLECTDAMAGE,1),BL_CHAR,tick,target,wd->amotion,sstatus->dmotion,rdamage,wd->flag);
 			else if( attack_type == BF_WEAPON || attack_type == BF_MISC) {
@@ -7039,10 +7039,15 @@ struct Damage battle_calc_misc_attack(struct block_list *src,struct block_list *
 				md.damage /= 10;
 			break;
 		case NPC_MAXPAIN_ATK:
-			if (ssc && ssc->data[SC_MAXPAIN])
-				md.damage = ssc->data[SC_MAXPAIN]->val2;
-			else
+			if (ssc && ssc->data[SC_MAXPAIN]) {
+				if (ssc->data[SC_MAXPAIN]->val2) {
+					md.damage = ssc->data[SC_MAXPAIN]->val2 * skill_lv / 10;
+				} else if (ssc->data[SC_MAXPAIN]->val3) {
+					md.damage = ssc->data[SC_MAXPAIN]->val3 * skill_lv / 10;
+				}
+			} else {
 				md.damage = 0;
+			}
 			break;
 		case SU_SV_ROOTTWIST_ATK:
 			md.damage = 100;
@@ -7362,11 +7367,6 @@ int64 battle_calc_return_damage(struct block_list* bl, struct block_list *src, i
 		}
 		if (ssc->data[SC_VENOMBLEED] && ssc->data[SC_VENOMBLEED]->val3 == 0)
 			rdamage -= damage * ssc->data[SC_VENOMBLEED]->val2 / 100;
-	}
-
-	if (sc) {
-		if (sc->data[SC_MAXPAIN])
-			rdamage = damage * sc->data[SC_MAXPAIN]->val1 * 10 / 100;
 	}
 
 	return cap_value(min(rdamage,max_damage),INT_MIN,INT_MAX);
