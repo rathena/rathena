bf993c2b3925d09a1107b8af3f215c2a013ea35d
aleos <aleos89@users.noreply.github.com>
2021-04-15 15:04:28 -0400

    Utilize nocostume mapflag [ci skip]

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -13467,7 +13467,7 @@ void pc_set_costume_view(struct map_session_data *sd) {
 		sd->status.robe = id->look;
 
 	// Costumes check
-	if (!map_getmapflag(sd->bl.m, MF_NOCOSTUME) && !map_flag_gvg2(sd->bl.m)) {
+	if (!map_getmapflag(sd->bl.m, MF_NOCOSTUME)) {
 		if ((i = sd->equip_index[EQI_COSTUME_HEAD_LOW]) != -1 && (id = sd->inventory_data[i])) {
 			if (!(id->equip&(EQP_COSTUME_HEAD_MID|EQP_COSTUME_HEAD_TOP)))
 				sd->status.head_bottom = id->look;
