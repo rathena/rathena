002be4d440fda35d4e2bd8d828a988fb31f8218e
Emistry Haoyan <equinox1991@gmail.com>
2021-04-29 10:37:21 +0800

    Display item_id of current item combo set
    - fix item_id aren't shown.

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -12645,7 +12645,7 @@ int status_change_start(struct block_list* src, struct block_list* bl,enum sc_ty
 	}
 
 	if (current_equip_combo_pos && tick == INFINITE_TICK) {
-		ShowWarning("sc_start: Item combo contains an INFINITE_TICK duration. Skipping bonus.\n");
+		ShowWarning("sc_start: Item combo of item #%u contains an INFINITE_TICK duration. Skipping bonus.\n", sd->inventory_data[pc_checkequip(sd, current_equip_combo_pos)]->nameid);
 		return 0;
 	}
 
