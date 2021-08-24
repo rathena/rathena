f8b1565766ffd668b2691acd3bd91c18af2f1fef
Lemongrass3110 <lemongrass@kstp.at>
2021-08-24 15:19:58 +0200

    Fixed atcommand_mobinfo_type config

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -7474,22 +7474,20 @@ ACMD_FUNC(mobinfo)
 		strcpy(atcmd_output, " ");
 		unsigned int j = 0;
 #ifdef RENEWAL_DROP
-		int penalty = pc_level_penalty_mod( sd, PENALTY_DROP, mob );
+		int drop_modifier;
+
+		if( battle_config.atcommand_mobinfo_type ){
+			drop_modifier = pc_level_penalty_mod( sd, PENALTY_DROP, mob );
+		}else{
+			drop_modifier = 100;
+		}
 #endif
 
 		for (i = 0; i < MAX_MOB_DROP_TOTAL; i++) {
 			if (mob->dropitem[i].nameid == 0 || mob->dropitem[i].rate < 1 || (item_data = itemdb_exists(mob->dropitem[i].nameid)) == NULL)
 				continue;
 
-			int droprate = mob_getdroprate(&sd->bl, mob, mob->dropitem[i].rate, 100);
-
-#ifdef RENEWAL_DROP
-			if( battle_config.atcommand_mobinfo_type ) {
-				droprate = droprate * penalty / 100;
-				if (droprate <= 0 && !battle_config.drop_rate0item)
-					droprate = 1;
-			}
-#endif
+			int droprate = mob_getdroprate( &sd->bl, mob, mob->dropitem[i].rate, drop_modifier );
 
 			if (item_data->slots)
 				sprintf(atcmd_output2, " - %s[%d]  %02.02f%%", item_data->ename.c_str(), item_data->slots, (float)droprate / 100);
