51b7272e38f2c7fdd1652490c821d25cc719250a
Balfear <balfear@yandex.ru>
2021-09-08 16:16:15 +0300

    Update src/map/script.cpp
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -8450,7 +8450,7 @@ BUILDIN_FUNC(delitemidx) {
 
 	idx = script_getnum(st, 2);
 	if (idx < 0 || idx >= MAX_INVENTORY) {
-		ShowWarning("buildin_delitemidx: Index (%d) should be from 0-%d.\n", idx, sd->inventory.max_amount - 1);
+		ShowWarning("buildin_delitemidx: Index %d is out of the range 0-%d.\n", idx, MAX_INVENTORY - 1);
 		script_pushint(st, 0);
 		return SCRIPT_CMD_SUCCESS;
 	}
