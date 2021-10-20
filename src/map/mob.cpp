8aef5bee65a2b6bf447ac7d438fa4c4d4e21b55b
Jittapan Pluemsumran <secret@rathena.org>
2021-10-21 03:37:31 +0700

    Follow up

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -4896,7 +4896,7 @@ void MobDatabase::loadingFinished() {
 			mob->range2 = max(1, mob->range2 * battle_config.view_range_rate / 100);
 
 		if (battle_config.chase_range_rate != 100)
-			mob->range3 = cap_value(mob->range3, mob->range2, mob->range3 * battle_config.chase_range_rate / 100);
+			mob->range3 = max(mob->range2, mob->range3 * battle_config.chase_range_rate / 100);
 
 		// Tests showed that chase range is effectively 2 cells larger than expected [Playtester]
 		mob->range3 += 2;
