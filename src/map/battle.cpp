5b8a567d383dfb562386588681b7f59962c8bace
Aleos <aleos89@users.noreply.github.com>
2021-12-06 08:47:05 -0500

    Update src/map/battle.cpp
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -2260,8 +2260,10 @@ static int64 battle_calc_base_damage(struct block_list *src, struct status_data 
 	sd = BL_CAST(BL_PC, src);
 
 	if (!sd) { //Mobs/Pets
-		if (sc != NULL && sc->data[SC_CHANGE] != NULL)
+#ifndef RENEWAL
+		if (sc != nullptr && sc->data[SC_CHANGE] != nullptr)
 			return status->matk_max; // [Aegis] simply uses raw max matk for base damage when Mental Charge active
+#endif
 		if(flag&4) {
 			atkmin = status->matk_min;
 			atkmax = status->matk_max;
