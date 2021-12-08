92f4a48ed10ce198d3b9f2941de556fc0bc10443
Daegaladh <daegaladh@shadowgames.es>
2021-12-08 20:33:33 +0100

    Self-destruction must lower intimacy even if doesn't hit any enemies

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -2441,12 +2441,11 @@ int skill_counter_additional_effect (struct block_list* src, struct block_list *
 		sc_start(src,src,SC_BLIND,2*skill_lv,skill_lv,skill_get_time2(skill_id,skill_lv));
 		break;
 	case HFLI_SBR44:	//[orn]
-	case HVAN_EXPLOSION:
 		if(src->type == BL_HOM){
-			TBL_HOM *hd = (TBL_HOM*)src;
-			hd->homunculus.intimacy = (skill_id == HFLI_SBR44) ? 200 : 100; // hom_intimacy_grade2intimacy(HOMGRADE_HATE_WITH_PASSION)
-			if (sd && hd->master)
-				clif_send_homdata(sd,SP_INTIMATE,hd->homunculus.intimacy / 100);
+			TBL_HOM *hd = BL_CAST(BL_HOM,src);
+			hd->homunculus.intimacy = 200; // hom_intimacy_grade2intimacy(HOMGRADE_HATE_WITH_PASSION)
+			if (hd->master)
+				clif_send_homdata(hd->master,SP_INTIMATE,hd->homunculus.intimacy / 100);
 		}
 		break;
 	case CR_GRANDCROSS:
@@ -7711,6 +7710,12 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			return 1;
 		}
 		status_damage(src, src, sstatus->max_hp,0,0,1, skill_id);
+		if(skill_id == HVAN_EXPLOSION && src->type == BL_HOM) {
+			TBL_HOM *hd = BL_CAST(BL_HOM,src);
+			hd->homunculus.intimacy = 100; // hom_intimacy_grade2intimacy(HOMGRADE_HATE_WITH_PASSION)
+			if (hd->master)
+				clif_send_homdata(hd->master,SP_INTIMATE,hd->homunculus.intimacy / 100);
+		}
 		break;
 	case AL_ANGELUS:
 #ifdef RENEWAL
