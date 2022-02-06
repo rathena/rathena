b8507ef8ca226855cc49f7473dc369f1744c36c1
Joam <35770095+attackjom@users.noreply.github.com>
2022-02-06 14:52:03 +0700

    Update mob.cpp

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -2481,7 +2481,7 @@ void mob_damage(struct mob_data *md, struct block_list *src, int damage)
 		for(i = 0; i < DAMAGELOG_SIZE; i++){ // must show hp bar to all char who already hit the mob.
 			struct map_session_data *sd = map_charid2sd(md->dmglog[i].id);
 			if( sd && check_distance_bl(&md->bl, &sd->bl, AREA_SIZE) ) // check if in range
-					clif_monster_hp_bar(md, sd->fd);
+				clif_monster_hp_bar(md, sd->fd);
 		}
 	}
 #endif
@@ -3410,7 +3410,7 @@ void mob_heal(struct mob_data *md,unsigned int heal)
 			if( md->dmglog[i].id ) {
 				struct map_session_data *sd = map_charid2sd(md->dmglog[i].id);
 				if( sd && check_distance_bl(&md->bl, &sd->bl, AREA_SIZE) ) // check if in range
-						clif_monster_hp_bar(md, sd->fd);
+					clif_monster_hp_bar(md, sd->fd);
 			}
 	}
 #endif
