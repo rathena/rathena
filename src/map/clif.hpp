08192a35bc856b38e94aa0d8b0c87574008cd97e
Lemongrass3110 <lemongrass@kstp.at>
2022-02-22 21:52:27 +0100

    Revamped Laphine UIs (#6625)
    Fixes #3302
    Closes #4348
    
    Thanks for the initial release by @Cydh in #4348 and everyone that contributed to it.
    
    All existing data was migrated and cleaned up where necessary.
    Thanks to @Everade for his help here.
    
    Laphine UIs are now fully yamlified and not dependent on the script engine.
    They make use of new item group features and of the already existing random option group feature.
    This way they will be far easier to be maintained, even though they are a little less customize able.
    
    Thanks to @limitro, @CairoLee, @dimasshotta and everyone else who contributed!
    
    Co-authored-by: Cydh <cydh.ramdh@gmail.com>
    Co-authored-by: Everade <Everade@users.noreply.github.com>
    Co-authored-by: Aleos <aleos89@users.noreply.github.com>

--- src/map/clif.hpp
+++ src/map/clif.hpp
@@ -45,6 +45,8 @@ struct guild_log_entry;
 enum e_guild_storage_log : uint16;
 enum e_bg_queue_apply_ack : uint16;
 enum e_instance_notify : uint8;
+struct s_laphine_synthesis;
+struct s_laphine_upgrade;
 
 enum e_PacketDBVersion { // packet DB
 	MIN_PACKET_DB  = 0x064,
@@ -1189,5 +1191,9 @@ void clif_barter_extended_open( struct map_session_data& sd, struct npc_data& nd
 
 void clif_summon_init(struct mob_data& md);
 void clif_summon_hp_bar(struct mob_data& md);
+
+// Laphine System
+void clif_laphine_synthesis_open( struct map_session_data *sd, std::shared_ptr<s_laphine_synthesis> synthesis );
+void clif_laphine_upgrade_open( struct map_session_data* sd, std::shared_ptr<s_laphine_upgrade> upgrade );
 
 #endif /* CLIF_HPP */
