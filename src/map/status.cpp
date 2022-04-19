rathena/rathena

commit de990f71ad5daa51c879f7a933e18dd7fbdacd88

Fixes mobs res/mres always 0 (#6760)
Fixes an issue where status_calc_misc resets the value of res/mres (potentially defined in mob_db.yml) to 0.

Thanks to @limitro !

src/map/status.cpp
@@ -1995,8 +1995,10 @@ void status_calc_misc(struct block_list *bl, struct status_data *status, int lev
 		status->def2 = status->mdef2 =
 		status->cri = status->flee2 =
 		status->patk = status->smatk =
-		status->hplus = status->crate =
-		status->res = status->mres = 0;
+		status->hplus = status->crate = 0;
+		
+		if (bl->type != BL_MOB)	// BL_MOB has values set when loading mob_db
+			status->res = status->mres = 0;
 
 #ifdef RENEWAL // Renewal formulas
 	if (bl->type == BL_HOM) {

commit b7e49098496ea27b6d2036e1d66af0959206ee95

Fixed RES/MRES for players
Fixes #6841

This was broken in de990f7

Co-authored-by: Playtester <Kenji.lto@gmx.de>

src/map/status.cpp
@@ -1989,7 +1989,7 @@ void status_calc_misc(struct block_list *bl, struct status_data *status, int lev
 	int stat;
 
 	// Non players get the value set, players need to stack with previous bonuses.
-	if( bl->type != BL_PC )
+	if( bl->type != BL_PC ){
 		status->batk =
 		status->hit = status->flee =
 		status->def2 = status->mdef2 =
@@ -1999,6 +1999,7 @@ void status_calc_misc(struct block_list *bl, struct status_data *status, int lev
 		
 		if (bl->type != BL_MOB)	// BL_MOB has values set when loading mob_db
 			status->res = status->mres = 0;
+	}
 
 #ifdef RENEWAL // Renewal formulas
 	if (bl->type == BL_HOM) {
b7e49098496ea27b6d2036e1d66af0959206ee95
Lemongrass3110 <lemongrass@kstp.at>
2022-04-19 11:37:19 +0200

    Fixed RES/MRES for players
    Fixes #6841
    
    This was broken in de990f7
    
    Co-authored-by: Playtester <Kenji.lto@gmx.de>

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -1989,7 +1989,7 @@ void status_calc_misc(struct block_list *bl, struct status_data *status, int lev
 	int stat;
 
 	// Non players get the value set, players need to stack with previous bonuses.
-	if( bl->type != BL_PC )
+	if( bl->type != BL_PC ){
 		status->batk =
 		status->hit = status->flee =
 		status->def2 = status->mdef2 =
@@ -1999,6 +1999,7 @@ void status_calc_misc(struct block_list *bl, struct status_data *status, int lev
 		
 		if (bl->type != BL_MOB)	// BL_MOB has values set when loading mob_db
 			status->res = status->mres = 0;
+	}
 
 #ifdef RENEWAL // Renewal formulas
 	if (bl->type == BL_HOM) {
