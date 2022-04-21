02cade919b4f1ab82be287a6726a66c055078ec1
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2022-04-21 19:06:38 +0900

    cleanup
    Co-authored-by: Aleos <aleos89@users.noreply.github.com>

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -7485,16 +7485,36 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 
 	case NPC_IMMUNE_PROPERTY:
 		switch (skill_lv) {
-			case 1  : type = SC_IMMUNE_PROPERTY_NOTHING;	break;
-			case 2  : type = SC_IMMUNE_PROPERTY_WATER;		break;
-			case 3  : type = SC_IMMUNE_PROPERTY_GROUND;		break;
-			case 4  : type = SC_IMMUNE_PROPERTY_FIRE;		break;
-			case 5  : type = SC_IMMUNE_PROPERTY_WIND;		break;
-			case 6  : type = SC_IMMUNE_PROPERTY_DARKNESS;	break;
-			case 7  : type = SC_IMMUNE_PROPERTY_SAINT;		break;
-			case 8  : type = SC_IMMUNE_PROPERTY_POISON;		break;
-			case 9  : type = SC_IMMUNE_PROPERTY_TELEKINESIS;break;
-			case 10 : type = SC_IMMUNE_PROPERTY_UNDEAD;		break;
+			case 1:
+				type = SC_IMMUNE_PROPERTY_NOTHING;
+				break;
+			case 2:
+				type = SC_IMMUNE_PROPERTY_WATER;
+				break;
+			case 3:
+				type = SC_IMMUNE_PROPERTY_GROUND;
+				break;
+			case 4:
+				type = SC_IMMUNE_PROPERTY_FIRE;
+				break;
+			case 5:
+				type = SC_IMMUNE_PROPERTY_WIND;
+				break;
+			case 6:
+				type = SC_IMMUNE_PROPERTY_DARKNESS;
+				break;
+			case 7:
+				type = SC_IMMUNE_PROPERTY_SAINT;
+				break;
+			case 8:
+				type = SC_IMMUNE_PROPERTY_POISON;
+				break;
+			case 9:
+				type = SC_IMMUNE_PROPERTY_TELEKINESIS;
+				break;
+			case 10:
+				type = SC_IMMUNE_PROPERTY_UNDEAD;
+				break;
 		}
 		clif_skill_nodamage(src,bl, skill_id,skill_lv,sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)));
 		break;
