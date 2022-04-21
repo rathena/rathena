166e2d7c6bdca7fe552154602781cd8a18daa0c7
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2022-04-21 19:37:43 +0900

    Group NPC_IMMUNE_PROPERTY SCs

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -7517,6 +7517,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 				break;
 		}
 		clif_skill_nodamage(src,bl, skill_id,skill_lv,sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)));
+		sc_start(src,bl, SC_IMMUNE_PROPERTY,100,skill_lv,skill_get_time(skill_id,skill_lv));
 		break;
 
 	case NPC_MOVE_COORDINATE:
