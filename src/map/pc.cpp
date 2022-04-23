35633b22f14013bbe71ce560b269c19f99fe89a6
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2022-04-23 23:05:06 +0900

    Fix equip order

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -11406,7 +11406,7 @@ bool pc_equipitem(struct map_session_data *sd,short n,int req_pos,bool equipswit
 	} else if(pos == EQP_ARMS && id->equip == EQP_HAND_R) { //Dual wield capable weapon.
 		pos = (req_pos&EQP_ARMS);
 		if (pos == EQP_ARMS) //User specified both slots, pick one for them.
-			pos = equip_index[EQI_HAND_R] >= 0 ? EQP_HAND_L : EQP_HAND_R;
+			pos = (equip_index[EQI_HAND_L] < 0) ? EQP_HAND_L : EQP_HAND_R;
 	} else if(pos == EQP_SHADOW_ACC) { // Shadow System
 		pos = req_pos&EQP_SHADOW_ACC;
 		if (pos == EQP_SHADOW_ACC)
