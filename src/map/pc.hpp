3d0477a3306acb2cc1bbb9cede4797d84bd93f16
Playtester <Kenji.Ito@gmx.de>
2022-05-01 13:38:43 +0200

    Fixes HPFactor being way too high - Fixes #6908 - HpFactor default changed from 20000 to 0 - Renamed HPMultiplicator to HpIncrease and SPFactor to SpIncrease so the naming is more consistent - Improved documentation

--- src/map/pc.hpp
+++ src/map/pc.hpp
@@ -1002,7 +1002,7 @@ public:
 
 struct s_job_info {
 	std::vector<uint32> base_hp, base_sp, base_ap; //Storage for the first calculation with hp/sp/ap factor and multiplicator
-	uint32 hp_factor, hp_multiplicator, sp_factor, max_weight_base;
+	uint32 hp_factor, hp_increase, sp_increase, max_weight_base;
 	std::vector<std::array<uint16,PARAM_MAX>> job_bonus;
 	std::vector<int16> aspd_base;
 	t_exp base_exp[MAX_LEVEL], job_exp[MAX_LEVEL];
