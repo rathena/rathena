c1f46a47f6093654eb8aa28dde56076f815e16ab
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2022-07-04 01:40:52 +0900

    Reupdate

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -3571,9 +3571,19 @@ int64 skill_attack (int attack_type, struct block_list* src, struct block_list *
 					dmg.damage -= dmg.damage * (6 * (1+per)) / 100;
 				}
 
-				if (dmg.damage > 0 && tsd && tsd->bonus.reduce_damage_return != 0) {
-					dmg.damage -= dmg.damage * tsd->bonus.reduce_damage_return / 100;
-					dmg.damage = i64max(dmg.damage, 1);
+				int64 reduce = 0;
+
+				if (tsd && tsd->bonus.reduce_damage_return != 0) {
+					reduce += tsd->bonus.reduce_damage_return;
+				}
+				if (tsc && tsc->data[SC_REFLECTDAMAGE]) {
+					reduce += (tsc->data[SC_REFLECTDAMAGE]->val2);
+				}
+				if (tsc && tsc->data[SC_REF_T_POTION])
+					reduce += 100;
+				if (dmg.damage > 0) {
+					dmg.damage -= dmg.damage * i64min(100, reduce) / 100;
+					dmg.damage = i64max(dmg.damage, dmg.div_);
 				}
 			}
 #endif
