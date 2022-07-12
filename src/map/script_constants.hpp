dd55e4ee13e80ffa8589ca3a2f5abe59cd34c61a
null <muramaza.prosever@live.com>
2022-07-12 22:36:24 +0700

    Update script_constants.hpp

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -5814,7 +5814,6 @@
 	export_constant(IG_EPIC_MODULE_MIX);
 	export_constant(IG_AUTO_M_I_BOX_A);
 	export_constant(IG_AUTO_M_I_BOX_B);
-	export_constant(IG_ILLUSION_MODULE_MIX);
 
 	export_constant(IG_ODIN_RELIC);
 	export_constant(IG_AUTOMATIC_MODULE_MIX);
