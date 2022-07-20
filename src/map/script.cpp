773e61a5d1db4dea0a43ab56038ebcaace3aa42a
aleos <aleos89@users.noreply.github.com>
2022-07-20 08:22:04 -0400

    Fixes script command rentalcountitem * Fixes #7121. * Fixes an issue with normal items getting mixed into the rental count. Thanks to @Cukinaru!

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -7031,9 +7031,9 @@ static int script_countitem_sub(struct item *items, std::shared_ptr<item_data> i
 		for (int i = 0; i < size; i++) {
 			item *itm = &items[i];
 
-			if (itm == nullptr || itm->nameid == 0 || itm->amount < 1 || (!rental && itm->expire_time > 0))
+			if (itm == nullptr || itm->nameid == 0 || itm->amount < 1)
 				continue;
-			if (itm->nameid == nameid)
+			if (itm->nameid == nameid && ((rental && itm->expire_time > 0) || (!rental && itm->expire_time == 0)))
 				count += itm->amount;
 		}
 	} else { // For expanded functions
@@ -7065,7 +7065,7 @@ static int script_countitem_sub(struct item *items, std::shared_ptr<item_data> i
 		for (int i = 0; i < size; i++) {
 			item *itm = &items[i];
 
-			if (itm == nullptr || itm->nameid == 0 || itm->amount < 1 || (!rental && items[i].expire_time > 0))
+			if (itm == nullptr || itm->nameid == 0 || itm->amount < 1)
 				continue;
 			if (itm->nameid != it.nameid || itm->identify != it.identify || itm->refine != it.refine || itm->attribute != it.attribute)
 				continue;
@@ -7082,7 +7082,8 @@ static int script_countitem_sub(struct item *items, std::shared_ptr<item_data> i
 					continue;
 			}
 
-			count += items[i].amount;
+			if ((rental && itm->expire_time > 0) || (!rental && itm->expire_time == 0))
+				count += items[i].amount;
 		}
 	}
 
