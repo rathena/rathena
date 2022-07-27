e9f7388a1724928b97c8236afc9f9c7c0a0d17c2
muh <schmunk@posteo.de>
2022-07-27 15:11:36 +0000

    * fixes SC_KI_SUL_RAMPAGE to break on movement

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -771,6 +771,10 @@ int unit_walktoxy( struct block_list *bl, short x, short y, unsigned char flag)
 	}
 
 	TBL_PC *sd = BL_CAST(BL_PC, bl);
+	if (sd && bl->type == BL_PC) {
+		if (sd->sc.data[SC_KI_SUL_RAMPAGE])
+			status_change_end(bl, SC_KI_SUL_RAMPAGE, INVALID_TIMER);
+	}
 
 	// Start timer to recall summon
 	if( sd != nullptr ){
