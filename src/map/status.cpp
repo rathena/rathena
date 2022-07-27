496fcd6f90e797b1a8e31be43f960e205f0f58f1
muh <schmunk@posteo.de>
2022-07-27 17:00:08 +0000

    * fixes ALL_ODINS_POWER failing on CELL_CHKLANDPROTECTOR thanks @Badarosk0

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -1868,7 +1868,6 @@ bool status_check_skilluse(struct block_list *src, struct block_list *target, ui
 				return false;
 			break;
 		case AL_TELEPORT:
-		case ALL_ODINS_POWER:
 			// Should fail when used on top of Land Protector [Skotlex]
 			if (src && map_getcell(src->m, src->x, src->y, CELL_CHKLANDPROTECTOR)
 				&& !status_has_mode(status,MD_STATUSIMMUNE)
