6a0f287df4266f2196f80b8671f4d20f9adbaa37
Null il3ol2ed <59070443+null6385@users.noreply.github.com>
2022-08-10 21:26:14 +0700

    Update script_constants.hpp

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -8795,7 +8795,7 @@
 	export_constant(ITEMJ_ALL_UPPER);
 	export_constant(ITEMJ_ALL_BABY);
 	export_constant(ITEMJ_ALL_THIRD);
-    export_constant(ITEMJ_ALL_FOURTH);
+	export_constant(ITEMJ_ALL_FOURTH);
 
 	/* item drop effects */
 	export_constant(DROPEFFECT_NONE);
