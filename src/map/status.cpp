cb54bcc2dc76a318a3dbdf3e6b213178ec7fdd83
Aleos <aleos89@users.noreply.github.com>
2022-09-22 08:42:35 -0400

    Apply suggestions from code review
    Thanks to @Lemongrass3110!
    
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -1916,14 +1916,14 @@ bool status_check_skilluse(struct block_list *src, struct block_list *target, ui
 
 		if (sc->data[SC_WINKCHARM] && target && !flag) { // Prevents skill usage
 			block_list *wink_target = map_id2bl(sc->data[SC_WINKCHARM]->val2);
-			unit_data *wink_ud = unit_bl2ud(src);
 
 			if (wink_target != nullptr) {
+				unit_data *wink_ud = unit_bl2ud(src);
 				if (wink_ud != nullptr && wink_ud->walktimer == INVALID_TIMER)
 					unit_walktobl(src, wink_target, 3, 1);
 				clif_emotion(src, ET_THROB);
 			} else
-				status_change_end(src, SC_WINKCHARM, INVALID_TIMER);
+				status_change_end(src, SC_WINKCHARM);
 		}
 
 		if (sc->data[SC_BLADESTOP]) {
