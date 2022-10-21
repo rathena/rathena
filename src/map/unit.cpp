858f4d051abb0e50898c6cf00fc69d55f503ce2c
munkrej <schmunk@posteo.de>
2022-10-21 06:53:34 +0200

    Update src/map/unit.cpp
    Co-authored-by: Aleos <aleos89@users.noreply.github.com>

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -780,7 +780,8 @@ int unit_walktoxy( struct block_list *bl, short x, short y, unsigned char flag)
 	}
 
 	TBL_PC *sd = BL_CAST(BL_PC, bl);
-	if (sd && bl->type == BL_PC) {
+
+	if (sd) {
 		if (sd->sc.data[SC_KI_SUL_RAMPAGE])
 			status_change_end(bl, SC_KI_SUL_RAMPAGE, INVALID_TIMER);
 	}
