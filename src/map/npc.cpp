3d53cdd61d32a86979b8523e691a563e7c7db5ac
Lemongrass3110 <lemongrass@kstp.at>
2022-11-10 18:24:13 +0100

    Block dynamic NPCs in instances
    Fixes #7420
    
    Thanks to @attackjom

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -5805,7 +5805,10 @@ struct npc_data* npc_duplicate_npc_for_player( struct npc_data& nd, struct map_s
 		return nullptr;
 	}
 
-	// TODO: check maps that might forbid usage? maybe create mapflag?
+	if( map_getmapflag( sd.bl.m, MF_NODYNAMICNPC ) ){
+		// It has been confirmed that there is no reply to the client
+		return nullptr;
+	}
 
 	int16 new_x, new_y;
 
