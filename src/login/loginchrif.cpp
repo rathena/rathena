9e6a4224877b8265196dbd41f75ec04132af077e
Ex0ample <ex0ample@gmail.com>
2023-02-08 18:47:25 +0700

    Fix re-check char slots

--- src/login/loginchrif.cpp
+++ src/login/loginchrif.cpp
@@ -182,7 +182,9 @@ int logchrif_send_accdata(int fd, uint32 aid) {
 		char_vip = login_config.vip_sys.char_increase;
 		if( acc.vip_time > time(NULL) ) {
 			isvip = true;
-			char_slots += char_vip;
+			if (char_slots < MIN_CHARS + MAX_CHAR_VIP) {
+				char_slots += char_vip;
+			}
 		}
 #endif
 	}
