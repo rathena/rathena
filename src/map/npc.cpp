eff613108748dfb20d9ac2b9e566fc90c2548519
Vincent Stumpf <vincents.995@gmail.com>
2023-02-12 11:01:14 -0800

    Speed up loading of npcs and maps (#7597)

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -6028,10 +6028,14 @@ int npc_reload(void) {
 
 	//TODO: the following code is copy-pasted from do_init_npc(); clean it up
 	// Reloading npcs now
+	ShowStatus("Loading NPC Files\n");
 	for (nsl = npc_src_files; nsl; nsl = nsl->next) {
+#ifdef DEBUG
 		ShowStatus("Loading NPC file: %s" CL_CLL "\r", nsl->name);
+#endif
 		npc_parsesrcfile(nsl->name);
 	}
+	ShowInfo("Loaded NPC Files\n");
 	ShowInfo ("Done loading '" CL_WHITE "%d" CL_RESET "' NPCs:" CL_CLL "\n"
 		"\t-'" CL_WHITE "%d" CL_RESET "' Warps\n"
 		"\t-'" CL_WHITE "%d" CL_RESET "' Shops\n"
@@ -6185,7 +6189,9 @@ void do_init_npc(void){
 	// process all npc files
 	ShowStatus("Loading NPCs...\r");
 	for( file = npc_src_files; file != NULL; file = file->next ) {
+#ifdef DEBUG
 		ShowStatus("Loading NPC file: %s" CL_CLL "\r", file->name);
+#endif
 		npc_parsesrcfile(file->name);
 	}
 	ShowInfo ("Done loading '" CL_WHITE "%d" CL_RESET "' NPCs:" CL_CLL "\n"
