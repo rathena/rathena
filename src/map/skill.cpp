186e9786b3f169928872dd14d58442f0d49a5496
Lemongrass3110 <lemongrass@kstp.at>
2023-02-12 22:42:24 +0100

    Reverted some unconfirmed and unrelated changes

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -8013,15 +8013,8 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			sc_start2(src,bl,type,100,skill_lv,skill_id,skill_get_time(skill_id,skill_lv)));
 		break;
 	case HLIF_AVOID:
-		sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)); // Master
-		clif_skill_nodamage(src,src,skill_id,skill_lv,sc_start(src,src,type,100,skill_lv,skill_get_time(skill_id,skill_lv))); // Homunc
-		break;
 	case HAMI_DEFENCE:
 		sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)); // Master
-#ifdef RENEWAL
-		if (hd)
-			skill_blockhomun_start(hd, skill_id, skill_get_cooldown(skill_id, skill_lv));
-#endif
 		clif_skill_nodamage(src,src,skill_id,skill_lv,sc_start(src,src,type,100,skill_lv,skill_get_time(skill_id,skill_lv))); // Homunc
 		break;
 	case NJ_BUNSINJYUTSU:
@@ -10481,20 +10474,14 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 	case HFLI_FLEET:
 	case HFLI_SPEED:
 	case HLIF_CHANGE:
-		clif_skill_nodamage(src,bl,skill_id,skill_lv,
-			sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)));
-		if (hd)
-#ifdef RENEWAL
-			skill_blockhomun_start(hd, skill_id, skill_get_cooldown(skill_id,skill_lv));
-#else
-			skill_blockhomun_start(hd, skill_id, skill_get_time2(skill_id,skill_lv));
-#endif
-		break;
 	case MH_ANGRIFFS_MODUS:
 	case MH_GOLDENE_FERSE:
 		clif_skill_nodamage(src,bl,skill_id,skill_lv,
 			sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)));
+		if (hd)
+			skill_blockhomun_start(hd, skill_id, skill_get_time2(skill_id,skill_lv));
 		break;
+
 	case NPC_DRAGONFEAR:
 		if (flag&1) {
 			const enum sc_type sc[] = { SC_STUN, SC_SILENCE, SC_CONFUSION, SC_BLEEDING };
