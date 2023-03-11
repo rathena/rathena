a137cd6145c1cf87532e392958757449ffcf722a
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2023-03-11 15:09:24 +0900

    Fix code including other pull stuff

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -3346,28 +3346,6 @@ int battle_get_magic_element(struct block_list* src, struct block_list* target, 
 		element = rnd()%ELE_ALL;
 
 	switch(skill_id) {
-		case SS_ANKOKURYUUAKUMU:
-			if (mflag & SKILL_ALTDMG_FLAG)
-				element = ELE_FIRE;
-			break;
-		case SU_CN_METEOR:
-		case SH_HYUN_ROKS_BREEZE:
-		case SH_HYUN_ROK_CANNON:
-			if (sc && sc->count) {
-				if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_1))
-					element = ELE_WATER;
-				else if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_2))
-					element = ELE_WIND;
-				else if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_3))
-					element = ELE_EARTH;
-				else if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_4))
-					element = ELE_FIRE;
-				else if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_5))
-					element = ELE_DARK;
-				else if (sc->getSCE(SC_COLORS_OF_HYUN_ROK_6))
-					element = ELE_HOLY;
-			}
-			break;
 		case NPC_EARTHQUAKE:
 			element = ELE_NEUTRAL;
 			break;
