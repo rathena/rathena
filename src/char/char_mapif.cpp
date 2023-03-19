59f39fe5d88500f44af1a0cc4f355087dbf0fc18
Vincent Stumpf <vincents.995@gmail.com>
2023-03-18 17:35:22 -0700

    A better fix for char leak

--- src/char/char_mapif.cpp
+++ src/char/char_mapif.cpp
@@ -1480,7 +1480,7 @@ int chmapif_init(int fd){
  * @param id: id of map-serv (should be >0, FIXME)
  */
 void chmapif_server_init(int id) {
-	memset(&map_server[id], 0, sizeof(map_server[id]));
+	map_server[id] = {};
 	map_server[id].fd = -1;
 }
 
@@ -1493,7 +1493,6 @@ void chmapif_server_destroy(int id){
 		do_close(map_server[id].fd);
 		map_server[id].fd = -1;
 	}
-	map_server[id].maps.clear();
 }
 
 /**
