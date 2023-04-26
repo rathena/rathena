84bf6b87f1946f28055e90bf713cedd4f401c685
Atemo <Atemo@users.noreply.github.com>
2023-04-26 23:35:24 +0200

    Fixed an issue for map_drops.yml (#7735)
    The key for umap should be the index, to allow to define the same item in yaml file

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -6545,7 +6545,7 @@ bool MapDropDatabase::parseDrop( const ryml::NodeRef& node, std::unordered_map<u
 	}
 
 	if( !exists ){
-		drops[drop->nameid] = drop;
+		drops[index] = drop;
 	}
 
 	return true;
