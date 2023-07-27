35bc756e557742397a471498b4f4b32d0720fab5
Atemo <capucrath@gmail.com>
2023-07-28 00:02:03 +0200

    Frozen Slash - Increases cooldown from 0.3 seconds to 0.45 seconds. - Increases SP consumption from 85 to 96 based on level 5. - Increases AP recovery rate from 1 to 2. - Increases damage from 3000%/4250%(Climax)Matk to 4750%/6650%(Climax)Matk based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -7878,10 +7878,10 @@ struct Damage battle_calc_magic_attack(struct block_list *src,struct block_list 
 						RE_LVL_DMOD(100);
 						break;
 					case AG_FROZEN_SLASH:
-						skillratio += -100 + 600 * skill_lv + 5 * sstatus->spl;
+						skillratio += -100 + 250 + 900 * skill_lv + 5 * sstatus->spl;
 
 						if( sc != nullptr && sc->getSCE( SC_CLIMAX ) ){
-							skillratio += 250 * skill_lv;
+							skillratio += 150 + 350 * skill_lv;
 						}
 
 						RE_LVL_DMOD(100);
