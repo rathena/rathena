57dfd1174b24385b9bf2516dd49426df3a99b57f
Atemo <capucrath@gmail.com>
2023-07-27 23:58:23 +0200

    Stratum Tremor - Reduces delay after skill from 0.5 seconds to 0.25 seconds. - Increases SP consumption from 75 to 94 based on level 5. - Increases damage from 1250%Matk to 3750%Matk per hit based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -7795,7 +7795,7 @@ struct Damage battle_calc_magic_attack(struct block_list *src,struct block_list 
 						RE_LVL_DMOD(100);
 						break;
 					case AG_STRANTUM_TREMOR:
-						skillratio += -100 + 250 * skill_lv + 5 * sstatus->spl;
+						skillratio += -100 + 100 + 730 * skill_lv + 5 * sstatus->spl;
 						RE_LVL_DMOD(100);
 						break;
 					case AG_ALL_BLOOM_ATK:
