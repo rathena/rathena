c8a4ce842cffde381163beda35219b2aa041c8ec
Atemo <capucrath@gmail.com>
2023-08-06 00:56:14 +0200

    Explosion Blaster - Adds global cool time by 1 second. - Increases SP consumption from 90 to 125 based on level 5. - Reduces area of effect from 11 x 11 cells to 9 x 9 cells based on level 5. - Reduces base damage of from 14000%Atk to 12000%Atk based on level 5. - Increases base damage against Oleum Sanctum debuff from 16000%Atk to 17600%Atk based on level 5. - Reduces factor weight of POW in skill formula (without Oleum Sanctum debuff) from 15 to 10.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5419,7 +5419,7 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case IQ_OLEUM_SANCTUM:
-			skillratio += -100 + 500 + 2000 * skill_lv + 5 * sstatus->pow;
+			skillratio += -100 + 500 + 2000 * skill_lv + 10 * sstatus->pow;
 			RE_LVL_DMOD(100);
 			break;
 		case IQ_MASSIVE_F_BLASTER:
@@ -5429,10 +5429,10 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case IQ_EXPOSION_BLASTER:
-			skillratio += -100 + 2800 * skill_lv + 15 * sstatus->pow;
+			skillratio += -100 + 2400 * skill_lv + 15 * sstatus->pow;
 
 			if( tsc != nullptr && tsc->getSCE( SC_HOLY_OIL ) ){
-				skillratio += 400 * skill_lv;
+				skillratio += 350 + 1050 * skill_lv;
 			}
 
 			RE_LVL_DMOD(100);
