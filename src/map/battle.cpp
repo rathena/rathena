36fd33f58c319033c0095ed4ca11295b45df5279
Atemo <capucrath@gmail.com>
2023-08-09 01:07:26 +0200

    Storm Slash - Reduces cooldown from 0.35 seconds to 0.3 seconds. - Increases AP recovery rate from 1 to 2. - Increases damage from 600%Atk to 950%Atk per hit based on level 5. - Increases the chance to double skill damage while under Giant Growth buff from 15% to 30%.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5400,9 +5400,9 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 				skillratio *= 2;
 			break;
 		case DK_STORMSLASH:
-			skillratio += -100 + 120 * skill_lv + 5 * sstatus->pow;
+			skillratio += -100 + 100 + 170 * skill_lv + 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
-			if (sc && sc->getSCE(SC_GIANTGROWTH))
+			if (sc && sc->getSCE(SC_GIANTGROWTH) && rnd()%100 < 30)
 				skillratio *= 2;
 			break;
 		case IQ_OLEUM_SANCTUM:
