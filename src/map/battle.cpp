d80a4708798784794e169a64f564cee8552b0eac
Atemo <capucrath@gmail.com>
2023-08-09 01:57:47 +0200

    Servant Weapon - Phantom - Removes 0.8 seconds variable casting time. - Reduces cooldown from 2 seconds to 0.5 seconds. - Removes 0.8 seconds delay after skill. - Increases damage from 600%Atk to 1700%Atk per hit based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5367,7 +5367,7 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case DK_SERVANT_W_PHANTOM:
-			skillratio += 100 * skill_lv + 5 * sstatus->pow;
+			skillratio += -100 + 200 + 300 * skill_lv + 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
 			break;
 		case DK_SERVANT_W_DEMOL:
