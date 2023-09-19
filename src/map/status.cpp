5361f01f3287314f8fcc2ebf6876041ace9574b9
Leemonn <47922762+Leemonn@users.noreply.github.com>
2023-09-19 12:36:29 +0200

    fix(status): fixed crit calculation

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -5780,7 +5780,7 @@ void status_calc_bl_main(struct block_list *bl, std::bitset<SCB_MAX> flag)
 		if (status->luk == b_status->luk)
 			status->cri = status_calc_critical(bl, sc, b_status->cri);
 		else
-			status->cri = status_calc_critical(bl, sc, b_status->cri + 3*(status->luk - b_status->luk));
+			status->cri = status_calc_critical(bl, sc, b_status->cri + (status->luk - b_status->luk)*10/3);
 
 		/// After status_calc_critical so the bonus is applied despite if you have or not a sc bugreport:5240
 		if (sd) {
