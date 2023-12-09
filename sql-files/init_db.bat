bb1866c93c98b0b183207d94c58472e65fb6e063
Wachira Norasing <oldnew123@gmail.com>
2023-12-09 20:50:09 +0700

    custom rate and script

---
+++ sql-files/init_db.bat
@@ -0,0 +1,55 @@
+mysql -uroot -proot rathena < main.sql
+
+mysql -uroot -proot rathena < logs.sql
+
+@REM  ITEM
+
+mysql -uroot -proot rathena < item_db.sql
+
+mysql -uroot -proot rathena < item_db2.sql
+
+mysql -uroot -proot rathena < item_db_re.sql
+
+mysql -uroot -proot rathena < item_db2_re.sql
+
+mysql -uroot -proot rathena < item_db_equip.sql
+
+mysql -uroot -proot rathena < item_db_etc.sql
+
+mysql -uroot -proot rathena < item_db_usable.sql
+
+mysql -uroot -proot rathena < item_db_re_equip.sql
+
+mysql -uroot -proot rathena < item_db_re_etc.sql
+
+mysql -uroot -proot rathena < item_db_re_usable.sql
+
+mysql -uroot -proot rathena < roulette_default_data.sql
+
+
+
+
+@REM mysql -uroot -proot ragnarok < item_cash_db.sql
+
+@REM mysql -uroot -proot ragnarok < item_cash_db2.sql
+
+
+@REM  MOB
+
+mysql -uroot -proot rathena < mob_db.sql
+
+mysql -uroot -proot rathena < mob_db2.sql
+
+mysql -uroot -proot rathena < mob_db_re.sql
+
+mysql -uroot -proot rathena < mob_db2_re.sql
+
+mysql -uroot -proot rathena < mob_skill_db.sql
+
+mysql -uroot -proot rathena < mob_skill_db2.sql
+
+mysql -uroot -proot rathena < mob_skill_db_re.sql
+
+mysql -uroot -proot rathena < mob_skill_db2_re.sql
+
+
