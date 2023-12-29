b133e19658d4180eb4feb6c734e9baf48fa3505e
Atemo <Atemo@users.noreply.github.com>
2023-12-29 16:51:43 +0100

    Rebalance shadow cross 20221221 (#8019)
    * Savage Impact
    - Reduces cooldown from 1 second to 0.7 seconds.
    - Increases area of effect from 5 x 5 cells to 7 x 7 cells based on level 10.
    - Applies AP recovery rate by 2.
    
    * Shadow Stab
    - The skill will deal damage for 2 hits.
    - While under Cloaking Exceed buff, increases number of hit to 3 hits with higher damage per hit.
    - Increases factor weight of POW in skill formula while under Cloaking Exceed buff from 5 to 7.
    - Increases base damage from 1500%Atk to 1750%/2000%Atk per hit based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5550,7 +5550,12 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case SHC_SHADOW_STAB:
-			skillratio += -100 + 300 * skill_lv + 5 * sstatus->pow;
+			skillratio += -100 + 350 * skill_lv + 5 * sstatus->pow;
+
+			if( sc && sc->getSCE( SC_CLOAKINGEXCEED ) ){
+				skillratio += 50 * skill_lv + 2 * sstatus->pow;
+			}
+
 			RE_LVL_DMOD(100);
 			break;
 		case SHC_IMPACT_CRATER:
@@ -6694,7 +6699,7 @@ static struct Damage initialize_weapon_data(struct block_list *src, struct block
 				break;
 			case SHC_SHADOW_STAB:
 				if (wd.miscflag == 2)
-					wd.div_ = 2;
+					wd.div_ = 3;
 				break;
 			case SHC_IMPACT_CRATER:
 				if (sc && sc->getSCE(SC_ROLLINGCUTTER))
