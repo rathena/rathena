23766e61c3908b97dd664f7c40f3e484fe225d2a
Atemo <Atemo@users.noreply.github.com>
2023-12-29 23:57:52 +0100

    Rebalance Windhawk 20221221 (#8020)
    * No Limits (Ranger)
    - No longer be removed by Dispell or Clearance.
    
    * Calamity Gale
    - Increases duration from 60 seconds to 180 seconds.
    - Reduces cooldown from 300 seconds to 180 seconds.
    
    Note: it was not implemented
    - No longer be removed when using No Limits.
    
    * Gale Storm
    - Reduces cooldown from 1.5 seconds to 1.2 seconds.
    - Increases SP consumption from 100 to 120 based on level 10.
    - Increases base damage from 9500%Atk to 10000%Atk based on level 5.
    - Reduces area of effect from 11 x 11 cells to 9 x 9 cells based on level 10.
    
    Note: CON weight was already 10
    - Increases factor weight of CON in skill formula from 5 to 10.
    
    * Crescive Bolt
    - Increases cooldown from 0.15 seconds to 0.35 seconds.
    - Increases global cool time from 0.3 seconds to 0.7 seconds.
    - Increases base damage from 3400%Atk to 9400%Atk based on level 10.
    
    Additionnally
    - Removed leftover Deep Blind Trap / Solid Trap / Swift Trap / Flame Trap status ailments in `skills_db.yml` (from previous rebalance)
    - Corrected WH HAWK BOOMERANG and WH_HAWKBOOMERANG formula according to the Korean description of the skill.
    - Included WH_NATUREFRIENDLY in the damage formula of WH_HAWKRUSH and WH_HAWKBOOMERANG (x1.5 at level 5) (source: tests on KRO)
    
    Thanks to @datawulf

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5627,22 +5627,26 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			break;
 		case WH_HAWKRUSH:
 			skillratio += -100 + 500 * skill_lv + 5 * sstatus->con;
+			if (sd)
+				skillratio += skillratio * pc_checkskill(sd, WH_NATUREFRIENDLY) / 10;
 			RE_LVL_DMOD(100);
 			break;
-		case WH_HAWKBOOMERANG:// Affected by trait stats??? CON for sure but the other one unknown. Likely POW. [Rytech]
-			skillratio += -100 + 600 * skill_lv + 10 * sstatus->pow + 10 * sstatus->con;
+		case WH_HAWKBOOMERANG:
+			skillratio += -100 + 600 * skill_lv + 10 * sstatus->con;
+			if (sd)
+				skillratio += skillratio * pc_checkskill(sd, WH_NATUREFRIENDLY) / 10;
 			if (tstatus->race == RC_BRUTE || tstatus->race == RC_FISH)
 				skillratio += skillratio * 50 / 100;
 			RE_LVL_DMOD(100);
 			break;
 		case WH_GALESTORM:
-			skillratio += -100 + 950 * skill_lv + 10 * sstatus->con;
+			skillratio += -100 + 1000 * skill_lv + 10 * sstatus->con;
 			RE_LVL_DMOD(100);
 			if (sc && sc->getSCE(SC_CALAMITYGALE) && (tstatus->race == RC_BRUTE || tstatus->race == RC_FISH))
 				skillratio += skillratio * 50 / 100;
 			break;
 		case WH_CRESCIVE_BOLT:
-			skillratio += -100 + 340 * skill_lv + 5 * sstatus->con;
+			skillratio += -100 + 400 + 900 * skill_lv + 5 * sstatus->con;
 			RE_LVL_DMOD(100);
 			if (sc) {
 				if (sc->getSCE(SC_CRESCIVEBOLT))
