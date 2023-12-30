97745ca8ae3b24db6f1757236a1f94f84a894c72
Atemo <capucrath@gmail.com>
2023-12-30 19:19:04 +0100

    Abyss Dagger - Increases cooldown from 0.3 seconds to 0.4 seconds. - Increases SP consumption from 64 to 76 based on level 5. - Increases base damage from 2600%Atk to 4600%Atk based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5605,7 +5605,7 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case ABC_ABYSS_DAGGER:
-			skillratio += -100 + 100 + 500 * skill_lv + 5 * sstatus->pow;
+			skillratio += -100 + 100 + 900 * skill_lv + 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
 			break;
 		case ABC_UNLUCKY_RUSH:
