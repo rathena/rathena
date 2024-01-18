97edaa8ec3020eb14047aa3efc239e329fde2a62
Atemo <capucrath@gmail.com>
2024-01-18 15:15:25 +0100

    Double Bowling Bash - Reduces knock-back distance from 2 cells to 1 cell. - Increases base damage from 2650%Atk to 3200%Atk per hit based on level 10.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5848,7 +5848,7 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case HN_DOUBLEBOWLINGBASH:
-			skillratio += -100 + 150 + 250 * skill_lv;
+			skillratio += -100 + 200 + 300 * skill_lv;
 			skillratio += pc_checkskill(sd, HN_SELFSTUDY_TATICS) * 3 * skill_lv;
 			skillratio += 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
