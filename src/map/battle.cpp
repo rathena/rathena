e9326aa9a2de69b916fb599ad482555d274efa59
Atemo <capucrath@gmail.com>
2024-02-10 18:23:42 +0100

    Gale Storm - Reduces skill cooldown from 1.2 seconds to 0.7 seconds. - Increases base damage from 10000%Atk to 13500%Atk based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5690,7 +5690,8 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case WH_GALESTORM:
-			skillratio += -100 + 1000 * skill_lv + 10 * sstatus->con;
+			skillratio += -100 + 1350 * skill_lv;
+			skillratio += 10 * sstatus->con;
 			RE_LVL_DMOD(100);
 			if (sc && sc->getSCE(SC_CALAMITYGALE) && (tstatus->race == RC_BRUTE || tstatus->race == RC_FISH))
 				skillratio += skillratio * 50 / 100;
