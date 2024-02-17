aa5e1015e70e81d4542fbed674d3c089df4b8cc1
Atemo <capucrath@gmail.com>
2024-02-17 04:25:36 +0100

    Spark Blaster - Increases base damage from 9250%Atk to 14600%Atk based on level 10.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5639,7 +5639,7 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case MT_SPARK_BLASTER:
-			skillratio += -100 + 250 + 900 * skill_lv;
+			skillratio += -100 + 600 + 1400 * skill_lv;
 			skillratio += 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
 			break;
