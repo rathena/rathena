6d7a24c0890827374adc39fab957218175a4043c
aleos <aleos89@users.noreply.github.com>
2024-02-24 22:11:00 -0500

    Resolves a potential crash from Howling Mine * Fixes #8146. * Minor cleanups to ensure the Howling Mine status is active before accessing. Thanks to @LadyNanuia!

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -6892,14 +6892,16 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 				break;
 			}
 			// Triggered by RL_FLICKER
-			if (sd && sd->flicker && tsc && tsc->getSCE(SC_H_MINE) && tsc->getSCE(SC_H_MINE)->val2 == src->id) {
+			if (sd && sd->flicker) {
 				// Splash damage around it!
 				map_foreachinrange(skill_area_sub, bl, skill_get_splash(skill_id, skill_lv), BL_CHAR|BL_SKILL,
 					src, skill_id, skill_lv, tick, flag|BCT_ENEMY|1, skill_castend_damage_id);
 				flag |= 1; // Don't consume requirement
-				tsc->getSCE(SC_H_MINE)->val3 = 1; // Mark the SC end because not expired
-				status_change_end(bl, SC_H_MINE);
-				sc_start4(src, bl, SC_BURNING, 10 * skill_lv, skill_lv, 1000, src->id, 0, skill_get_time2(skill_id,skill_lv));
+				if (tsc &&tsc->getSCE(SC_H_MINE) && tsc->getSCE(SC_H_MINE)->val2 == src->id) {
+					tsc->getSCE(SC_H_MINE)->val3 = 1; // Mark the SC end because not expired
+					status_change_end(bl, SC_H_MINE);
+					sc_start4(src, bl, SC_BURNING, 10 * skill_lv, skill_lv, 1000, src->id, 0, skill_get_time2(skill_id,skill_lv));
+				}
 			}
 		}
 		else
