0442da4ec09829cb0a588bcc60fde4b020db0a4e
Atemo <capucrath@gmail.com>
2024-03-10 23:17:19 +0100

    Wild Fire - Increases base damage from 13250%/12500%Atk to 17500%/16500%Atk based on level 5.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5956,12 +5956,12 @@ static int battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list *
 			RE_LVL_DMOD(100);
 			break;
 		case NW_WILD_FIRE:
-			skillratio += -100 + 1000 + 2300 * skill_lv;
+			skillratio += -100 + 1500 + 3000 * skill_lv;
 			skillratio += 5 * sstatus->con;
 			if (sc && sc->getSCE(SC_INTENSIVE_AIM_COUNT))
 				skillratio += sc->getSCE(SC_INTENSIVE_AIM_COUNT)->val1 * 500 * skill_lv;
 			if (sd && sd->weapontype1 == W_SHOTGUN)
-				skillratio += 150 * skill_lv;
+				skillratio += 200 * skill_lv;
 			RE_LVL_DMOD(100);
 			break;
 		case NW_MAGAZINE_FOR_ONE:
