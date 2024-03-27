1a004f01642fc2b5eb10bf412a5a11cd78bb4f97
Playtester <3785983+Playtester@users.noreply.github.com>
2024-03-27 11:48:05 +0100

    Mercenary, Regen and Frenzy Improvements (#8185)
    - When a Mercenary gains a bonus, there will now be an animation
    - Mercenary bonuses now last for 5 minutes instead of 10 minutes
    - Fixed HP/SP recovery values of Mercenaries (and Elementals)
    - Mercenaries now recover HP when walking
    - Homunculi no longer recover SP when walking
    - Mercenary natural recovery interval is 8s for HP and 6s for SP
    - Homunculus natural recovery interval is 2s for HP and 4s for SP
    - MER_CRASH now only deals 1 hit and can be cast-cancelled
    - Frenzy now drains HP every 10 seconds instead of every 15 in pre-renewal
    - Fixed SP cost Mercenary Frenzy (100 -> 200 SP)
    - Killing monsters exactly 2 times below you base level now still counts as mercenary kill
    - Fixes #8184 
    - Fixes #7663

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -3109,8 +3109,8 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 					achievement_update_objective(sd, AG_BATTLE, 1, md->mob_id);
 			}
 
-			// The master or Mercenary can increase the kill count
-			if (sd->md && src && (src->type == BL_PC || src->type == BL_MER) && mob->lv > sd->status.base_level / 2)
+			// The master or Mercenary can increase the kill count, if the monster level is greater or equal than half the baselevel of the master
+			if (sd->md && src && (src->type == BL_PC || src->type == BL_MER) && mob->lv >= sd->status.base_level / 2)
 				mercenary_kills(sd->md);
 		}
 
