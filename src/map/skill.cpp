ca1af420fabc33892ab94c24b378a27fd6461684
vstumpf <vincents.995@gmail.com>
2024-04-21 00:45:31 +0000

    Move skill_onskillusage from skill_attack to skill_castend
    Fixes #8256

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -2521,7 +2521,7 @@ int skill_onskillusage(map_session_data *sd, struct block_list *bl, uint16 skill
 		sd->state.autocast = 0;
 	}
 
-	// Check for player and pet autobonuses when being attacked by skill_id
+	// Check for player and pet autobonuses when casting skill_id
 	if (sd != nullptr) {
 		// Player
 		if (!sd->autobonus3.empty()) {
@@ -4138,8 +4138,6 @@ int64 skill_attack (int attack_type, struct block_list* src, struct block_list *
 				}
 				break;
 		}
-		if( sd )
-			skill_onskillusage(sd, bl, skill_id, tick);
 	}
 
 	if (!(flag&2)) {
@@ -7181,7 +7179,7 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 		{// consume arrow on last invocation to this skill.
 			battle_consume_ammo(sd, skill_id, skill_lv);
 		}
-
+		skill_onskillusage(sd,bl,skill_id,skill_lv);
 		// perform skill requirement consumption
 		if (!(flag&SKILL_NOCONSUME_REQ))
 			skill_consume_requirement(sd,skill_id,skill_lv,2);
