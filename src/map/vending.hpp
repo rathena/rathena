827517f8cfc17589435cd19ed1ee9f71e05f5f4f
Lemongrass3110 <lemongrass@kstp.at>
2024-05-02 22:44:31 +0200

    Cleaned up a few TODOs from #7963 (#8254)

--- src/map/vending.hpp
+++ src/map/vending.hpp
@@ -23,9 +23,9 @@ void do_final_vending(void);
 void do_init_vending(void);
 void do_init_vending_autotrade( void );
  
-void vending_reopen( map_session_data* sd );
+void vending_reopen( map_session_data& sd );
 void vending_closevending(map_session_data* sd);
-int8 vending_openvending(map_session_data* sd, const char* message, const uint8* data, int count, struct s_autotrader *at);
+int8 vending_openvending( map_session_data& sd, const char* message, const uint8* data, int count, struct s_autotrader *at );
 void vending_vendinglistreq(map_session_data* sd, int id);
 void vending_purchasereq(map_session_data* sd, int aid, int uid, const uint8* data, int count);
 bool vending_search(map_session_data* sd, t_itemid nameid);
