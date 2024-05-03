be4fee7459c3688e7c9488cc4a293668554b24f6
Lemongrass3110 <lemongrass@kstp.at>
2024-05-03 02:45:22 +0200

    Replaced all NULLs in web

--- src/web/charconfig_controller.cpp
+++ src/web/charconfig_controller.cpp
@@ -53,7 +53,7 @@ HANDLER_FUNC(charconfig_save) {
 
 	if (SqlStmt_NumRows(stmt) > 0) {
 		char databuf[SQL_BUFFER_SIZE];
-		if (SQL_SUCCESS != SqlStmt_BindColumn(stmt, 0, SQLDT_STRING, &databuf, sizeof(databuf), NULL, NULL)
+		if (SQL_SUCCESS != SqlStmt_BindColumn(stmt, 0, SQLDT_STRING, &databuf, sizeof(databuf), nullptr, nullptr)
 			|| SQL_SUCCESS != SqlStmt_NextRow(stmt)
 		) {
 			SqlStmt_ShowDebug(stmt);
@@ -157,7 +157,7 @@ HANDLER_FUNC(charconfig_load) {
 
 	char databuf[SQL_BUFFER_SIZE];
 
-	if (SQL_SUCCESS != SqlStmt_BindColumn(stmt, 0, SQLDT_STRING, &databuf, sizeof(databuf), NULL, NULL)
+	if (SQL_SUCCESS != SqlStmt_BindColumn(stmt, 0, SQLDT_STRING, &databuf, sizeof(databuf), nullptr, nullptr)
 		|| SQL_SUCCESS != SqlStmt_NextRow(stmt)
 	) {
 		SqlStmt_ShowDebug(stmt);
