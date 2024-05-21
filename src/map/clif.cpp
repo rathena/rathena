2fc675b206600449331be60fe90329d0001aae4f
Playtester <3785983+Playtester@users.noreply.github.com>
2024-05-22 00:05:10 +0200

    Improved documentation and moved setting to battle.conf

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -5246,8 +5246,23 @@ int clif_damage(struct block_list* src, struct block_list* dst, t_tick tick, int
 
 	// Calculate what sdelay to send to the client so it applies damage at the same time as the server
 	if (battle_config.synchronize_damage && src->type == BL_MOB) {
-		// The client only accepts values between 0 and 432; 432 stands for 1x animation speed
-		sdelay = sdelay * DEFAULT_ANIMATION_SPEED / status_get_clientamotion(src);
+		// When a clif_damage packet is sent to the client it will also send "sdelay" (amotion) as value.
+		// The client however does not interpret this value as AttackMotion but incorrectly as an inverted
+		// animation speed modifier, with 432 standing for 1x animation speed.
+		// The client will ignore all values above 432, but lower values will speed up the animation.
+		// 216 for example means play the animation at double the speed. 108 is quadruple speed.
+		// Each monster has an attack animation and may define the frame in the attack animation on which
+		// it displays the damage and makes the target flinch / stop. If the damage frame is undefined,
+		// it instead displays the damage / flinch / stop at the beginning of the second to last frame.
+		// We define the time after which the damage frame shows at 1x speed as clientamotion.
+		// Knowing when the damage frame happens in the animation allows us to synchronize the timing
+		// between client and server using the formula below.
+		sdelay = sdelay * DEFAULT_ANIMATION_SPEED / std::max(1ui16, status_get_clientamotion(src));
+
+		// Hint: If amotion is larger than clientamotion this results in a value above 432 which makes the
+		// client display the attack at 1x speed. In this case we need to shorten the delay damage timer
+		// on the server to clientamotion ms instead (see battle_delay_damage).
+		sdelay = std::min(sdelay, DEFAULT_ANIMATION_SPEED);
 	}
 
 	WBUFW(buf,0) = cmd;
