00e87fee6fb157fdafdad790f1fd4427df4be2f6
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2024-05-26 22:12:35 +0900

    Apply suggestion
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/packets_struct.hpp
+++ src/map/packets_struct.hpp
@@ -5964,14 +5964,6 @@ struct PACKET_CZ_ADVENTURER_AGENCY_JOIN_RESULT {
 DEFINE_PACKET_HEADER(CZ_ADVENTURER_AGENCY_JOIN_RESULT, 0x0af8);
 #endif  // PACKETVER_MAIN_NUM >= 20191218 || PACKETVER_RE_NUM >= 20191211 || PACKETVER_ZERO_NUM >= 20191224
 
-#if PACKETVER_MAIN_NUM >= 20221005
-struct PACKET_ZC_SPECIALPOPUP {
-	int16 PacketType;
-	int32 ppId;
-} __attribute__((packed));
-DEFINE_PACKET_HEADER(ZC_SPECIALPOPUP, 0x0bbe);
-#endif
-
 #if !defined(sun) && (!defined(__NETBSD__) || __NetBSD_Version__ >= 600000000) // NetBSD 5 and Solaris don't like pragma pack but accept the packed attribute
 #pragma pack(pop)
 #endif // not NetBSD < 6 / Solaris
