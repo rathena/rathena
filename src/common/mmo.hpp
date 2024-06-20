b3e3a6ac43b76f3aa38fed91846c579baa95b47c
Daegaladh <daegaladh@shadowgames.es>
2024-06-20 20:33:32 +0200

    Reworked to make it more configurable Reworked for user names and passwords too

--- src/common/mmo.hpp
+++ src/common/mmo.hpp
@@ -152,7 +152,7 @@ const t_itemid WEDDING_RING_F = 2635;
 //For character names, title names, guilds, maps, etc.
 //Includes null-terminator as it is the length of the array.
 #define NAME_LENGTH (23 + 1)
-#define PASSWD_LENGTH (32+1)
+#define PASSWD_LENGTH (32 + 1)
 //NPC names can be longer than it's displayed on client (NAME_LENGTH).
 #define NPC_NAME_LENGTH 50
 // <NPC_NAME_LENGTH> for npc name + 2 for a "::" + <NAME_LENGTH> for label + 1 for EOS
