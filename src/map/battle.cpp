8f4f24a19389cefa4e66ef188089341e3ca51855
AoShinRO <darklord.dl@gmail.com>
2024-08-18 16:50:39 -0300

    doing more checks  to  animation

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -7702,6 +7702,20 @@ static struct Damage battle_calc_weapon_attack(struct block_list *src, struct bl
 
 	battle_do_reflect(BF_WEAPON,&wd, src, target, skill_id, skill_lv); //WIP [lighta]
 
+	if (battle_config.feature_restore_animation_skills && sd) 
+	{
+		if(!sd->animation.empty())
+		{
+			if(wd.dmg_lv==ATK_FLEE||wd.dmg_lv==ATK_MISS||wd.dmg_lv==ATK_BLOCK)
+			{ //miss cancel animation
+				int i;
+				ARR_FIND(0,sd->animation.size(),i,sd->animation[i]->skill_id==skill_id);
+				if(i<sd->animation.size())
+					sd->animation[i]->miss_flag = ATK_FLEE;
+			}
+		}
+	}
+
 	return wd;
 }
 
