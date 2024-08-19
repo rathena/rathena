1b3e0c174289d728343204581779208a2004f678
AoShinRO <darklord.dl@gmail.com>
2024-08-19 11:45:11 -0300

    cleaning destructor call

--- src/map/chrif.cpp
+++ src/map/chrif.cpp
@@ -152,7 +152,6 @@ bool chrif_auth_delete(uint32 account_id, uint32 char_id, enum sd_state state) {
 			if (node->sd->regs.arrays)
 				node->sd->regs.arrays->destroy(node->sd->regs.arrays, script_free_array_db);
 
-			node->sd->~map_session_data();
 			delete node->sd;
 		}
 
@@ -1904,7 +1903,6 @@ int auth_db_final(DBKey key, DBData *data, va_list ap) {
 		if (node->sd->regs.arrays)
 			node->sd->regs.arrays->destroy(node->sd->regs.arrays, script_free_array_db);
 
-		node->sd->~map_session_data();
 		delete node->sd;
 	}
 
