b42b517c48707f1627503ebceda111c3e64417a8
AoShinRO <darklord.dl@gmail.com>
2024-09-11 16:49:20 -0300

    adjusted cap

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -14861,7 +14861,7 @@ int skill_castend_map (map_session_data *sd, uint16 skill_id, const char *mapnam
 #if PACKETVER_MAIN_NUM >= 20170502 || PACKETVER_RE_NUM >= 20170419 || defined(PACKETVER_ZERO)
 			// check if the chosen map exists in the memo list
 			int ext_size = lv + static_cast<int>(pc_readreg2(sd, EXT_MEMO_VAR));
-			if(ext_size > PC_MAXMEMOPOINTS(sd)+1) ext_size = PC_MAXMEMOPOINTS(sd)+1; // crash prevention
+			if(ext_size > MAX_MEMOPOINTS+1) ext_size = 1 + MAX_MEMOPOINTS; // crash prevention
 			ARR_FIND( 0, ext_size, i, strncmp( p[i]->map, mapname, sizeof( p[i]->map ) ) == 0 );
 			if( i < ext_size ) {
 				x=p[i]->x;
