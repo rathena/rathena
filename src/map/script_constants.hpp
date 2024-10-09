ae168b1ba52fb1e62d5d00f1f6a96bc8fed8eda1
Atemo <capucrath@gmail.com>
2024-10-09 20:38:03 +0200

    Added missing constant

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -1920,6 +1920,7 @@
 	export_constant(SC_BATH_FOAM_B);
 	export_constant(SC_BATH_FOAM_C);
 	export_constant(SC_BUCHEDENOEL);
+	export_constant(SC_EP16_DEF);
 
 	/* status icons */
 	export_deprecated_constant2("SI_BLANK",-1);
