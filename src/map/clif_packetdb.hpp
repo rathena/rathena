58707692e867b01d75bc0e7fc25539128230f222
Lemongrass3110 <lemongrass@kstp.at>
2024-11-08 01:21:12 +0100

    Converted ZC_MEMBER_NEWENTRY to struct (#8788)

--- src/map/clif_packetdb.hpp
+++ src/map/clif_packetdb.hpp
@@ -87,7 +87,6 @@
 	parseable_packet(0x00d5,-1,clif_parse_CreateChatRoom,2,4,6,7,15);
 	packet(0x00d7,-1);
 	parseable_packet(0x00d9,14,clif_parse_ChatAddMember,2,6);
-	packet(0x00dc,28);
 	packet(0x00dd,29);
 	parseable_packet(0x00de,-1,clif_parse_ChatRoomStatusChange,2,4,6,7,15);
 	parseable_packet(0x00e0,30,clif_parse_ChangeChatOwner,2,6);
