0012b9471b69f85e76586164b134533b52474b2e
Atemo <capucrath@gmail.com>
2025-01-22 03:33:49 +0100

    4. Star Cannon - Reduces cooldown from 5 seconds to 3.5 seconds. - Increases base damage from 2700%Atk to 3000%Atk based on level 5. - Reduces skill duration from 4.5 seconds to 2.5 seconds. - Increases shooting star interval from 0.3 seconds to 0.5 seconds. - Reduces maximum shooting star fall per round from 4 stars to 2 stars. - Reduces area of effect of shooting star damage from 7 x 7 cells to 5 x 5 cells based on level 5. - Increases SP consumption from 63 to 110 based on level 10.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -6421,7 +6421,7 @@ static int32 battle_calc_attack_skill_ratio(struct Damage* wd, struct block_list
 			break;
 
 		case SKE_STAR_CANNON:
-			skillratio += -100 + 200 + 500 * skill_lv;
+			skillratio += -100 + 250 + 550 * skill_lv;
 			skillratio += pc_checkskill( sd, SKE_SKY_MASTERY ) * 5 * skill_lv;
 			skillratio += 5 * sstatus->pow;
 			RE_LVL_DMOD(100);
