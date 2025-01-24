fed910e139b0bac0bb727ceda4bfc9afca24be32
Lemongrass3110 <lemongrass@kstp.at>
2025-01-24 20:44:08 +0100

    Small SQLDT cleanup (#8963)
    Replaced all SQLDT_SHORT with SQLDT_INT16
    Replaced all SQLDT_USHORT with SQLDT_UINT16
    Replaced all SQLDT_INT with SQLDT_INT32
    Replaced all SQLDT_UINT with SQLDT_UINT32
    Dropped the platform dependent SQLDT types SQLDT_SHORT, SQLDT_USHORT, SQLDT_INT and SQLDT_UINT

--- src/char/int_storage.cpp
+++ src/char/int_storage.cpp
@@ -351,22 +351,22 @@ bool mapif_parse_itembound_retrieve(int32 fd)
 		return true;
 	}
 
-	stmt.BindColumn(0, SQLDT_INT,       &item.id,           0, nullptr, nullptr);
-	stmt.BindColumn(1, SQLDT_UINT,      &item.nameid,       0, nullptr, nullptr);
-	stmt.BindColumn(2, SQLDT_SHORT,     &item.amount,       0, nullptr, nullptr);
-	stmt.BindColumn(3, SQLDT_UINT,      &item.equip,        0, nullptr, nullptr);
+	stmt.BindColumn(0, SQLDT_INT32,       &item.id,           0, nullptr, nullptr);
+	stmt.BindColumn(1, SQLDT_UINT32,      &item.nameid,       0, nullptr, nullptr);
+	stmt.BindColumn(2, SQLDT_INT16,     &item.amount,       0, nullptr, nullptr);
+	stmt.BindColumn(3, SQLDT_UINT32,      &item.equip,        0, nullptr, nullptr);
 	stmt.BindColumn(4, SQLDT_CHAR,      &item.identify,     0, nullptr, nullptr);
 	stmt.BindColumn(5, SQLDT_CHAR,      &item.refine,       0, nullptr, nullptr);
 	stmt.BindColumn(6, SQLDT_CHAR,      &item.attribute,    0, nullptr, nullptr);
-	stmt.BindColumn(7, SQLDT_UINT,      &item.expire_time,  0, nullptr, nullptr);
+	stmt.BindColumn(7, SQLDT_UINT32,      &item.expire_time,  0, nullptr, nullptr);
 	stmt.BindColumn(8, SQLDT_CHAR,      &item.bound,        0, nullptr, nullptr);
 	stmt.BindColumn(9, SQLDT_ULONGLONG, &item.unique_id,    0, nullptr, nullptr);
 	stmt.BindColumn(10, SQLDT_INT8,     &item.enchantgrade, 0, nullptr, nullptr);
 	for( j = 0; j < MAX_SLOTS; ++j )
-		stmt.BindColumn(11+j, SQLDT_UINT, &item.card[j], 0, nullptr, nullptr);
+		stmt.BindColumn(11+j, SQLDT_UINT32, &item.card[j], 0, nullptr, nullptr);
 	for( j = 0; j < MAX_ITEM_RDM_OPT; ++j ) {
-		stmt.BindColumn(11+MAX_SLOTS+j*3, SQLDT_SHORT, &item.option[j].id, 0, nullptr, nullptr);
-		stmt.BindColumn(12+MAX_SLOTS+j*3, SQLDT_SHORT, &item.option[j].value, 0, nullptr, nullptr);
+		stmt.BindColumn(11+MAX_SLOTS+j*3, SQLDT_INT16, &item.option[j].id, 0, nullptr, nullptr);
+		stmt.BindColumn(12+MAX_SLOTS+j*3, SQLDT_INT16, &item.option[j].value, 0, nullptr, nullptr);
 		stmt.BindColumn(13+MAX_SLOTS+j*3, SQLDT_CHAR, &item.option[j].param, 0, nullptr, nullptr);
 	}
 	memset(&items, 0, sizeof(items));
