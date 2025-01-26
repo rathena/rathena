9f81f8d60658cdd9596d1ad2b3e302a6f37979b5
Playtester <3785983+Playtester@users.noreply.github.com>
2025-01-26 14:06:12 +0100

    Monster Ranges, Looters and Blind (#8936)
    - Looters now have a loot range of 12 (configurable) that is no longer reduced by Blind
    - Chase range is no longer increased when hitting a monster from outside its chase range 
      * Removed the "min_chase" custom solution
    - Blind now reduces both aggro range and chase range to 1
    - Chase range at the beginning of a chase is now only checked in Angry state (blind doesn't matter)
    - Chase range (including blind) is now checked whenever the attack timer ends
      * The attack timer can no longer be stopped by pre-planning a chase
      * This will make monsters stop attacking when outside chase range at the end of the timer
    - Monsters will now think about updating their chase already one cell before they reach the end of their path
      * This is not only official but also makes their movement and behavior smoother
      * If at this time no enemy is in chase range, the monster will drop its target but still finish moving
    - A monster's chase path will now always go all the way next to the target, even if the monster has ranged attacks
      * Monsters will now check for each cell moved if target is in attack range and then immediately attack
      * If they still have attack delay they will stop and attack when ready (if in chase range at this point)
    - Fixed a timer to delay movement during hit lock
      * Previously the timer didn't work at all and the whole mob AI had to be processed again
      * Now the monster will start moving immediately once the hit lock is gone
    - Fixed an endless loop that occurred when we call mob_randomwalk while the mob still has a target
      * This happens because we pre-plan chases with timers but the state remains Idle until the chase starts
    - Monsters now become active once a player is within 18 cells (AREA_SIZE+4)
    - Slaves now teleport back to their master if they are more than 15 cells away (AREA_SIZE+1) 
      * This only applies to AI_ABR and AI_BIONIC unless "slave_stick_with_master" is enabled
    - Improved code structure in unit.cpp: Bundling repeating code segments into new functions
    - Fixes #8914

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -1699,7 +1699,7 @@ int32 status_damage(struct block_list *src,struct block_list *target,int64 dhp, 
 		unit_remove_map(target,CLR_DEAD);
 	else { // Some death states that would normally be handled by unit_remove_map
 		unit_stop_attack(target);
-		unit_stop_walking(target,1);
+		unit_stop_walking(target,USW_FIXPOS|USW_RELEASE_TARGET);
 		unit_skillcastcancel(target,0);
 		clif_clearunit_area( *target, CLR_DEAD );
 		skill_unit_move(target,gettick(),4);
@@ -2239,16 +2239,17 @@ bool status_check_skilluse(struct block_list *src, struct block_list *target, ui
  * Checks whether the src can see the target
  * @param src:	Object using skill on target [PC|MOB|PET|HOM|MER|ELEM]
  * @param target: Object being targeted by src [PC|MOB|HOM|MER|ELEM]
- * @return src can see (1) or target is invisible (0)
+ * @param checkblind: Whether blind condition should be considered (sets view range to 1)
+ * @return src can see (true) or target is invisible (false)
  * @author [Skotlex]
  */
-int32 status_check_visibility(struct block_list *src, struct block_list *target)
+bool status_check_visibility(block_list* src, block_list* target, bool checkblind)
 {
 	int32 view_range;
 	status_change* tsc = status_get_sc(target);
 	switch (src->type) {
 		case BL_MOB:
-			view_range = ((TBL_MOB*)src)->min_chase;
+			view_range = ((TBL_MOB*)src)->db->range3;
 			break;
 		case BL_PET:
 			view_range = ((TBL_PET*)src)->db->range2;
@@ -2257,11 +2258,17 @@ int32 status_check_visibility(struct block_list *src, struct block_list *target)
 			view_range = AREA_SIZE;
 	}
 
+	if (checkblind) {
+		status_change* sc = status_get_sc(src);
+		if (sc != nullptr && sc->getSCE(SC_BLIND) != nullptr)
+			view_range = 1;
+	}
+
 	if (src->m != target->m || !check_distance_bl(src, target, view_range))
-		return 0;
+		return false;
 
 	if ( src->type == BL_NPC) // NPCs don't care for the rest
-		return 1;
+		return true;
 
 	if (tsc) {
 		bool is_boss = (status_get_class_(src) == CLASS_BOSS);
@@ -2272,24 +2279,24 @@ int32 status_check_visibility(struct block_list *src, struct block_list *target)
 					map_session_data *tsd = (TBL_PC*)target;
 
 					if (((tsc->option&(OPTION_HIDE|OPTION_CLOAK|OPTION_CHASEWALK)) || tsc->getSCE(SC_CAMOUFLAGE) || tsc->getSCE(SC_STEALTHFIELD) || tsc->getSCE(SC_SUHIDE)) && !is_boss && (tsd->special_state.perfect_hiding || !is_detector))
-						return 0;
+						return false;
 					if ((tsc->getSCE(SC_CLOAKINGEXCEED) || tsc->getSCE(SC_NEWMOON)) && !is_boss && ((tsd && tsd->special_state.perfect_hiding) || is_detector))
-						return 0;
+						return false;
 					if (tsc->getSCE(SC__FEINTBOMB) && !is_boss && !is_detector)
-						return 0;
+						return false;
 				}
 				break;
 			case BL_ELEM:
 				if (tsc->getSCE(SC_ELEMENTAL_VEIL) && !is_boss && !is_detector)
-					return 0;
+					return false;
 				break;
 			default:
 				if (((tsc->option&(OPTION_HIDE|OPTION_CLOAK|OPTION_CHASEWALK)) || tsc->getSCE(SC_CAMOUFLAGE) || tsc->getSCE(SC_STEALTHFIELD) || tsc->getSCE(SC_SUHIDE)) && !is_boss && !is_detector)
-					return 0;
+					return false;
 		}
 	}
 
-	return 1;
+	return true;
 }
 
 /**
