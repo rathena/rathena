2c3be1d173888cdf2bed7d27c9310d03e2381701
Lemongrass3110 <lemongrass@kstp.at>
2025-01-27 00:06:59 +0100

    Converted CZ_MOVE_ITEM_FROM_STORE_TO_CART to struct

--- src/map/packets.hpp
+++ src/map/packets.hpp
@@ -1769,6 +1769,13 @@ struct PACKET_CZ_SELECT_WARPPOINT{
 } __attribute__((packed));
 DEFINE_PACKET_HEADER(CZ_SELECT_WARPPOINT, 0x11b);
 
+struct PACKET_CZ_MOVE_ITEM_FROM_STORE_TO_CART{
+	int16 packetType;
+	uint16 index;
+	int32 amount;
+} __attribute__((packed));
+DEFINE_PACKET_HEADER(CZ_MOVE_ITEM_FROM_STORE_TO_CART, 0x128);
+
 // NetBSD 5 and Solaris don't like pragma pack but accept the packed attribute
 #if !defined( sun ) && ( !defined( __NETBSD__ ) || __NetBSD_Version__ >= 600000000 )
 	#pragma pack( pop )
