504f97b0954136f2af5d7dab03e9f9d9a8963bf3
Atemo <Atemo@users.noreply.github.com>
2025-02-09 23:33:54 +0100

    Converted ZC_CONFIG_NOTIFY to struct (#9050)

--- src/map/packets.hpp
+++ src/map/packets.hpp
@@ -1887,6 +1887,12 @@ struct PACKET_ZC_GM_CHECKER{
 } __attribute__((packed));
 DEFINE_PACKET_HEADER(ZC_GM_CHECKER, 0xc0c);
 
+struct PACKET_ZC_CONFIG_NOTIFY {
+	int16 packetType;
+	uint8 flag;
+} __attribute__((packed));
+DEFINE_PACKET_HEADER(ZC_CONFIG_NOTIFY, 0x2da);
+
 // NetBSD 5 and Solaris don't like pragma pack but accept the packed attribute
 #if !defined( sun ) && ( !defined( __NETBSD__ ) || __NetBSD_Version__ >= 600000000 )
 	#pragma pack( pop )
