2993b1d31c66d7c4ca49754e52e6d1ba9435a7e4
Playtester <3785983+Playtester@users.noreply.github.com>
2025-03-18 21:00:35 +0100

    Official Signum Crucis Success Chance - Success chance of Signum Crucis is now [25% + 4%*skill_lv + srcLevel - tarLevel]   (2% higher than before) - Fixes #9179

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -7852,7 +7852,7 @@ int32 skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, 
 
 	case AL_CRUCIS:
 		if (flag&1)
-			sc_start(src,bl,type, 23+skill_lv*4 +status_get_lv(src) -status_get_lv(bl), skill_lv,skill_get_time(skill_id,skill_lv));
+			sc_start(src, bl, type, 25 + skill_lv * 4 + status_get_lv(src) - status_get_lv(bl), skill_lv, skill_get_time(skill_id, skill_lv));
 		else {
 			map_foreachinallrange(skill_area_sub, src, skill_get_splash(skill_id, skill_lv), BL_CHAR,
 				src, skill_id, skill_lv, tick, flag|BCT_ENEMY|1, skill_castend_nodamage_id);
