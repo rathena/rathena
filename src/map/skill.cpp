13651d57e18730d42f55aaebe5e7eb68701b9895
Playtester <3785983+Playtester@users.noreply.github.com>
2025-03-24 21:17:34 +0100

    Walk Delay on Attack/Skill Cleanup/Fix (#9184)
    - Replaced the special "walkdelay by inactive AI on attack" solution with a separate monster_ai setting
    - Cast end and cast cancel delay events will now also go through unit_set_attackdelay
    - Monster-specific delays are now handled by a new mob_set_delay function called from unit_set_attackdelay
      * It handles setting the AI inactive on attack and cast end
      * And setting monster skill delays on cast end and cast cancel
    - Attack delays at cast end for monsters is now handled by unit_set_attackdelay instead of mobskill_end
      * Renamed mobskill_end to mobskill_delay as this function now only handles setting monster skill delays
    - Disabled attack_walk_delay by default and improved the documentation of this setting
      * This has no visible change as the client will still send movement requests delayed by amotion
      * Feel free to re-enable for "PC" if you want to prevent client edits allowing to move earlier

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -13934,16 +13934,10 @@ TIMER_FUNC(skill_castend_id){
 			break;
 
 		// These actions happen even if the skill fails except when the caster is already dead
+		unit_set_attackdelay(*src, tick, DELAY_EVENT_CASTEND);
 		if (md != nullptr) {
-			// When a monster uses a skill, its AI will be inactive for its attack motion
-			// This is also the reason why it doesn't move during this time
-			md->next_thinktime = tick + status_get_amotion(src);
-
 			if (md->skill_idx >= 0 && md->db->skill[md->skill_idx]->emotion >= ET_SURPRISE && md->db->skill[md->skill_idx]->emotion < ET_MAX)
 				clif_emotion(*src, static_cast<emotion_type>(md->db->skill[md->skill_idx]->emotion));
-
-			// Sets cooldowns and attack delay
-			mobskill_end(*md, tick);
 		}
 
 		if (!target || target->prev == nullptr)
@@ -14312,16 +14306,10 @@ TIMER_FUNC(skill_castend_pos){
 			break;
 
 		// These actions happen even if the skill fails except when the caster is already dead
+		unit_set_attackdelay(*src, tick, DELAY_EVENT_CASTEND);
 		if (md != nullptr) {
-			// When a monster uses a skill, its AI will be inactive for its attack motion
-			// This is also the reason why it doesn't move during this time
-			md->next_thinktime = tick + status_get_amotion(src);
-
 			if (md->skill_idx >= 0 && md->db->skill[md->skill_idx]->emotion >= ET_SURPRISE && md->db->skill[md->skill_idx]->emotion < ET_MAX)
 				clif_emotion(*src, static_cast<emotion_type>(md->db->skill[md->skill_idx]->emotion));
-
-			// Sets cooldowns and attack delay
-			mobskill_end(*md, tick);
 		}
 
 		if (!skill_pos_maxcount_check(src, ud->skillx, ud->skilly, ud->skill_id, ud->skill_lv, src->type, true))
