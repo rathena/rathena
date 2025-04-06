e5856372e9dc38852512200ba82b7a3efaafd251
Playtester <3785983+Playtester@users.noreply.github.com>
2025-04-06 09:02:12 +0200

    Explicit nullptr check

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -1416,7 +1416,7 @@ bool battle_status_block_damage(struct block_list *src, struct block_list *targe
 
 	if ((sc->getSCE(SC_PNEUMA) && (flag&(BF_MAGIC | BF_LONG)) == BF_LONG) ||
 #ifndef RENEWAL
-		(sc->getSCE(SC_BASILICA) && !status_bl_has_mode(src, MD_STATUSIMMUNE) && skill_id != SP_SOULEXPLOSION) ||
+		(sc->getSCE(SC_BASILICA) != nullptr && !status_bl_has_mode(src, MD_STATUSIMMUNE) && skill_id != SP_SOULEXPLOSION) ||
 #endif
 		(sc->getSCE(SC_ZEPHYR) && !(flag&BF_MAGIC && skill_id) && !(skill_get_inf(skill_id)&(INF_GROUND_SKILL | INF_SELF_SKILL))) ||
 		sc->getSCE(SC__MANHOLE) ||
