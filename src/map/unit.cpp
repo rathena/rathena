9f8540e97c60698b1a60e7bc70a675e1318ac09f
Playtester <3785983+Playtester@users.noreply.github.com>
2025-04-22 18:34:39 +0200

    Explicit nullptr check + clean int comparison as suggested
    Co-authored-by: Daegaladh <Daegaladh@users.noreply.github.com>

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -2452,9 +2452,9 @@ int32 unit_skilluse_id2(struct block_list *src, int32 target_id, uint16 skill_id
 	// In official this is triggered even if no cast time.
 	clif_skillcasting(src, src->id, target_id, 0,0, skill_id, skill_lv, skill_get_ele(skill_id, skill_lv), casttime);
 
-	if (sd && target->type == BL_MOB
+	if (sd != nullptr && target->type == BL_MOB
 #ifndef RENEWAL
-		&& (casttime > 0 || combo)
+		&& (casttime > 0 || combo > 0)
 #endif
 	) {
 		TBL_MOB *md = (TBL_MOB*)target;
