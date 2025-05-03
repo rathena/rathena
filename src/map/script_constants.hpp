ba863c11fcb488a74d53aae68841a122bd622ba2
Atemo <capucrath@gmail.com>
2025-05-03 14:50:42 +0200

    Whirlwind, Deluge and Volcano behavior * overall the behavior of these skills remains the same, only the behavior of the icon indicating the time for free cast is updated

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -1973,6 +1973,7 @@
 	export_constant(SC_CONTENTS_34);
 	export_constant(SC_CONTENTS_35);
 	export_constant(SC_NOACTION);
+	export_constant(SC_GROUNDMAGIC);
 
 /// Do not modify code below this, until the end of the API hook, since it will be automatically generated again
 /// @APIHOOK_START(EFST_CONST)
