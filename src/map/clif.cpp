31c444e59426ffcfcc2c1058706a93c6990e3e03
Haydrich <54556365+Haydrich@users.noreply.github.com>
2025-05-16 08:16:50 +0300

    Update src/map/clif.cpp
    Co-authored-by: Atemo <Atemo@users.noreply.github.com>

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -17675,10 +17675,7 @@ void clif_parse_configuration( int32 fd, map_session_data* sd ){
 			sd->hd->homunculus.autofeed = flag;
 			break;
 		case CONFIG_DISABLE_SHOWCOSTUMES:
-			if(flag == true)
-				sd->status.disable_showcostumes = false;
-			else
-				sd->status.disable_showcostumes = true;
+			sd->status.disable_showcostumes = !flag;
 			pc_set_costume_view(sd);
 			break;
 		default:
