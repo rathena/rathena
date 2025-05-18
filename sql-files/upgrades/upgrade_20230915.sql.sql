50fe2e4d6708d4da8ac5c70425ce84fd0b3c3868
ƒɾìҽɾҽղ <muramaza.prosever@live.com>
2025-05-18 18:18:31 +0700

    re  commit

---
+++ sql-files/upgrades/upgrade_20230915.sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `macro_user_report` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `reporter_aid` INT UNSIGNED NOT NULL,
+  `reported_aid` INT UNSIGNED NOT NULL,
+  `report_type` INT UNSIGNED NOT NULL,
+  `report_msg` VARCHAR(101) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
--- src/map/clif.hpp
+++ src/map/clif.hpp
@@ -858,6 +858,19 @@ enum e_changestate_pet : uint8 {
 	CHANGESTATEPET_UPDATE_EGG = 6,
 };
 
+enum e_macro_user_report_status : uint32
+{
+	MACRO_USER_REPORT_COUNTLIMIT,			// If you have exceeded the number of daily reports, you will no longer be able to report.
+	MACRO_USER_REPORT_SUCCESS,				// %s was reported.
+	MACRO_USER_REPORT_COOLTIME,				// The target is being reported by another player.
+	MACRO_USER_REPORT_EFST_ACTIVATED,		// The target is under report protection.
+	MACRO_USER_REPORT_PENALTY_COUNTMAX,		// You can no longer report beyond the cumulative number of daily reports.
+	MACRO_USER_REPORT_REPORTED,				// You have been reported by another player.
+	MACRO_USER_REPORT_NOT_USE_AREA,			// This is an area where reporting is not possible.
+
+	MACRO_USER_REPORT_INVALID,				// CUSTOM: It will cause unexpected behavior (UB) to the client.
+};
+
 int32 clif_setip(const char* ip);
 void clif_setbindip(const char* ip);
 void clif_setport(uint16 port);
