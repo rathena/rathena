f8ff8c46060e142b805c1694e6a6445ed1440de6
Haydrich <54556365+Haydrich@users.noreply.github.com>
2025-05-18 07:49:39 +0300

    Update sql-files/upgrades/upgrade_20250517.sql
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- sql-files/upgrades/upgrade_20250517.sql
+++ sql-files/upgrades/upgrade_20250517.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `mob_db`
-	ADD COLUMN `groupid` smallint(6) unsigned DEFAULT NULL AFTER `damage_taken`
+	ADD COLUMN `groupid` smallint(6) unsigned DEFAULT NULL AFTER `damage_taken`,
 	ADD COLUMN `title` text DEFAULT NULL AFTER `groupid`
 ;
 ALTER TABLE `mob_db2`
