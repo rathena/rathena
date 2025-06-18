aaf136cbf118dd9d6ab4b0821a8e677f67e78420
Absinthe1975 <benoittheret@hotmail.com>
2025-06-18 08:16:59 +0200

    Update atcommand.cpp
    deletion of unnecessary texts

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -7,9 +7,9 @@
 #include <cstdlib>
 #include <set>
 #include <unordered_map>
-#include <algorithm> // by Absinthe
-#include <cctype>    // by Absinthe
-#include <cstring>   // by Absinthe
+#include <algorithm> 
+#include <cctype>    
+#include <cstring>   
 
 #include <common/cbasetypes.hpp>
 #include <common/database.hpp>
@@ -57,7 +57,7 @@
 #include "storage.hpp"
 #include "trade.hpp"
 #include "vending.hpp"
-#include "../custom/nofx.hpp" // by Absinthe
+#include "../custom/nofx.hpp" 
 
 using namespace rathena;
 
