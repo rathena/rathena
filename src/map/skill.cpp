08847ddbe458a6f47a3af0b1945aea299d7f819d
Haydrich <54556365+Haydrich@users.noreply.github.com>
2025-06-30 16:19:37 +0300

    Implemented New Imperial Guard Skill "Imperial Pressure" (#8979)

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -5815,6 +5815,7 @@ int32 skill_castend_damage_id (struct block_list* src, struct block_list *bl, ui
 	case IQ_THIRD_CONSECRATION:
 	case IG_OVERSLASH:
 	case IG_RADIANT_SPEAR:
+	case IG_IMPERIAL_PRESSURE:
 	case CD_ARBITRIUM_ATK:
 	case CD_PETITIO:
 	case CD_FRAMEN:
@@ -5946,6 +5947,7 @@ int32 skill_castend_damage_id (struct block_list* src, struct block_list *bl, ui
 			switch ( skill_id ) {
 				case GN_CARTCANNON:
 				case SU_SCRATCH:
+				case IG_IMPERIAL_PRESSURE:
 				case BO_MAYHEMIC_THORNS:
 				case DK_DRAGONIC_BREATH:
 				case DK_HACKANDSLASHER:
