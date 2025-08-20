cbd7132142cbf5c8f233fb80b1a18b66a02d23ed
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2025-08-20 17:12:50 +0100

    Add a Strategy Pattern for skills (#9468)
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>
    Co-authored-by: vstumpf <vincents.995@gmail.com>

---
+++ src/map/skills/skill_impl.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#include "skill_impl.hpp"
+
+SkillImpl::SkillImpl(e_skill skill_id) : skill_id_(skill_id) {
+}
+
+e_skill SkillImpl::getSkillId() const {
+	return skill_id_;
+}
+
+void SkillImpl::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	// no-op
+}
+
+void SkillImpl::calculateSkillRatio(const Damage*, const block_list*, const block_list*, uint16, int32&) const {
+	// no-op
+}
+
+void SkillImpl::modifyHitRate(int16&, const block_list*, const block_list*, uint16) const {
+	// no-op
+}
+
+void SkillImpl::applyAdditionalEffects(block_list*, block_list*, uint16, t_tick, int32, enum damage_lv) const {
+	// no-op
+}
