4f39d5aa697736617c71cc739a606eac91a46f86
Lemongrass3110 <lemongrass@kstp.at>
2025-08-21 22:35:07 +0200

    Added a sample SkillCustomBash

--- src/map/skills/custom/skill_factory_custom.cpp
+++ src/map/skills/custom/skill_factory_custom.cpp
@@ -3,9 +3,16 @@
 
 #include "skill_factory_custom.hpp"
 
+#include "../swordman/bash.hpp"
+
+class SkillCustomBash;
+
 std::unique_ptr<const SkillImpl> SkillFactoryCustom::create(const e_skill skill_id) const {
 #if 0
 	switch( skill_id ){
+		case SM_BASH:
+			return std::make_unique<SkillCustomBash>();
+
 		default:
 			return nullptr;
 	}
@@ -13,3 +20,15 @@ std::unique_ptr<const SkillImpl> SkillFactoryCustom::create(const e_skill skill_
 
 	return nullptr;
 }
+
+class SkillCustomBash : public SkillBash{
+	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio) const override{
+		// Normal Bash:
+		// Base 100% + 30% per level
+		// base_skillratio += 30 * skill_lv;
+
+		// But my custom Bash Skill is stronger:
+		// Base 100% + 300% per level
+		base_skillratio += 300 * skill_lv;
+	}
+};
