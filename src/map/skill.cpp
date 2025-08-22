822973441ee1109b992034205a9a988027759635
Danilo Lemes <daniloleemes@hotmail.com>
2025-08-22 22:06:02 +0100

    revert comment to original place

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -1196,6 +1196,12 @@ struct s_skill_unit_layout *skill_get_unit_layout(uint16 skill_id, uint16 skill_
 	return &skill_unit_layout[0]; // default 1x1 layout
 }
 
+/** Stores temporary values.
+ * Common usages:
+ * [0] holds number of targets in area
+ * [1] holds the id of the original target
+ * [2] counts how many targets have been processed. counter is added in skill_area_sub if the foreach function flag is: flag&(SD_SPLASH|SD_PREAMBLE)
+ */
 int32 skill_area_temp[8];
 
 /*==========================================
