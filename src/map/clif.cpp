ad9ea6644226dd0eeea5a795d52e61b49336a42d
thanna <dahakathanna@gmail.com>
2025-08-22 14:56:08 +0800

    Add map_data null check.

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -21468,6 +21468,8 @@ void clif_hat_effects( block_list& bl, enum send_target target, block_list& tbl 
 /// 0A3B <Length>.W <AID>.L <Status>.B { <HatEffectId>.W } (ZC_EQUIPMENT_EFFECT)
 void clif_hat_effect_single( block_list& bl, uint16 effectId, bool enable ){
 #if PACKETVER_MAIN_NUM >= 20150507 || PACKETVER_RE_NUM >= 20150429 || defined(PACKETVER_ZERO)
+	struct map_data* mdata = map_getmapdata(bl.m);
+	if (!mdata) return; // or handle gracefully
 	if( map_getmapdata( bl.m )->getMapFlag( MF_NOCOSTUME ) ){
 		return;
 	}
