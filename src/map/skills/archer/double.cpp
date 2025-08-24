29e80773d22787bf7fc7341fdf709bda886c4054
Danilo Lemes <daniloleemes@hotmail.com>
2025-08-24 23:10:07 +0100

    arrow shower splash

--- src/map/skills/archer/double.cpp
+++ src/map/skills/archer/double.cpp
@@ -3,9 +3,9 @@
 
 #include "double.hpp"
 
-SkillDouble::SkillDouble() : WeaponSkillImpl(AC_DOUBLE) {
+SkillDoubleStrafe::SkillDoubleStrafe() : WeaponSkillImpl(AC_DOUBLE) {
 }
 
-void SkillDouble::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio) const {
+void SkillDoubleStrafe::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio) const {
 	base_skillratio += 10 * (skill_lv - 1);
 }
