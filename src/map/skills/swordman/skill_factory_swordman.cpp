e5e4931c81184e544f65d0b8bacdf83b22a66112
Lemongrass3110 <lemongrass@kstp.at>
2025-08-24 21:26:50 +0200

    Added skill factories for all jobs (#9505)
    Renamed Swordsman => Swordman
    Added skill factories for all jobs
    Added a factory for custom skills
    Added a factory for NPC skills
    Added a sample SkillCustomBash
    Added castendNoDamageId

--- src/map/skills/swordman/skill_factory_swordman.cpp
+++ src/map/skills/swordman/skill_factory_swordman.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
 // For more information, see LICENCE in the main folder
 
-#include "skill_factory_swordsman.hpp"
+#include "skill_factory_swordman.hpp"
 
 #include "bash.hpp"
 
-std::unique_ptr<const SkillImpl> SkillFactorySwordsman::create(const e_skill skill_id) const {
+std::unique_ptr<const SkillImpl> SkillFactorySwordman::create(const e_skill skill_id) const {
 	switch( skill_id ){
 		case SM_BASH:
 			return std::make_unique<SkillBash>();
