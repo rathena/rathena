aa87e6587d7e78b1e9964f545a0a8412a65612dc
rhrlima <ricardo.hrlima@gmail.com>
2025-09-01 23:08:44 -0300

    fix: point PR to master #4

--- 3rdparty/libconfig/grammar.c
+++ 3rdparty/libconfig/grammar.c
@@ -91,10 +91,12 @@
 
 #include <malloc.h>
 #endif
-#include "grammar.h"
 #include "parsectx.h"
 #include "scanctx.h"
-#include "scanner.h"
+
+/* these delcarations are provided to suppress compiler warnings */
+extern int libconfig_yylex();
+extern int libconfig_yyget_lineno();
 
 static const char *err_array_elem_type = "mismatched element type in array";
 static const char *err_duplicate_setting = "duplicate setting name";
--- 3rdparty/yaml-cpp/src/emitterutils.cpp
+++ 3rdparty/yaml-cpp/src/emitterutils.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cstdint>
 #include <iomanip>
 #include <sstream>
 
