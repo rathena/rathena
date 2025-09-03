e463c0a575bc260ba0b83b6c6cbd9e19f0755069
Haydrich <54556365+Haydrich@users.noreply.github.com>
2025-09-03 23:17:44 +0300

    Update

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -842,6 +842,7 @@
 	export_constant2("bAddItemSPHealRate", SP_ADD_ITEM_SPHEAL_RATE);
 	export_constant2("bAddItemGroupSPHealRate", SP_ADD_ITEMGROUP_SPHEAL_RATE);
 	export_constant2("bWeaponSubSize", SP_WEAPON_SUBSIZE);
+	export_constant2("bNormalAtkRate", SP_NORMAL_ATK_RATE);
 
 	/* equip indices */
 	export_constant(EQI_COMPOUND_ON);
