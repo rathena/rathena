37d8f0125417f62b82408d3a83fbff51c1eba04a
Danilo Lemes <daniloleemes@hotmail.com>
2025-09-13 15:27:00 +0100

    convert indent to tab

--- src/map/skills/archer/double.hpp
+++ src/map/skills/archer/double.hpp
@@ -6,9 +6,10 @@
 #include "../weapon_skill_impl.hpp"
 #include "../../battle.hpp"
 
-class SkillDoubleStrafe : public WeaponSkillImpl {
+class SkillDoubleStrafe : public WeaponSkillImpl
+{
 public:
     SkillDoubleStrafe();
-    
-    void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio) const override;
+
+    void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const override;
 };
--- src/map/skills/archer/shower.hpp
+++ src/map/skills/archer/shower.hpp
@@ -6,10 +6,11 @@
 #include "../skill_impl.hpp"
 #include "../../battle.hpp"
 
-class SkillArrowShower : public SkillImpl {
+class SkillArrowShower : public SkillImpl
+{
 public:
-    SkillArrowShower();
-    
-    void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio) const override;
-    void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
+	SkillArrowShower();
+
+	void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const override;
+	void castendDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 flag) const override;
 };
