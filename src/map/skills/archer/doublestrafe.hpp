c36f966c80479297e70664858092b7521f301d4a
Danilo Lemes <daniloleemes@hotmail.com>
2025-09-13 15:46:09 +0100

    replace spaces again

--- src/map/skills/archer/doublestrafe.hpp
+++ src/map/skills/archer/doublestrafe.hpp
@@ -9,7 +9,7 @@
 class SkillDoubleStrafe : public WeaponSkillImpl
 {
 public:
-    SkillDoubleStrafe();
+	SkillDoubleStrafe();
 
-    void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const override;
+	void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const override;
 };
