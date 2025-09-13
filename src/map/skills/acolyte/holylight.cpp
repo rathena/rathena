df946fa401ab02dd28dc58f03fe86f02e1415a7f
Danilo Lemes <daniloleemes@hotmail.com>
2025-09-13 22:09:50 +0100

    fallthrough for holylight

--- src/map/skills/acolyte/holylight.cpp
+++ src/map/skills/acolyte/holylight.cpp
@@ -3,12 +3,13 @@
 
 #include "holylight.hpp"
 
-#include "../../status.hpp"
 #include "../../clif.hpp"
+#include "../../status.hpp"
 
 SkillHolyLight::SkillHolyLight() : SkillImpl(AL_HOLYLIGHT) {
 }
 
-void SkillHolyLight::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+void SkillHolyLight::castendDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 flag) const {
 	status_change_end(target, SC_P_ALTER);
+	skill_attack(BF_MAGIC, src, src, target, getSkillId(), skill_lv, tick, flag);
 }
