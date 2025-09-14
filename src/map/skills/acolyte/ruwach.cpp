43bca01c1cc17eaf99f6a03f63609e1266b6ca50
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2025-09-14 10:04:54 +0100

    Apply suggestion from @Lemongrass3110
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/acolyte/ruwach.cpp
+++ src/map/skills/acolyte/ruwach.cpp
@@ -12,7 +12,6 @@ SkillRuwach::SkillRuwach() : SkillImpl(AL_RUWACH)
 
 void SkillRuwach::castendNoDamageId(struct block_list *src, struct block_list *bl, uint16 skill_lv, t_tick tick, int32 flag) const
 {
-	status_change *tsc = status_get_sc(bl);
 	sc_type type = skill_get_sc(getSkillId());
 
 	clif_skill_nodamage(src, *bl, getSkillId(), skill_lv, sc_start2(src, bl, type, 100, skill_lv, getSkillId(), skill_get_time(getSkillId(), skill_lv)));
