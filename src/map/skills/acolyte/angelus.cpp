6e38597c8d394b93d79b967903fd476ca36f299b
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2025-09-14 10:01:49 +0100

    Apply suggestion from @Lemongrass3110
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/acolyte/angelus.cpp
+++ src/map/skills/acolyte/angelus.cpp
@@ -14,7 +14,6 @@ SkillAngelus::SkillAngelus() : SkillImpl(AL_ANGELUS)
 void SkillAngelus::castendNoDamageId(struct block_list *src, struct block_list *bl, uint16 skill_lv, t_tick tick, int32 flag) const
 {
 	map_session_data *sd = BL_CAST(BL_PC, src);
-	sc_type type = skill_get_sc(getSkillId());
 	status_change* sc = status_get_sc(src);
 
 	if (sd == nullptr || sd->status.party_id == 0 || (flag & 1))
