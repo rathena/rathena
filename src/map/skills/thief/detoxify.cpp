1e92913523def490da0012d30da72e7a28e87078
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2025-09-16 10:15:27 +0100

    Apply suggestion from @Lemongrass3110
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/thief/detoxify.cpp
+++ src/map/skills/thief/detoxify.cpp
@@ -9,7 +9,7 @@
 SkillDetoxify::SkillDetoxify() : SkillImpl(TF_DETOXIFY) {
 }
 
-void SkillDetoxify::castendNoDamageId(struct block_list *src, struct block_list *bl, uint16 skill_lv, t_tick tick, int32 &flag) const {
+void SkillDetoxify::castendNoDamageId(block_list *src, block_list *bl, uint16 skill_lv, t_tick tick, int32 &flag) const {
 	clif_skill_nodamage(src, *bl, getSkillId(), skill_lv);
 	status_change_end(bl, SC_POISON);
 	status_change_end(bl, SC_DPOISON);
