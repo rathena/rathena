8f0751ba2a9af364bdbb6bd4a59f4a530fcd70f3
gidzdlcrz <62187272+gidzdlcrz@users.noreply.github.com>
2025-11-04 22:20:56 +0800

    Update src/map/atcommand.cpp
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -1968,8 +1968,10 @@ ACMD_FUNC(bodystyle){
 	}
 
 	if( message == nullptr || !*message ){
-		clif_displaymessage( fd, msg_txt( sd, 739 ) ); // Please enter a body style (usage: @bodystyle <job ID>)
-		clif_displaymessage( fd, msg_txt( sd, 1541 ) ); // Or @bodystyle <off> to restore default job body style.
+		if( const char* help = atcommand_help_string( command ); help != nullptr ){
+			clif_displaymessage( fd, help );
+		}
+
 		return -1;
 	}
 
