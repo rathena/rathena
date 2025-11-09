bcb42c105af166f19fca61e345297a43db390cc6
Danilo Lemes <daniloleemes@hotmail.com>
2025-11-09 18:43:59 +0000

    comments on tk_run

--- src/map/skills/taekwon/run.cpp
+++ src/map/skills/taekwon/run.cpp
@@ -11,7 +11,8 @@ SkillRun::SkillRun() : SkillImpl(TK_RUN) {
 
 void SkillRun::castendNoDamageId(block_list *src, block_list *bl, uint16 skill_lv, t_tick tick, int32 &flag) const {
 	sc_type type = skill_get_sc(getSkillId());
-	status_change_entry *tsce = status_get_sc(bl)->getSCE(SC_RUN);
+	status_change *tsc = status_get_sc(bl);
+	status_change_entry *tsce = (tsc && type != SC_NONE) ? tsc->getSCE(type) : nullptr;
 	map_session_data *sd = BL_CAST(BL_PC, src);
 
 	if (tsce) {
