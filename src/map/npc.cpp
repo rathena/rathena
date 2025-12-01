568a8dcbdae3ca03400662cc9a5d7a244636e089
Roger <darklord.dl@gmail.com>
2025-12-01 12:24:59 -0300

    corrected compiling

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -346,8 +346,8 @@ uint64 StylistDatabase::parseBodyNode( const ryml::NodeRef& node ){
 						return 0;
 					}
 
-					if (!job_db.exists(job_id) && (job_id <= JOB_SECOND_JOB_START || job_id >= JOB_SECOND_JOB_END)) {
-						this->invalidWarning(optionNode["Value"], "stylist_parseBodyNode: Job Body %s does not exist.\n", job_id);
+					if (!job_db.exists(static_cast<uint16>(job_id)) && (job_id <= JOB_SECOND_JOB_START || job_id >= JOB_SECOND_JOB_END)) {
+						this->invalidWarning(optionNode["Value"], "stylist_parseBodyNode: Job Body \"%u\" does not exist.\n", job_id);
 						return 0;
 					}
 
@@ -370,8 +370,8 @@ uint64 StylistDatabase::parseBodyNode( const ryml::NodeRef& node ){
 				return 0;
 			}
 
-			if (!job_db.exists(RequiredJob)) {
-				this->invalidWarning(node["RequiredJob"], "stylist_parseBodyNode: Job %s does not exist.\n", RequiredJob);
+			if (!job_db.exists(static_cast<uint16>(RequiredJob))) {
+				this->invalidWarning(node["RequiredJob"], "stylist_parseBodyNode: Job \"%u\" does not exist.\n", RequiredJob);
 				return 0;
 			}
 
