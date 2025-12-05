cc8bc14c15e3a4438a6f618098bd45d46324e852
Atemo <capucrath@gmail.com>
2025-12-05 23:17:05 +0100

    * Thundering Cannon - Reworks skill to become target skill and also deal damage to surrounding enemies around the target. - Area of effect is 7 x 7 cells based on level 10. - Increases cast range from 1 cell to 9 cells. - Reduces SP consumption from 92 to 80 based on level 10.
    Skipped (updated next rebalance)
    - Reduces base damage from 13600%Matk to 10000%Matk based on level 10.

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -5851,6 +5851,7 @@ int32 skill_castend_damage_id (block_list* src, block_list *bl, uint16 skill_id,
 	case SKE_STAR_LIGHT_KICK:
 	case SS_KAGEGISSEN:
 	case SS_SEKIENHOU:
+	case SS_RAIDENPOU:
 		if( flag&1 ) {//Recursive invocation
 			int32 sflag = skill_area_temp[0] & 0xFFF;
 			int32 heal = 0;
@@ -15469,26 +15470,6 @@ int32 skill_castend_pos2(block_list* src, int32 x, int32 y, uint16 skill_id, uin
 	case SS_KUNAIKUSSETSU:
 		map_foreachinallrange(skill_detonator, src, skill_get_splash(skill_id, skill_lv), BL_SKILL, src, skill_lv);
 		clif_skill_nodamage(src, *src, skill_id, skill_lv);
-		break;
-	case SS_RAIDENPOU:
-		skill_area_temp[1] = 0;
-		skill_mirage_cast(*src, nullptr, SS_ANTENPOU, skill_lv, x, y, tick, flag | BCT_WOS);
-		if (map_getcell(src->m, x, y, CELL_CHKLANDPROTECTOR)) {
-			clif_skill_fail( *sd, skill_id, USESKILL_FAIL );
-			return 0;
-		}
-		clif_skill_nodamage(src, *src, skill_id, skill_lv);
-		if (battle_config.skill_eightpath_algorithm) {
-			//Use official AoE algorithm
-			map_foreachindir(skill_attack_area, src->m, src->x, src->y, x, y,
-				skill_get_splash(skill_id, skill_lv), skill_get_maxcount(skill_id, skill_lv), 0, splash_target(src),
-				skill_get_type(skill_id), src, src, skill_id, skill_lv, tick, flag, BCT_ENEMY);
-		}
-		else {
-			map_foreachinpath(skill_attack_area, src->m, src->x, src->y, x, y,
-				skill_get_splash(skill_id, skill_lv), skill_get_maxcount(skill_id, skill_lv), splash_target(src),
-				skill_get_type(skill_id), src, src, skill_id, skill_lv, tick, flag, BCT_ENEMY);
-		}
 		break;
 	case SS_SHINKIROU:
 		flag |= 1;
