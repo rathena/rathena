9e783f46e9fd5b3cb6012d778651b4d2c338ad0d
Lemongrass3110 <lemongrass@kstp.at>
2026-01-05 00:31:34 +0100

    Fixed a CodeQL finding

--- src/map/skills/gunslinger/piercingshot.cpp
+++ src/map/skills/gunslinger/piercingshot.cpp
@@ -10,9 +10,9 @@ SkillPiercingShot::SkillPiercingShot() : WeaponSkillImpl(GS_PIERCINGSHOT) {
 }
 
 void SkillPiercingShot::calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio, int32 mflag) const {
-	const map_session_data *sd = BL_CAST(BL_PC, src);
-
 #ifdef RENEWAL
+	const map_session_data* sd = BL_CAST(BL_PC, src);
+
 	if (sd && sd->weapontype1 == W_RIFLE)
 		base_skillratio += 150 + 30 * skill_lv;
 	else
