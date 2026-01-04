b082932369baa252f1e4d3850746610c09651f09
Lemongrass3110 <lemongrass@kstp.at>
2026-01-05 00:30:33 +0100

    Spreadattack uses the default SkillImplRecursiveDamageSplash
    There was old (wrong and unused) legacy eAthena code, which was identified and verified with the help of @Playtester
    The skill was never a (no damage) self skill, that targeted around the caster.
    You have to select a target and the damage is splashed around the target (splash range depends on the skill level).
    This is also reflected in both pre-renewal and renewal skill db and therefore the current skill_castend_nodamage_id code is never triggered.

--- src/map/skills/gunslinger/spreadattack.cpp
+++ src/map/skills/gunslinger/spreadattack.cpp
@@ -15,16 +15,3 @@ void SkillSpreadAttack::calculateSkillRatio(const Damage *wd, const block_list *
 	base_skillratio += 20 * (skill_lv - 1);
 #endif
 }
-
-void SkillSpreadAttack::castendDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 &flag) const {
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillSpreadAttack::castendNoDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 &flag) const {
-	int32 starget = BL_CHAR | BL_SKILL;
-
-	skill_area_temp[1] = 0;
-	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	map_foreachinrange(skill_area_sub, target, skill_get_splash(getSkillId(), skill_lv), starget, src, getSkillId(), skill_lv, tick, flag | BCT_ENEMY | SD_SPLASH | 1,
-	                   skill_castend_damage_id);
-}
