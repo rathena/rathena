dd469aa131ac437d55d87890a8d61869c7d13677
Lemongrass3110 <lemongrass@kstp.at>
2026-01-05 01:51:01 +0100

    getSkillId() in GS_DISARM

--- src/map/skills/gunslinger/disarm.cpp
+++ src/map/skills/gunslinger/disarm.cpp
@@ -10,5 +10,5 @@ SkillDisarm::SkillDisarm() : WeaponSkillImpl(GS_DISARM) {
 
 void SkillDisarm::applyAdditionalEffects(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 attack_type, enum damage_lv dmg_lv) const {
 	skill_strip_equip(src, target, getSkillId(), skill_lv);
-	clif_skill_nodamage(src, *target, GS_DISARM, skill_lv);
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
 }
