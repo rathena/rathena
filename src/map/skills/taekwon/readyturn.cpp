fb813917c2612638352d1172ca60c44a6f9eeeb8
Lemongrass3110 <lemongrass@kstp.at>
2026-01-06 17:28:42 +0100

    Readyturn dynamic sc_type

--- src/map/skills/taekwon/readyturn.cpp
+++ src/map/skills/taekwon/readyturn.cpp
@@ -10,12 +10,13 @@ SkillReadyTurn::SkillReadyTurn() : SkillImpl(TK_READYTURN) {
 }
 
 void SkillReadyTurn::castendNoDamageId(block_list *src, block_list *bl, uint16 skill_lv, t_tick tick, int32 &flag) const {
-	status_change_entry *tsce = status_get_sc(bl)->getSCE(SC_READYTURN);
+	sc_type type = skill_get_sc(getSkillId());
+	status_change_entry *tsce = status_get_sc(bl)->getSCE(type);
 
 	if (tsce) {
-		clif_skill_nodamage(src, *bl, getSkillId(), skill_lv, status_change_end(bl, SC_READYTURN));
+		clif_skill_nodamage(src, *bl, getSkillId(), skill_lv, status_change_end(bl, type));
 		return;
 	}
 
-	clif_skill_nodamage(src, *bl, getSkillId(), skill_lv, sc_start(src, bl, SC_READYTURN, 100, skill_lv, skill_get_time(getSkillId(), skill_lv)));
+	clif_skill_nodamage(src, *bl, getSkillId(), skill_lv, sc_start(src, bl, type, 100, skill_lv, skill_get_time(getSkillId(), skill_lv)));
 }
