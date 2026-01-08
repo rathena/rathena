2548a1e2fff9bea9db3e34b6bbf1b1d92a8d0eac
Lemongrass3110 <lemongrass@kstp.at>
2026-01-08 23:41:08 +0100

    AL_HOLYLIGHT

--- src/map/skills/acolyte/holylight.hpp
+++ src/map/skills/acolyte/holylight.hpp
@@ -10,4 +10,5 @@ public:
 	SkillHolyLight();
 	
 	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
+	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
 };
