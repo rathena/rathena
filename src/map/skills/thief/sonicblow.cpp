3ee34be3aca203d833c9684ceca5d738beacf0fd
Atemo <capucrath@gmail.com>
2026-01-14 20:23:37 +0100

    Moved variables

--- src/map/skills/thief/sonicblow.cpp
+++ src/map/skills/thief/sonicblow.cpp
@@ -12,14 +12,15 @@ SkillSonicBlow::SkillSonicBlow() : WeaponSkillImpl(AS_SONICBLOW) {
 }
 
 void SkillSonicBlow::calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio, int32 mflag) const {
-	const status_data* tstatus = status_get_status_data(*target);
-	const map_session_data* sd = BL_CAST( BL_PC, src );
-
 #ifdef RENEWAL
+	const status_data* tstatus = status_get_status_data(*target);
+
 	base_skillratio += 100 + 100 * skill_lv;
 	if (tstatus->hp < (tstatus->max_hp / 2))
 		base_skillratio += base_skillratio / 2;
 #else
+	const map_session_data* sd = BL_CAST( BL_PC, src );
+
 	base_skillratio += 200 + 50 * skill_lv;
 	if (sd && pc_checkskill(sd, AS_SONICACCEL) > 0)
 		base_skillratio += base_skillratio / 10;
