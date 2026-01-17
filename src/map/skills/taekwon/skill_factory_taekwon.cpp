4d9e91c877911559ce47c7ff9fccf1f4d39706de
Atemo <capucrath@gmail.com>
2026-01-18 00:07:01 +0100

    Updated skill_factory_taekwon.cpp

--- src/map/skills/taekwon/skill_factory_taekwon.cpp
+++ src/map/skills/taekwon/skill_factory_taekwon.cpp
@@ -8,11 +8,37 @@
 
 #include "counter.hpp"
 #include "downkick.hpp"
+#include "eska.hpp"
+#include "eske.hpp"
+#include "esma.hpp"
+#include "estin.hpp"
+#include "estun.hpp"
+#include "eswoo.hpp"
 #include "highjump.hpp"
 #include "jumpkick.hpp"
+#include "kaahi.hpp"
+#include "kaite.hpp"
+#include "kaizel.hpp"
+#include "kaupe.hpp"
 #include "mission.hpp"
 #include "run.hpp"
 #include "sevenwind.hpp"
+#include "spiritofrebirth.hpp"
+#include "spiritofthealchemist.hpp"
+#include "spiritoftheartist.hpp"
+#include "spiritoftheassasin.hpp"
+#include "spiritoftheblacksmith.hpp"
+#include "spiritofthecrusader.hpp"
+#include "spiritofthehunter.hpp"
+#include "spiritoftheknight.hpp"
+#include "spiritofthemonk.hpp"
+#include "spiritofthepriest.hpp"
+#include "spiritoftherogue.hpp"
+#include "spiritofthesage.hpp"
+#include "spiritofthesoullinker.hpp"
+#include "spiritofthestargladiator.hpp"
+#include "spiritofthesupernovice.hpp"
+#include "spiritofthewizard.hpp"
 #include "stormkick.hpp"
 #include "turnkick.hpp"
 
@@ -52,6 +78,58 @@ std::unique_ptr<const SkillImpl> SkillFactoryTaekwon::create(const e_skill skill
 			return std::make_unique<WeaponSkillImpl>(skill_id);
 		case SKE_RISING_MOON:
 			return std::make_unique<WeaponSkillImpl>(skill_id);
+		case SL_ALCHEMIST:
+			return std::make_unique<SkillSpiritoftheAlchemist>();
+		case SL_ASSASIN:
+			return std::make_unique<SkillSpiritoftheAssasin>();
+		case SL_BARDDANCER:
+			return std::make_unique<SkillSpiritoftheArtist>();
+		case SL_BLACKSMITH:
+			return std::make_unique<SkillSpiritoftheBlacksmith>();
+		case SL_CRUSADER:
+			return std::make_unique<SkillSpiritoftheCrusader>();
+		case SL_HIGH:
+			return std::make_unique<SkillSpiritofRebirth>();
+		case SL_HUNTER:
+			return std::make_unique<SkillSpiritoftheHunter>();
+		case SL_KAAHI:
+			return std::make_unique<SkillKaahi>();
+		case SL_KAITE:
+			return std::make_unique<SkillKaite>();
+		case SL_KAIZEL:
+			return std::make_unique<SkillKaizel>();
+		case SL_KAUPE:
+			return std::make_unique<SkillKaupe>();
+		case SL_KNIGHT:
+			return std::make_unique<SkillSpiritoftheKnight>();
+		case SL_MONK:
+			return std::make_unique<SkillSpiritoftheMonk>();
+		case SL_PRIEST:
+			return std::make_unique<SkillSpiritofthePriest>();
+		case SL_ROGUE:
+			return std::make_unique<SkillSpiritoftheRogue>();
+		case SL_SAGE:
+			return std::make_unique<SkillSpiritoftheSage>();
+		case SL_SKA:
+			return std::make_unique<SkillEska>();
+		case SL_SKE:
+			return std::make_unique<SkillEske>();
+		case SL_SMA:
+			return std::make_unique<SkillEsma>();
+		case SL_SOULLINKER:
+			return std::make_unique<SkillSpiritoftheSoulLinker>();
+		case SL_STAR:
+			return std::make_unique<SkillSpiritoftheStarGladiator>();
+		case SL_STIN:
+			return std::make_unique<SkillEstin>();
+		case SL_STUN:
+			return std::make_unique<SkillEstun>();
+		case SL_SUPERNOVICE:
+			return std::make_unique<SkillSpiritoftheSupernovice>();
+		case SL_SWOO:
+			return std::make_unique<SkillEswoo>();
+		case SL_WIZARD:
+			return std::make_unique<SkillSpiritoftheWizard>();
 		case SP_SOULREAPER:
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case TK_COUNTER:
