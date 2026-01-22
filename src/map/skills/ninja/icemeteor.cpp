ece6efb158f9f0a9904361e2fe4cf8fa2167afd3
Atemo <capucrath@gmail.com>
2026-01-22 22:53:17 +0100

    Missing castendPos2 for NJ_HYOUSYOURAKU (ice meteor)

--- src/map/skills/ninja/icemeteor.cpp
+++ src/map/skills/ninja/icemeteor.cpp
@@ -17,6 +17,11 @@ void SkillIceMeteor::calculateSkillRatio(const Damage *wd, const block_list *src
 		base_skillratio += 100 * sd->spiritcharm;
 }
 
+void SkillIceMeteor::castendPos2(block_list* src, int32 x, int32 y, uint16 skill_lv, t_tick tick, int32& flag) const {
+	flag|=1;//Set flag to 1 to prevent deleting ammo (it will be deleted on group-delete).
+	skill_unitsetting(src,getSkillId(),skill_lv,x,y,0);
+}
+
 void SkillIceMeteor::applyAdditionalEffects(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 attack_type, enum damage_lv dmg_lv) const {
 	sc_start(src,target,SC_FREEZE,(10+10*skill_lv),skill_lv,skill_get_time2(getSkillId(),skill_lv));
 }
