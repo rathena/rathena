b7608b254892db1eed1c56c87a26982046eda6e7
Atemo <capucrath@gmail.com>
2026-02-01 11:40:05 +0100

    Updated KR_ICE_SPLASH Splash skill around a single target BaseLevel modifier doesn't depend on SC_TRUTH_OF_ICE INT ratio 4 clif_skill_nodamage called before splashSearch

--- src/map/skills/druid/icesplash.cpp
+++ src/map/skills/druid/icesplash.cpp
@@ -3,32 +3,30 @@
 
 #include "icesplash.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
 SkillIceSplash::SkillIceSplash() : SkillImplRecursiveDamageSplash(KR_ICE_SPLASH) {
 }
 
-void SkillIceSplash::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillIceSplash::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+void SkillIceSplash::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_change* sc = status_get_sc(src);
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 1140 + 70 * (skill_lv - 1);
+	skillratio += -100 + 1140 + 70 * (skill_lv - 1);
+
 	if (sc != nullptr && sc->hasSCE(SC_TRUTH_OF_ICE)) {
-		skillratio += sstatus->int_; // TODO - unknown scaling [munkrej]
-		RE_LVL_DMOD(100);
+		skillratio += 4 * sstatus->int_;
 	}
-	base_skillratio += -100 + skillratio;
+
+	// Unlike what the description indicates, the BaseLevel modifier is not part of the condition on SC_TRUTH_OF_ICE
+	RE_LVL_DMOD(100);
 }
 
-int64 SkillIceSplash::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+void SkillIceSplash::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
--- src/map/skills/druid/icesplash.hpp
+++ src/map/skills/druid/icesplash.hpp
@@ -9,7 +9,6 @@ class SkillIceSplash : public SkillImplRecursiveDamageSplash {
 public:
 	SkillIceSplash();
 
-	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
-	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
-	int64 splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
+	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const override;
+	void splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
 };
