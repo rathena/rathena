8c9982b3aacf3b555d76b6ea8e56a0f6d93b0261
Atemo <capucrath@gmail.com>
2026-02-05 15:09:02 +0100

    Updated DR_CRUEL_BITE Weapon skill single targetted HitCount 2 str ratio 4

--- src/map/skills/druid/cruelbite.cpp
+++ src/map/skills/druid/cruelbite.cpp
@@ -3,40 +3,40 @@
 
 #include "cruelbite.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/pc.hpp"
 #include "map/unit.hpp"
 
-SkillCruelBite::SkillCruelBite() : SkillImplRecursiveDamageSplash(DR_CRUEL_BITE) {
+SkillCruelBite::SkillCruelBite() : WeaponSkillImpl(DR_CRUEL_BITE) {
 }
 
-void SkillCruelBite::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	if (!(flag & 1)) {
-		if (!unit_movepos(src, target->x, target->y, 2, true)) {
-			map_session_data *sd = BL_CAST(BL_PC, src);
-			if (sd) {
-				clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
-			}
-			return;
-		}
-		clif_blown(src);
-	}
-	skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
-}
-
-void SkillCruelBite::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+void SkillCruelBite::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_change* sc = status_get_sc(src);
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 60 * skill_lv;
+	skillratio += -100 + 60 * skill_lv;
+
 	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
 		skillratio += 20 * skill_lv;
 	}
-	skillratio += sstatus->str; // TODO - unknown scaling [munkrej]
+
+	skillratio += 4 * sstatus->str;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
+}
+
+void SkillCruelBite::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	if (!unit_movepos(src, target->x, target->y, 2, true)) {
+		if (map_session_data *sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
+		}
+		return;
+	}
+
+	clif_blown(src);
+	WeaponSkillImpl::castendDamageId(src, target, skill_lv, tick, flag);
 }
--- src/map/skills/druid/cruelbite.hpp
+++ src/map/skills/druid/cruelbite.hpp
@@ -3,12 +3,12 @@
 
 #pragma once
 
-#include "../skill_impl.hpp"
+#include "../weapon_skill_impl.hpp"
 
-class SkillCruelBite : public SkillImplRecursiveDamageSplash {
+class SkillCruelBite : public WeaponSkillImpl {
 public:
 	SkillCruelBite();
 
+	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const override;
 	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
-	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
 };
