b178dcca116a03c06f8035367487f14eb8ba0681
Atemo <capucrath@gmail.com>
2026-02-05 15:50:12 +0100

    Updated DR_SHOOTING_FEATHER HitCount 2 dex ratio 3 Weapon skill single targetted without splash damage

--- src/map/skills/druid/shootingfeather.cpp
+++ src/map/skills/druid/shootingfeather.cpp
@@ -3,29 +3,31 @@
 
 #include "shootingfeather.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
-SkillShootingFeather::SkillShootingFeather() : SkillImplRecursiveDamageSplash(DR_SHOOTING_FEATHER) {
+SkillShootingFeather::SkillShootingFeather() : WeaponSkillImpl(DR_SHOOTING_FEATHER) {
 }
 
-void SkillShootingFeather::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
-}
-
-void SkillShootingFeather::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+void SkillShootingFeather::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_change* sc = status_get_sc(src);
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 20 * skill_lv;
+	skillratio += -100 + 20 * skill_lv;
+
 	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 20 * skill_lv;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
+
+	skillratio += 3 * sstatus->dex;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
+}
+
+void SkillShootingFeather::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	WeaponSkillImpl::castendDamageId(src, target, skill_lv, tick, flag);
 }
