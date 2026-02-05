30643675e98f5e94c9bcceeb04317b4b3c3990c9
Atemo <capucrath@gmail.com>
2026-02-05 16:00:44 +0100

    Updated DR_LOW_FLIGHT dex ratio 4 HitCount 3 Weapon skill single targetted with splash damage

--- src/map/skills/druid/lowflight.cpp
+++ src/map/skills/druid/lowflight.cpp
@@ -3,6 +3,8 @@
 
 #include "lowflight.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/pc.hpp"
 #include "map/status.hpp"
@@ -11,33 +13,31 @@
 SkillLowFlight::SkillLowFlight() : SkillImplRecursiveDamageSplash(DR_LOW_FLIGHT) {
 }
 
-void SkillLowFlight::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	if (!(flag & 1)) {
-		if (!unit_movepos(src, target->x, target->y, 2, true)) {
-			map_session_data* sd = BL_CAST(BL_PC, src);
-			if (sd) {
-				clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
-			}
-			return;
-		}
-		clif_blown(src);
-	}
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillLowFlight::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+void SkillLowFlight::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_change* sc = status_get_sc(src);
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 20 * skill_lv;
+	skillratio += -100 + 20 * skill_lv;
+
 	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 20 * skill_lv;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
+
+	skillratio += 4 * sstatus->dex;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
+}
+
+void SkillLowFlight::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	if (!unit_movepos(src, target->x, target->y, 2, true)) {
+		if (map_session_data *sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
+		}
+		return;
+	}
+	clif_blown(src);
+
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
