a6a329956a71aaf8ee42e94fbbc450fc0d03905a
Atemo <capucrath@gmail.com>
2026-02-05 15:41:11 +0100

    Updated DR_HUNGER HitCount 1 str ratio 3 Weapon skill single targetted

--- src/map/skills/druid/hunger.cpp
+++ src/map/skills/druid/hunger.cpp
@@ -3,18 +3,33 @@
 
 #include "hunger.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
-SkillHunger::SkillHunger() : SkillImplRecursiveDamageSplash(DR_HUNGER) {
+SkillHunger::SkillHunger() : SkillImpl(DR_HUNGER) {
+}
+
+void SkillHunger::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	const status_change* sc = status_get_sc(src);
+	const status_data* sstatus = status_get_status_data(*src);
+
+	skillratio += -100 + 80 * skill_lv;
+
+	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
+		skillratio += 40 * skill_lv;
+	}
+
+	skillratio += 3 * sstatus->str;
+
+	RE_LVL_DMOD(100);
 }
 
 void SkillHunger::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
 
-	int64 damage = skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+	int64 damage = skill_attack(BF_WEAPON, src, src, target, getSkillId(), skill_lv, tick, flag);
 	if (damage > 0) {
 		int32 rate = (skill_lv + 1) / 2;
 		int64 heal = damage * rate / 100;
@@ -25,17 +40,4 @@ void SkillHunger::castendDamageId(block_list* src, block_list* target, uint16 sk
 
 		status_heal(src, heal, 0, 0, 2);
 	}
-}
-
-void SkillHunger::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
-	const status_data* sstatus = status_get_status_data(*src);
-
-	int32 skillratio = 80 * skill_lv;
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
-		skillratio += 40 * skill_lv;
-	}
-	skillratio += sstatus->str; // TODO - unknown scaling [munkrej]
-	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
