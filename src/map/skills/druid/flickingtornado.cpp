332694a8c444ed92d3df9e0804c6a4365b1fdb02
Atemo <capucrath@gmail.com>
2026-02-05 16:12:08 +0100

    Updated DR_FLICKING_TONADO HitCount -7 dex ratio 5 single target weapon skill with splash damage

--- src/map/skills/druid/flickingtornado.cpp
+++ src/map/skills/druid/flickingtornado.cpp
@@ -3,6 +3,8 @@
 
 #include "flickingtornado.hpp"
 
+#include <config/const.hpp>
+
 #include "map/clif.hpp"
 #include "map/map.hpp"
 #include "map/status.hpp"
@@ -10,32 +12,27 @@
 SkillFlickingTornado::SkillFlickingTornado() : SkillImplRecursiveDamageSplash(DR_FLICKING_TONADO) {
 }
 
-void SkillFlickingTornado::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-	if (!(flag & 1)) {
-		uint8 dir = map_calc_dir(src, target->x, target->y);
-		int32 retreat = skill_lv >= 6 ? 5 : 3;
-		skill_blown(src, src, retreat, dir, BLOWN_IGNORE_NO_KNOCKBACK);
-	}
-}
-
-void SkillFlickingTornado::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
+void SkillFlickingTornado::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_change* sc = status_get_sc(src);
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 100 * skill_lv;
+	skillratio += -100 + 100 * skill_lv;
+
 	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 50 * skill_lv;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
+
+	skillratio += 5 * sstatus->dex;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
 
-int64 SkillFlickingTornado::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag | SD_ANIMATION);
+void SkillFlickingTornado::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
+
+	uint8 dir = map_calc_dir(src, target->x, target->y);
+	int32 retreat = skill_lv >= 6 ? 5 : 3;
+	skill_blown(src, src, retreat, dir, BLOWN_IGNORE_NO_KNOCKBACK);
 }
