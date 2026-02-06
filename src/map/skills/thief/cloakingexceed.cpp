0bd82d2fdbd8e797198e757918fa6c5b92ed4f01
Atemo <capucrath@gmail.com>
2026-02-06 15:48:54 +0100

    Corrected GC_CLOAKINGEXCEED

--- src/map/skills/thief/cloakingexceed.cpp
+++ src/map/skills/thief/cloakingexceed.cpp
@@ -12,10 +12,12 @@ SkillCloakingExceed::SkillCloakingExceed() : SkillImpl(GC_CLOAKINGEXCEED) {
 
 void SkillCloakingExceed::castendNoDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32& flag) const {
 	sc_type type = skill_get_sc(getSkillId());
+	status_change *tsc = status_get_sc(target);
+	status_change_entry *tsce = (tsc != nullptr && type != SC_NONE) ? tsc->getSCE(type) : nullptr;
 	map_session_data* sd = BL_CAST( BL_PC, src );
-	bool i = 0;
+	int32 i = 0;
 
-	if (status_get_sc(target) != nullptr && type != SC_NONE) {
+	if (tsce) {
 		i = status_change_end(target, type);
 		if( i )
 			clif_skill_nodamage(src,*target,getSkillId(),-1,i);
