50942a92eaa48dfee58b2d89b1287e5283067b89
Lemongrass3110 <lemongrass@kstp.at>
2026-02-08 11:27:27 +0100

    Apply suggestion from @Atemo
    Co-authored-by: Atemo <Atemo@users.noreply.github.com>

--- src/map/skills/merchant/abrbattlewarior.cpp
+++ src/map/skills/merchant/abrbattlewarior.cpp
@@ -5,7 +5,6 @@
 
 #include "map/clif.hpp"
 #include "map/mob.hpp"
-#include "map/skill.hpp"
 #include "map/status.hpp"
 
 SkillAbrBattleWarrior::SkillAbrBattleWarrior() : StatusSkillImpl(MT_SUMMON_ABR_BATTLE_WARIOR) {
