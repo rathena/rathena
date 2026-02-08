753bf65c08f4556e11c3ddece742fe44024b1fa4
Lemongrass3110 <lemongrass@kstp.at>
2026-02-08 11:27:56 +0100

    Apply suggestion from @Atemo
    Co-authored-by: Atemo <Atemo@users.noreply.github.com>

--- src/map/skills/merchant/abrmothernet.cpp
+++ src/map/skills/merchant/abrmothernet.cpp
@@ -5,7 +5,6 @@
 
 #include "map/clif.hpp"
 #include "map/mob.hpp"
-#include "map/skill.hpp"
 #include "map/status.hpp"
 
 SkillAbrMotherNet::SkillAbrMotherNet() : StatusSkillImpl(MT_SUMMON_ABR_MOTHER_NET) {
