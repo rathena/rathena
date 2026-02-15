1388691e4338aefdd746ad39847fae9100220638
Danilo Lemes <daniloleemes@hotmail.com>
2026-02-15 11:51:06 +0000

    fix force of vanguard

--- src/map/skills/swordman/forceofvanguard.cpp
+++ src/map/skills/swordman/forceofvanguard.cpp
@@ -12,7 +12,7 @@ SkillForceOfVanguard::SkillForceOfVanguard() : SkillImpl(LG_FORCEOFVANGUARD) {
 void SkillForceOfVanguard::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
 	const status_change* tsc = status_get_sc(target);
 	sc_type type = skill_get_sc(getSkillId());
-	status_change_entry* tsce = (tsc && type != SC_NONE) ? tsc->getSCE(type) : nullptr;
+	const status_change_entry* tsce = (tsc && type != SC_NONE) ? tsc->getSCE(type) : nullptr;
 	map_session_data* sd = BL_CAST(BL_PC, src);
 
 	int32 result;
