7ca597b2bd1f7ba17b5e82c457b0ba203da80af1
Atemo <capucrath@gmail.com>
2026-02-18 19:16:34 +0100

    KR_CLAW_WAVE str ratio 5, by damage comparison
    (unchanged)
    HitCount 3
    ground skill

--- src/map/skills/druid/clawwave.cpp
+++ src/map/skills/druid/clawwave.cpp
@@ -3,33 +3,30 @@
 
 #include "clawwave.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
 SkillClawWave::SkillClawWave() : SkillImplRecursiveDamageSplash(KR_CLAW_WAVE) {
 }
 
-void SkillClawWave::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillClawWave::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
+void SkillClawWave::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 880 + 70 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
+	skillratio += -100 + 880 + 70 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
 		skillratio += 320;
 	}
-	skillratio += sstatus->str; // TODO - unknown scaling [munkrej]
+
+	skillratio += 5 * sstatus->str;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
 
-int64 SkillClawWave::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+void SkillClawWave::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
