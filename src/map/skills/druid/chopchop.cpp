d1b26e88fe318be60311fe6d8b5b94bb0cf531ac
Atemo <capucrath@gmail.com>
2026-02-18 19:22:26 +0100

    KR_CHOP_CHOP str ratio 5, by damage comparison HitCount: -2
    (unchanged)
    self skill with splash damage using a "special" area of effect

--- src/map/skills/druid/chopchop.cpp
+++ src/map/skills/druid/chopchop.cpp
@@ -3,37 +3,32 @@
 
 #include "chopchop.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
 SkillChopChop::SkillChopChop() : SkillImplRecursiveDamageSplash(KR_CHOP_CHOP) {
 }
 
-void SkillChopChop::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillChopChop::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillChopChop::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
+void SkillChopChop::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 740 + 80 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
+	skillratio += -100 + 740 + 80 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_WOLF)) {
 		skillratio += 400;
 	}
-	skillratio += sstatus->str; // TODO - unknown scaling [munkrej]
+
+	skillratio += 5 * sstatus->str;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
 
-int64 SkillChopChop::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+void SkillChopChop::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+	// TODO : unknown usage of EFST_TASK_CHOP_CHOP (tick 9999, no icon, not refreshed), which starts (most probably) around skill_check_condition_castbegin
+	// and ends at the same timer of the last EFST_POSTDELAY
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	this->castendDamageId(src, target, skill_lv, tick, flag);
 }
