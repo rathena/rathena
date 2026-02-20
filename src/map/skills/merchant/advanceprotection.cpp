d5ae826742e04572b1b0facdc40a03d01c148003
Atemo <Atemo@users.noreply.github.com>
2026-02-20 19:59:31 +0100

    Apply suggestions from code review
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/merchant/advanceprotection.cpp
+++ src/map/skills/merchant/advanceprotection.cpp
@@ -5,13 +5,11 @@
 
 #include "map/clif.hpp"
 #include "map/pc.hpp"
-#include "map/status.hpp"
 
 SkillAdvanceProtection::SkillAdvanceProtection() : StatusSkillImpl(BO_ADVANCE_PROTECTION) {
 }
 
 void SkillAdvanceProtection::castendNoDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	sc_type type = skill_get_sc(getSkillId());
 	map_session_data* sd = BL_CAST(BL_PC, src);
 	map_session_data* dstsd = BL_CAST(BL_PC, target);
 
