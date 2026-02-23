694720fd0ef6fc2d9313254a22799edbaf1e37eb
Atemo <capucrath@gmail.com>
2026-02-24 00:16:17 +0100

    Updated AT_FURIOS_STORM
    SPL ratio 20
    Self target with splash damage
    AT_FURIOS_STORM doesn't grants any charge

--- src/map/skills/druid/furiousstorm.cpp
+++ src/map/skills/druid/furiousstorm.cpp
@@ -3,28 +3,29 @@
 
 #include "furiousstorm.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
+#include "map/status.hpp"
 
 SkillFuriousStorm::SkillFuriousStorm() : SkillImplRecursiveDamageSplash(AT_FURIOS_STORM) {
 }
 
+void SkillFuriousStorm::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	const status_data* sstatus = status_get_status_data(*src);
+
+	skillratio += -100 + 7800 + 400 * (skill_lv - 1);
+	skillratio += 20 * sstatus->spl;
+
+	RE_LVL_DMOD(100);
+}
+
 void SkillFuriousStorm::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	castendDamageId(src, target, skill_lv, tick, flag);
+	this->castendDamageId(src, target, skill_lv, tick, flag);
 }
 
-void SkillFuriousStorm::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
+void SkillFuriousStorm::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
 
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillFuriousStorm::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	int32 skillratio = 7800 + 400 * (skill_lv - 1);
-	base_skillratio += -100 + skillratio;
-}
-
-int64 SkillFuriousStorm::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
