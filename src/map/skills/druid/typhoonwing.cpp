f26e1e16f2f593768ba9d3d6ad5b7197c0dec6d9
Atemo <capucrath@gmail.com>
2026-02-23 19:54:42 +0100

    Updated KR_TYPHOON_WING Dex ratio 4, by damage comparison HitCount: 3 Ground skill Knock back

--- src/map/skills/druid/typhoonwing.cpp
+++ src/map/skills/druid/typhoonwing.cpp
@@ -3,33 +3,24 @@
 
 #include "typhoonwing.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
 SkillTyphoonWing::SkillTyphoonWing() : SkillImplRecursiveDamageSplash(KR_TYPHOON_WING) {
 }
 
-void SkillTyphoonWing::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillTyphoonWing::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
+void SkillTyphoonWing::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 600 + 80 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
+	skillratio += -100 + 600 + 80 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 300;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
-	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
-}
 
-int64 SkillTyphoonWing::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+	skillratio += 4 * sstatus->dex;
+
+	RE_LVL_DMOD(100);
 }
--- src/map/skills/druid/typhoonwing.hpp
+++ src/map/skills/druid/typhoonwing.hpp
@@ -9,7 +9,5 @@ class SkillTyphoonWing : public SkillImplRecursiveDamageSplash {
 public:
 	SkillTyphoonWing();
 
-	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
 	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
-	int64 splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
 };
