53e5e0cd1c0cbe100c1a89bf558c2b4380ebdd4a
Atemo <capucrath@gmail.com>
2026-02-23 19:44:01 +0100

    KR_SHARPEN_HAIL Dex ratio 4, by damage comparison HitCount 5 Single target weapon skill with splash damage

--- src/map/skills/druid/sharpenhail.cpp
+++ src/map/skills/druid/sharpenhail.cpp
@@ -3,33 +3,30 @@
 
 #include "sharpenhail.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
 
 SkillSharpenHail::SkillSharpenHail() : SkillImplRecursiveDamageSplash(KR_SHARPEN_HAIL) {
 }
 
-void SkillSharpenHail::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillSharpenHail::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
+void SkillSharpenHail::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 640 + 40 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
+	skillratio += -100 + 640 + 40 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 300;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
+
+	skillratio += 4 * sstatus->dex;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
 
-int64 SkillSharpenHail::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+void SkillSharpenHail::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
