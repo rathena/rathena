c89a35dcf38f20adda7b3cc34fb4d69f41de0bd3
Atemo <capucrath@gmail.com>
2026-02-23 19:46:02 +0100

    KR_FEATHER_SPRINKLE
    Dex ratio 4, by damage comparison
    HitCount: 3
    Self skill with splash damage

--- src/map/skills/druid/feathersprinkle.cpp
+++ src/map/skills/druid/feathersprinkle.cpp
@@ -3,41 +3,30 @@
 
 #include "feathersprinkle.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/status.hpp"
-
-#include "skill_factory_druid.hpp"
 
 SkillFeatherSprinkle::SkillFeatherSprinkle() : SkillImplRecursiveDamageSplash(KR_FEATHER_SPRINKLE) {
 }
 
-void SkillFeatherSprinkle::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	castendDamageId(src, target, skill_lv, tick, flag);
-}
-
-void SkillFeatherSprinkle::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
-
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-
-	SkillFactoryDruid::try_gain_growth_stacks(src, tick, getSkillId());
-}
-
-void SkillFeatherSprinkle::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
+void SkillFeatherSprinkle::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	int32 skillratio = 1100 + 90 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
+	skillratio += -100 + 1100 + 90 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
 		skillratio += 390;
 	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
+
+	skillratio += 4 * sstatus->dex;
+
 	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
 }
 
-int64 SkillFeatherSprinkle::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+void SkillFeatherSprinkle::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
+	this->castendDamageId(src, target, skill_lv, tick, flag);
 }
