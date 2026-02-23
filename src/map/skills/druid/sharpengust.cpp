f769ca3b4849a1d16f8505b7ba304dfaa59dddd3
Atemo <capucrath@gmail.com>
2026-02-23 19:41:50 +0100

    Updated KR_SHARPEN_GUST
    Dex ratio 5, by damage comparison
    HitCount 2
    Single target weapon skill with splash damage
    Splash area 3x7

--- src/map/skills/druid/sharpengust.cpp
+++ src/map/skills/druid/sharpengust.cpp
@@ -3,62 +3,43 @@
 
 #include "sharpengust.hpp"
 
+#include <config/core.hpp>
+
+#include "map/battle.hpp"
 #include "map/clif.hpp"
 #include "map/map.hpp"
 #include "map/status.hpp"
-#include "map/unit.hpp"
 
-SkillSharpenGust::SkillSharpenGust() : SkillImplRecursiveDamageSplash(KR_SHARPEN_GUST) {
+SkillSharpenGust::SkillSharpenGust() : SkillImpl(KR_SHARPEN_GUST) {
+}
+
+void SkillSharpenGust::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	const status_data* sstatus = status_get_status_data(*src);
+
+	skillratio += -100 + 1000 + 80 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
+		skillratio += 550;
+	}
+
+	skillratio += 5 * sstatus->dex;
+
+	RE_LVL_DMOD(100);
 }
 
 void SkillSharpenGust::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
 
-	if (flag & 1) {
-		SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
-		return;
-	}
-
-	const int16 path_width = 1;
-	const int32 path_length = 7;
-	const uint8 dir = map_calc_dir(src, target->x, target->y);
-	const int16 dir_x = dirx[dir];
-	const int16 dir_y = diry[dir];
-	const int16 start_x = src->x;
-	const int16 start_y = src->y;
-	const int16 end_x = src->x + dir_x;
-	const int16 end_y = src->y + dir_y;
-
-	skill_area_temp[0] = 0;
 	skill_area_temp[1] = target->id;
-	skill_area_temp[2] = 0;
 
 	if (battle_config.skill_eightpath_algorithm) {
 		// Use official AoE algorithm
-		map_foreachindir(skill_area_sub, src->m, start_x, start_y, end_x, end_y, path_width, path_length, 1, splash_target(src),
-			src, getSkillId(), skill_lv, tick, flag | BCT_ENEMY | SD_PREAMBLE | SD_SPLASH | 1, skill_castend_damage_id);
+		map_foreachindir(skill_attack_area, src->m, src->x, src->y, target->x, target->y,
+			skill_get_splash(getSkillId(), skill_lv), skill_get_range(getSkillId(), skill_lv)+2, 0, splash_target(src),
+			skill_get_type(getSkillId()), src, src, getSkillId(), skill_lv, tick, flag, BCT_ENEMY);
+	} else {
+		map_foreachinpath(skill_attack_area, src->m, src->x, src->y, target->x, target->y,
+			skill_get_splash(getSkillId(), skill_lv), skill_get_range(getSkillId(), skill_lv)+2, splash_target(src),
+			skill_get_type(getSkillId()), src, src, getSkillId(), skill_lv, tick, flag, BCT_ENEMY);
 	}
-	else {
-		map_foreachindir(skill_area_sub, src->m, start_x, start_y, end_x, end_y, path_width, path_length, 1, splash_target(src),
-			src, getSkillId(), skill_lv, tick, flag | BCT_ENEMY | SD_PREAMBLE | SD_SPLASH | 1, skill_castend_damage_id);
-	}
-}
-
-void SkillSharpenGust::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
-	const status_data* sstatus = status_get_status_data(*src);
-
-	int32 skillratio = 1000 + 80 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_ENRAGE_RAPTOR)) {
-		skillratio += 550;
-	}
-	skillratio += sstatus->dex; // TODO - unknown scaling [munkrej]
-	RE_LVL_DMOD(100);
-	base_skillratio += -100 + skillratio;
-}
-
-int64 SkillSharpenGust::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
 }
--- src/map/skills/druid/sharpengust.hpp
+++ src/map/skills/druid/sharpengust.hpp
@@ -5,11 +5,10 @@
 
 #include "../skill_impl.hpp"
 
-class SkillSharpenGust : public SkillImplRecursiveDamageSplash {
+class SkillSharpenGust : public SkillImpl {
 public:
 	SkillSharpenGust();
 
-	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
 	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
-	int64 splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
+	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
 };
