423e1f7fc4c18ed25b2fc9458838b7276c1ec2ed
Atemo <capucrath@gmail.com>
2026-02-24 12:05:09 +0100

    Updated AT_GLACIER_STOMP
    SPL ratio 4 with SC_TRUTH_OF_ICE
    HitCount 2
    Self skill with splash damage
    Teleport the player near the center (if there are no obstacles)

--- src/map/skills/druid/glacialstomp.cpp
+++ src/map/skills/druid/glacialstomp.cpp
@@ -3,69 +3,77 @@
 
 #include "glacialstomp.hpp"
 
+#include <config/core.hpp>
+
 #include "map/clif.hpp"
 #include "map/pc.hpp"
-#include "map/skill.hpp"
 #include "map/status.hpp"
+#include "map/unit.hpp"
 
-#include "skill_factory_druid.hpp"
+#include "glacialnova.hpp"
 
 SkillGlacialStomp::SkillGlacialStomp() : SkillImplRecursiveDamageSplash(AT_GLACIER_STOMP) {
 }
 
-void SkillGlacialStomp::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	castendDamageId(src, target, skill_lv, tick, flag);
+void SkillGlacialStomp::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	skillratio += -100 + 6400 + 500 * (skill_lv - 1);
+
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_TRUTH_OF_ICE)) {
+		const status_data* sstatus = status_get_status_data(*src);
+
+		skillratio += 4 * sstatus->spl;
+	}
+
+	// Unlike what the description indicates, the BaseLevel modifier is not part of the condition on SC_TRUTH_OF_ICE
+	RE_LVL_DMOD(100);
 }
 
-void SkillGlacialStomp::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+void SkillGlacialStomp::castendNoDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
+	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+
 	status_change* sc = status_get_sc(src);
 
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
+	if (sc == nullptr) {
+		if (map_session_data* sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
+		}
+		return;
 	}
 
-	if (!(flag & 1)) {
-		int32 gx = 0;
-		int32 gy = 0;
-		if (!SkillFactoryDruid::get_glacier_center_on_map(src, sc, gx, gy)) {
-			map_session_data* sd = BL_CAST(BL_PC, src);
-			if (sd) {
-				clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
-			}
-			return;
-		}
-		if (!unit_movepos(src, gx, gy, 2, true)) {
-			map_session_data* sd = BL_CAST(BL_PC, src);
-			if (sd) {
-				clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
-			}
-			return;
-		}
-		clif_fixpos(*src);
-	}
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
+	const status_change_entry *sce = sc->getSCE(SC_GLACIER_SHEILD);
 
-	if (!(flag & 1)) {
-		int32 gx = 0;
-		int32 gy = 0;
-		if (SkillFactoryDruid::get_glacier_center_on_map(src, sc, gx, gy)) {
-			skill_castend_pos2(src, gx, gy, AT_GLACIER_NOVA, 1, tick, 0);
+	if (sce == nullptr) {
+		if (map_session_data* sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
 		}
+		return;
 	}
+
+	if (src->m != sce->val4) {
+		if (map_session_data* sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
+		}
+		return;
+	}
+
+	// TODO : Should the distance to the player be checked?
+	// TODO : the player should be teleported to one case from the center
+
+	if (!unit_movepos(src, sce->val2, sce->val3, 2, true)) {
+		if (map_session_data* sd = BL_CAST(BL_PC, src); sd != nullptr) {
+			clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL);
+		}
+		return;
+	}
+
+	clif_fixpos(*src);
+
+	this->castendDamageId(src, target, skill_lv, tick, flag);
 }
 
-void SkillGlacialStomp::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
-	const status_data* sstatus = status_get_status_data(*src);
+void SkillGlacialStomp::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
+	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 
-	int32 skillratio = 6400 + 500 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_TRUTH_OF_ICE)) {
-		skillratio += sstatus->int_; // TODO - unknown scaling [munkrej]
-		RE_LVL_DMOD(100);
-	}
-	base_skillratio += -100 + skillratio;
-}
-
-int64 SkillGlacialStomp::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+	SkillGlacialNova skillnova;
+	skillnova.castendPos2(src, 0, 0, 1, tick, flag);
 }
