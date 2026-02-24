56761ebe2850f70d9aaa75928c7b22fa44130426
Atemo <Atemo@users.noreply.github.com>
2026-02-24 11:01:07 +0100

    Update src/map/skills/gunslinger/hammerofgod.cpp

--- src/map/skills/gunslinger/hammerofgod.cpp
+++ src/map/skills/gunslinger/hammerofgod.cpp
@@ -14,7 +14,6 @@ SkillHammerOfGod::SkillHammerOfGod() : SkillImplRecursiveDamageSplash(RL_HAMMER_
 }
 
 
-// TODO: refactor to SkillImplRecursiveDamageSplash
 void SkillHammerOfGod::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
 	const map_session_data* sd = BL_CAST(BL_PC, src);
 	const status_change* tsc = status_get_sc(target);
