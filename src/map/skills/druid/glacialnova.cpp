29c5fc0d5c5ecadc88581c01f9ced32a42797799
Atemo <capucrath@gmail.com>
2026-02-24 11:48:33 +0100

    Updated AT_GLACIER_NOVA Rewrote using castendPos2 SPL ratio 15 Ground skill, centered on Glacial monolith (x, y saved in SC_GLACIER_SHEILD)

--- src/map/skills/druid/glacialnova.cpp
+++ src/map/skills/druid/glacialnova.cpp
@@ -3,25 +3,40 @@
 
 #include "glacialnova.hpp"
 
-#include "map/clif.hpp"
-#include "map/skill.hpp"
+#include <config/core.hpp>
+
+#include "map/status.hpp"
 
 SkillGlacialNova::SkillGlacialNova() : SkillImplRecursiveDamageSplash(AT_GLACIER_NOVA) {
 }
 
-void SkillGlacialNova::castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	if (!(flag & 1)) {
-		clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	}
+void SkillGlacialNova::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	const status_data* sstatus = status_get_status_data(*src);
 
-	SkillImplRecursiveDamageSplash::castendDamageId(src, target, skill_lv, tick, flag);
+	skillratio += -100 + 15000;
+
+	// SPL and BaseLevel ratio do not depend on SC_TRUTH_OF_ICE
+	skillratio += 15 * sstatus->spl;
+
+	RE_LVL_DMOD(100);
 }
 
-void SkillGlacialNova::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	int32 skillratio = 15000;
-	base_skillratio += -100 + skillratio;
-}
+void SkillGlacialNova::castendPos2(block_list* src, int32 x, int32 y, uint16 skill_lv, t_tick tick, int32& flag) const {
+	status_change* sc = status_get_sc(src);
 
-int64 SkillGlacialNova::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag);
+	if (sc == nullptr)
+		return;
+
+	const status_change_entry *sce = sc->getSCE(SC_GLACIER_SHEILD);
+
+	if (sce == nullptr)
+		return;
+
+	if (src->m != sce->val4)
+		return;
+
+	// TODO : Should the distance to the player be checked?
+	// On official server SC_GLACIER_SHEILD does not save the position of glacial monolith
+
+	SkillImplRecursiveDamageSplash::castendPos2(src, sce->val2, sce->val3, skill_lv, tick, flag);
 }
--- src/map/skills/druid/glacialnova.hpp
+++ src/map/skills/druid/glacialnova.hpp
@@ -9,7 +9,6 @@ class SkillGlacialNova : public SkillImplRecursiveDamageSplash {
 public:
 	SkillGlacialNova();
 
-	void castendDamageId(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& flag) const override;
 	void calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
-	int64 splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
+	void castendPos2(block_list* src, int32 x, int32 y, uint16 skill_lv, t_tick tick, int32& flag) const override;
 };
