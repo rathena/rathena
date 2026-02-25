c54e129457de4e71576887ac76ec79a86d4fbec3
Danilo Lemes <daniloleemes@hotmail.com>
2026-02-25 18:49:20 +0000

    Split up Strengthen Guardians skill (GD_GUARDUP:10003)

--- src/map/skills/guild/skill_factory_guild.cpp
+++ src/map/skills/guild/skill_factory_guild.cpp
@@ -13,6 +13,8 @@ std::unique_ptr<const SkillImpl> SkillFactoryGuild::create(const e_skill skill_i
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case GD_GUARDRESEARCH:
 			return std::make_unique<StatusSkillImpl>(skill_id);
+		case GD_GUARDUP:
+			return std::make_unique<StatusSkillImpl>(skill_id);
 
 		default:
 			return nullptr;
