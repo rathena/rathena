4e90e2994919f1dda206576783854e2007d7efcf
Danilo Lemes <daniloleemes@hotmail.com>
2026-02-25 18:48:59 +0000

    Split up Guardian Research skill (GD_GUARDRESEARCH:10002)

--- src/map/skills/guild/skill_factory_guild.cpp
+++ src/map/skills/guild/skill_factory_guild.cpp
@@ -11,6 +11,8 @@ std::unique_ptr<const SkillImpl> SkillFactoryGuild::create(const e_skill skill_i
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case GD_KAFRACONTRACT:
 			return std::make_unique<StatusSkillImpl>(skill_id);
+		case GD_GUARDRESEARCH:
+			return std::make_unique<StatusSkillImpl>(skill_id);
 
 		default:
 			return nullptr;
