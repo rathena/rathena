fab7a58972c3106528ff953e86e861e2101f7026
Atemo <capucrath@gmail.com>
2026-03-12 12:26:47 +0100

    Updated dragontail

--- src/map/skills/gunslinger/dragontail.cpp
+++ src/map/skills/gunslinger/dragontail.cpp
@@ -6,8 +6,6 @@
 #include <config/core.hpp>
 
 #include "map/clif.hpp"
-#include "map/map.hpp"
-#include "map/pc.hpp"
 #include "map/status.hpp"
 
 SkillDragonTail::SkillDragonTail() : SkillImplRecursiveDamageSplash(RL_D_TAIL) {
@@ -17,22 +15,16 @@ int32 SkillDragonTail::getSplashTarget(block_list* src) const {
 	return BL_CHAR;
 }
 
-int64 SkillDragonTail::splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	return skill_attack(skill_get_type(getSkillId()), src, src, target, getSkillId(), skill_lv, tick, flag | SD_ANIMATION);
-}
-
 void SkillDragonTail::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
-	const map_session_data* sd = BL_CAST(BL_PC, src);
 	const status_change* tsc = status_get_sc(target);
 
-	if (sd != nullptr && tsc != nullptr && tsc->hasSCE(SC_C_MARKER)) {
-		int32 i = 0;
-
-		ARR_FIND(0, MAX_SKILL_CRIMSON_MARKER, i, sd->c_marker[i] == target->id);
-		if (i < MAX_SKILL_CRIMSON_MARKER) {
-			flag |= 8;
-		}
+	// TODO: do we need to check the src id?
+	if (tsc != nullptr && tsc->hasSCE(SC_C_MARKER) && tsc->getSCE(SC_C_MARKER)->val2 == src->id) {
+		skill_area_temp[0] |= SKILL_ALTDMG_FLAG;
 	}
+
+	// Disable skill animation
+	skill_area_temp[1] = 0;
 
 	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
 
@@ -42,7 +34,7 @@ void SkillDragonTail::splashSearch(block_list* src, block_list* target, uint16 s
 void SkillDragonTail::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& skillratio, int32 mflag) const {
 	skillratio += -100 + 500 + 200 * skill_lv;
 
-	if (const map_session_data* sd = BL_CAST(BL_PC, src); sd != nullptr && (wd->miscflag & 8)) {
+	if (wd->miscflag & SKILL_ALTDMG_FLAG) {
 		skillratio *= 2;
 	}
 
