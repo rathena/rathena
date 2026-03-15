ae5e338c3c45a47f3bd73877d3803e62b10c57c6
Lemongrass3110 <lemongrass@kstp.at>
2026-03-15 15:36:56 +0100

    Refactored self skills that give damage around caster (#9918)
    This is based on the old "List of self skills that give damage around caster".
    So there may be more skills that need refactoring.
    
    Co-authored-by: Atemo <Atemo@users.noreply.github.com>

--- src/map/skills/taekwon/fallingstar.hpp
+++ src/map/skills/taekwon/fallingstar.hpp
@@ -13,12 +13,12 @@ public:
 
 
 // SJ_FALLINGSTAR_ATK2
-class SkillFallingStarAttack : public WeaponSkillImpl {
+class SkillFallingStarAttack : public SkillImplRecursiveDamageSplash {
 public:
 	SkillFallingStarAttack();
 
 	void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &skillratio, int32 mflag) const override;
-	void castendDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32& flag) const override;
+	int64 splashDamage(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const override;
 	void castendNoDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32& flag) const override;
 };
 
