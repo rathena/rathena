a4d7f148117a3a4e977592fe2017d5ba0b0f2293
Atemo <capucrath@gmail.com>
2026-03-15 13:04:22 +0100

    Updated AT_TERRA_HARVEST * updated HitCount * updated spl ratio

--- src/map/skills/druid/terraharvest.cpp
+++ src/map/skills/druid/terraharvest.cpp
@@ -3,29 +3,35 @@
 
 #include "terraharvest.hpp"
 
-#include "map/clif.hpp"
-#include "map/skill.hpp"
+#include <config/core.hpp>
 
-#include "skill_factory_druid.hpp"
+#include "map/clif.hpp"
+#include "map/status.hpp"
+
+#include "groundbloom.hpp"
 
 SkillTerraHarvest::SkillTerraHarvest() : SkillImplRecursiveDamageSplash(AT_TERRA_HARVEST) {
 }
 
-void SkillTerraHarvest::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
-	const status_change* sc = status_get_sc(src);
-	const status_data* sstatus = status_get_status_data(*src);
+void SkillTerraHarvest::calculateSkillRatio(const Damage*, const block_list* src, const block_list*, uint16 skill_lv, int32& skillratio, int32 mflag) const {
+	skillratio += -100 + 18000 + 500 * (skill_lv - 1);
 
-	int32 skillratio = 18000 + 500 * (skill_lv - 1);
-	if (sc != nullptr && sc->hasSCE(SC_TRUTH_OF_EARTH)) {
-		skillratio += sstatus->int_; // TODO - unknown scaling [munkrej]
-		RE_LVL_DMOD(100);
+	if (const status_change* sc = status_get_sc(src); sc != nullptr && sc->hasSCE(SC_TRUTH_OF_EARTH)) {
+		const status_data* sstatus = status_get_status_data(*src);
+
+		skillratio += 15 * sstatus->spl;
 	}
-	base_skillratio += -100 + skillratio;
+
+	// Unlike what the description indicates, the BaseLevel modifier is not part of the condition on SC_TRUTH_OF_EARTH
+	RE_LVL_DMOD(100);
 }
 
 void SkillTerraHarvest::splashSearch(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32 flag) const {
 	clif_skill_nodamage(src, *target, getSkillId(), skill_lv);
-	SkillFactoryDruid::try_gain_growth_stacks(src, tick, getSkillId());
 
+	// Updates growth status and casts Ground Bloom if the conditions are met
+	SkillGroundBloom::castGroundBloom(src, tick, 2);
+
+	// Skill damage
 	SkillImplRecursiveDamageSplash::splashSearch(src, target, skill_lv, tick, flag);
 }
