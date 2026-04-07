abc7dec7230a2b082265ae6ccb7b43bed6ad29b7
Atemo <capucrath@gmail.com>
2026-04-07 20:58:58 +0200

    Shadow Stab - Removes dagger exclusive condition. - Removes Cloaking Exceed bonus. - Changes base damage from 2750%/3250%(Cloaking Exceed) ATK per hit to 3250% ATK per hit based on level 5.

--- src/map/skills/thief/shadowstab.cpp
+++ src/map/skills/thief/shadowstab.cpp
@@ -14,22 +14,13 @@ SkillShadowStab::SkillShadowStab() : WeaponSkillImpl(SHC_SHADOW_STAB) {
 void SkillShadowStab::calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &skillratio, int32 mflag) const {
 	const status_data* sstatus = status_get_status_data(*src);
 
-	skillratio += -100 + 550 * skill_lv;
-	skillratio += 5 * sstatus->pow;
-
-	if (wd->miscflag & SKILL_ALTDMG_FLAG) {
-		skillratio += 100 * skill_lv + 2 * sstatus->pow;
-	}
+	skillratio += -100 + 650 * skill_lv;
+	skillratio += 5 * sstatus->pow;	// TODO : check pow ratio
 
 	RE_LVL_DMOD(100);
 }
 
 void SkillShadowStab::castendDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32& flag) const {
-	status_change *sc = status_get_sc(src);
-
-	if (sc && sc->getSCE(SC_CLOAKINGEXCEED))
-		flag |= SKILL_ALTDMG_FLAG;
-
 	status_change_end(src, SC_CLOAKING);
 	status_change_end(src, SC_CLOAKINGEXCEED);
 
