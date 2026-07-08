97a66ec0a619b4b79361033db4bdef8466bf8d87
llchrisll <h0und3y3@gmail.com>
2026-07-08 08:09:55 +0200

    Initial implementation of custom Taekwon status calls of Mild Wind skill (TK_SEVENWIND) - buff_icons_tk battle flag added in conf/battle/status.conf - Edited original Mild Wind status to support new ones

--- src/map/skills/taekwon/sevenwind.cpp
+++ src/map/skills/taekwon/sevenwind.cpp
@@ -12,7 +12,9 @@ SkillSevenWind::SkillSevenWind() : SkillImpl(TK_SEVENWIND) {
 void SkillSevenWind::castendNoDamageId(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 &flag) const {
 	sc_type type = SC_NONE;
 
-	switch (skill_get_ele(getSkillId(), skill_lv)) {
+	if (battle_config.buff_icons_tk == 0) { // [llchrisll]
+		switch (skill_get_ele(getSkillId(), skill_lv)) {
+
 		case ELE_EARTH:
 			type = SC_EARTHWEAPON;
 			break;
@@ -34,6 +36,34 @@ void SkillSevenWind::castendNoDamageId(block_list *src, block_list *target, uint
 		case ELE_HOLY:
 			type = SC_ASPERSIO;
 			break;
+		}
+
+	}
+	else {
+		switch (skill_get_ele(getSkillId(), skill_lv)) {
+
+		case ELE_EARTH:
+			type = SC_EARTHWEAPON_TK;
+			break;
+		case ELE_WIND:
+			type = SC_WINDWEAPON_TK;
+			break;
+		case ELE_WATER:
+			type = SC_WATERWEAPON_TK;
+			break;
+		case ELE_FIRE:
+			type = SC_FIREWEAPON_TK;
+			break;
+		case ELE_GHOST:
+			type = SC_GHOSTWEAPON;
+			break;
+		case ELE_DARK:
+			type = SC_SHADOWWEAPON;
+			break;
+		case ELE_HOLY:
+			type = SC_HOLYWEAPON_TK;
+			break;
+		}
 	}
 
 	clif_skill_nodamage(src, *target, getSkillId(), skill_lv, sc_start(src, target, type, 100, skill_lv, skill_get_time(getSkillId(), skill_lv)));
